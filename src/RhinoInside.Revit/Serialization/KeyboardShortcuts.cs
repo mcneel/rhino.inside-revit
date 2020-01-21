d90ef8550a707173061ea2790926a5837df82d52
Kike <kike@mcneel.com>
2020-01-21 10:37:38 +0100

    Moved RhinoInside.Revit.UI.CommandRhinoInside.RegisterShortcut to RhinoInside.Revit.Serialization.KeyboardShortcuts.RegisterDefaultShortcut

--- src/RhinoInside.Revit/Serialization/KeyboardShortcuts.cs
+++ src/RhinoInside.Revit/Serialization/KeyboardShortcuts.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml.Serialization;
 
 namespace RhinoInside.Revit.Serialization
@@ -65,5 +66,69 @@ namespace RhinoInside.Revit.Serialization
         return false;
       }
     }
-  }
+
+    public static bool RegisterDefaultShortcut(string tabName, string panelName, string commandId, string commandName, string commandShortcuts)
+    {
+      commandId = $"CustomCtrl_%CustomCtrl_%{tabName}%{panelName}%{commandId}";
+
+      string keyboardShortcutsPath = Path.Combine(Revit.CurrentUsersDataFolderPath, "KeyboardShortcuts.xml");
+      if (!File.Exists(keyboardShortcutsPath))
+        keyboardShortcutsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Autodesk", $"RVT {Revit.ApplicationUI.ControlledApplication.VersionNumber}", "UserDataCache", "KeyboardShortcuts.xml");
+
+      if (!LoadFrom(keyboardShortcutsPath, out var shortcuts))
+        LoadFromResources($"RhinoInside.Resources.RVT{Revit.ApplicationUI.ControlledApplication.VersionNumber}.KeyboardShortcuts.xml", out shortcuts);
+
+#if DEBUG
+      // Those lines generate the KeyboardShortcuts.xml template file when new Revit version is supported
+      string keyboardShortcutsTemplatePath = Path.Combine(Addin.SourceCodePath, "Resources", $"RVT{Revit.ApplicationUI.ControlledApplication.VersionNumber}", "KeyboardShortcuts.xml");
+      var info = new FileInfo(keyboardShortcutsTemplatePath);
+      if (info.Length == 0)
+      {
+        var shortcutsSummary = new Serialization.KeyboardShortcuts.Shortcuts();
+        foreach (var shortcutItem in shortcuts.OrderBy(x => x.CommandId))
+        {
+          if (!string.IsNullOrEmpty(shortcutItem.Shortcuts))
+          {
+            var shortcutDefinition = new Serialization.KeyboardShortcuts.ShortcutItem
+            {
+              CommandId = shortcutItem.CommandId,
+              Shortcuts = shortcutItem.Shortcuts
+            };
+            shortcutsSummary.Add(shortcutDefinition);
+          }
+        }
+
+        SaveAs(shortcutsSummary, keyboardShortcutsTemplatePath);
+      }
+#endif
+
+      bool shortcutUpdated = false;
+      try
+      {
+        var shortcutItem = shortcuts.Where(x => x.CommandId == commandId).First();
+        if (shortcutItem.Shortcuts is null)
+        {
+          shortcutItem.Shortcuts = commandShortcuts;
+          shortcutUpdated = true;
+        }
+      }
+      catch (InvalidOperationException)
+      {
+        var shortcutItem = new ShortcutItem()
+        {
+          CommandName = commandName,
+          CommandId = commandId,
+          Shortcuts = commandShortcuts,
+          Paths = $"{tabName}>{panelName}"
+        };
+        shortcuts.Add(shortcutItem);
+        shortcutUpdated = true;
+      }
+
+      if (shortcutUpdated)
+        SaveAs(shortcuts, Path.Combine(Revit.CurrentUsersDataFolderPath, "KeyboardShortcuts.xml"));
+
+      return shortcutUpdated;
+    }
+ }
 }
