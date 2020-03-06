71445121018ccd97643aa6cd33fc4f1a96f2fc56
Kike <kike@mcneel.com>
2020-03-06 09:32:47 +0100

    Added Keyword "Revit" to all components.

--- src/RhinoInside.Revit.GH/GH/Components/Component.cs
+++ src/RhinoInside.Revit.GH/GH/Components/Component.cs
@@ -2,6 +2,7 @@ using System.Drawing;
 using System.Linq;
 using DB = Autodesk.Revit.DB;
 using Grasshopper.Kernel;
+using System.Collections.Generic;
 
 namespace RhinoInside.Revit.GH.Components
 {
@@ -23,6 +24,9 @@ namespace RhinoInside.Revit.GH.Components
   {
     protected Component(string name, string nickname, string description, string category, string subCategory)
     : base(name, nickname, description, category, subCategory) { }
+
+    static string[] keywords = new string[] { "Revit" };
+    public override IEnumerable<string> Keywords => base.Keywords is null ? keywords : Enumerable.Concat(base.Keywords, keywords);
 
     protected virtual DB.ElementFilter ElementFilter { get; }
     public virtual bool NeedsToBeExpired(DB.Events.DocumentChangedEventArgs e)
