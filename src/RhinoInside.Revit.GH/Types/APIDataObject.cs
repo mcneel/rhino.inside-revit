0a41deaa52a185600026507031a5f68e9ad37ec3
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-04-02 14:04:48 -0700

    Added wrapper gh_type to wrap host API data objects

---
+++ src/RhinoInside.Revit.GH/Types/APIDataObject.cs
@@ -0,0 +1,26 @@
+using System;
+
+using Grasshopper.Kernel.Types;
+
+using DB = Autodesk.Revit.DB;
+
+namespace RhinoInside.Revit.GH.Types
+{
+  public class APIDataObject : GH_Goo<object>
+  {
+    public override bool IsValid => Value != null;
+
+    public override string TypeName => "Revit API Data Object";
+
+    public override string TypeDescription => "Wraps Revit API Data Objects";
+
+    public APIDataObject(object apiObject): base(apiObject) {}
+
+    public override IGH_Goo Duplicate()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override string ToString() => $"Revit API Data Object: {Value.GetType().Name}";
+  }
+}
