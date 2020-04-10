39ae96648512786f4ce5609fc1227404794dd42b
Kike <kike@mcneel.com>
2020-04-10 19:01:08 +0200

    Added Document.WallTypes, Element.Dependents and ElementType.Instances

---
+++ src/RhinoInside.Revit.GH/Components/Element/Dependents.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+using Grasshopper.Kernel;
+using DB = Autodesk.Revit.DB;
+
+namespace RhinoInside.Revit.GH.Components
+{
+  public class ElementDependents : Component
+  {
+    public override Guid ComponentGuid => new Guid("97D71AA8-6987-45B9-8F25-B92671E20EF4");
+    protected override string IconTag => "D";
+
+    public ElementDependents() : base
+    (
+      name: "Element Dependents",
+      nickname: "Dependents",
+      description: "Queries for all elements that, from a logical point of view, are the children of Element",
+      category: "Revit",
+      subCategory: "Element")
+    { }
+
+    protected override void RegisterInputParams(GH_InputParamManager manager)
+    {
+      manager.AddParameter(new Parameters.Element(), "Element", "E", string.Empty, GH_ParamAccess.item);
+      manager[manager.AddParameter(new Parameters.ElementFilter(), "Filter", "F", "Filter that will be applied to dependant elements", GH_ParamAccess.item)].Optional = true;
+    }
+
+    protected override void RegisterOutputParams(GH_OutputParamManager manager)
+    {
+      manager.AddParameter(new Parameters.Element(), "Elements", "E", "ElementType of the Element", GH_ParamAccess.list);
+    }
+
+    protected override void TrySolveInstance(IGH_DataAccess DA)
+    {
+      DB.Element element = null;
+      if (!DA.GetData("Element", ref element))
+        return;
+
+      DB.ElementFilter filter = null;
+      DA.GetData("Filter", ref filter);
+
+      var elements = element.GetDependentElements(filter);
+
+      DA.SetDataList("Elements", elements.Select(x => Types.Element.FromElementId(element.Document, x)));
+    }
+  }
+}
---
+++ src/RhinoInside.Revit.GH/Components/ElementType/Instances.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using Grasshopper.Kernel;
+using DB = Autodesk.Revit.DB;
+
+namespace RhinoInside.Revit.GH.Components
+{
+  public class ElementTypeInstances : Component
+  {
+    public override Guid ComponentGuid => new Guid("9958995F-CCD4-48DE-B3B3-AE769F04F4DD");
+    public override GH_Exposure Exposure => GH_Exposure.secondary;
+
+    protected override string IconTag => "I";
+
+    public ElementTypeInstances() : base
+    (
+      "ElementType Instances", "Instances",
+      "Obtains all elements of the specified Type",
+      "Revit", "Type"
+    )
+    { }
+
+    protected override void RegisterInputParams(GH_InputParamManager manager)
+    {
+      manager.AddParameter(new Parameters.ElementType(), "Type", "T", "ElementType to query for its instances", GH_ParamAccess.item);
+      manager[manager.AddParameter(new Parameters.ElementFilter(), "Filter", "F", "Filter", GH_ParamAccess.item)].Optional = true;
+    }
+
+    protected override void RegisterOutputParams(GH_OutputParamManager manager)
+    {
+      manager.AddParameter(new Parameters.GraphicalElement(), "Elements", "E", string.Empty, GH_ParamAccess.list);
+    }
+
+    protected override void TrySolveInstance(IGH_DataAccess DA)
+    {
+      var elementType = default(DB.ElementType);
+      if (!DA.GetData("Type", ref elementType))
+        return;
+
+      var filter = default(DB.ElementFilter);
+      DA.GetData("Filter", ref filter);
+
+      using (var collector = new DB.FilteredElementCollector(elementType.Document))
+      {
+        var elementCollector = collector.WhereElementIsNotElementType();
+
+        if(elementType.Category?.Id is DB.ElementId categoryId)
+          elementCollector = elementCollector.OfCategoryId(categoryId);
+
+        if (filter is object)
+          elementCollector = elementCollector.WherePasses(filter);
+
+        DA.SetDataList
+        (
+          "Elements",
+          elementCollector.
+          OfTypeId(elementType.Id).
+          Select(x => Types.Element.FromElement(x))
+        );
+      }
+    }
+  }
+}
