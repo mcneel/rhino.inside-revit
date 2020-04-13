a43c1ac7bdb889379fe8a8dd553d562383d25124
Kike <kike@mcneel.com>
2020-04-13 19:20:59 +0200

    Moved ApplicationServices.Application extension methods to External.ApplicationServices.Extensions

--- src/RhinoInside.Revit/External/ApplicationServices/Extensions/Application.cs
+++ src/RhinoInside.Revit/External/ApplicationServices/Extensions/Application.cs
@@ -40,23 +40,23 @@ namespace RhinoInside.Revit.External.ApplicationServices.Extensions
     {
       switch (value)
       {
-        case LanguageType.English_USA:          return 1033;
-        case LanguageType.German:               return 1031;
-        case LanguageType.Spanish:              return 1034;
-        case LanguageType.French:               return 1036;
-        case LanguageType.Italian:              return 1040;
-        case LanguageType.Dutch:                return 1043;
-        case LanguageType.Chinese_Simplified:   return 2052;
-        case LanguageType.Chinese_Traditional:  return 1028;
-        case LanguageType.Japanese:             return 1041;
-        case LanguageType.Korean:               return 1042;
-        case LanguageType.Russian:              return 1049;
-        case LanguageType.Czech:                return 1029;
-        case LanguageType.Polish:               return 1045;
-        case LanguageType.Hungarian:            return 1038;
+        case LanguageType.English_USA: return 1033;
+        case LanguageType.German: return 1031;
+        case LanguageType.Spanish: return 1034;
+        case LanguageType.French: return 1036;
+        case LanguageType.Italian: return 1040;
+        case LanguageType.Dutch: return 1043;
+        case LanguageType.Chinese_Simplified: return 2052;
+        case LanguageType.Chinese_Traditional: return 1028;
+        case LanguageType.Japanese: return 1041;
+        case LanguageType.Korean: return 1042;
+        case LanguageType.Russian: return 1049;
+        case LanguageType.Czech: return 1029;
+        case LanguageType.Polish: return 1045;
+        case LanguageType.Hungarian: return 1038;
         case LanguageType.Brazilian_Portuguese: return 1046;
 #if REVIT_2018
-        case LanguageType.English_GB:           return 2057;
+        case LanguageType.English_GB: return 2057;
 #endif
       }
 
