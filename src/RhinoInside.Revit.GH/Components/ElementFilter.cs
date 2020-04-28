03b3291ea4186d017b9f9b204999f364cb970af2
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-04-28 11:48:41 -0700

    filter components now looks up the input level correctly

--- src/RhinoInside.Revit.GH/Components/ElementFilter.cs
+++ src/RhinoInside.Revit.GH/Components/ElementFilter.cs
@@ -518,7 +518,7 @@ namespace RhinoInside.Revit.GH.Components
       var _Level_ = Params.IndexOfInputParam("Level");
       if
       (
-        Params.Output[_Level_].DataType != GH_ParamData.@void &&
+        Params.Input[_Level_].DataType != GH_ParamData.@void &&
         !DA.GetData(_Level_, ref level)
       )
         return;
@@ -553,7 +553,7 @@ namespace RhinoInside.Revit.GH.Components
       var _DesignOption_ = Params.IndexOfInputParam("Design Option");
       if
       (
-        Params.Output[_DesignOption_].DataType != GH_ParamData.@void &&
+        Params.Input[_DesignOption_].DataType != GH_ParamData.@void &&
         !DA.GetData(_DesignOption_, ref designOption)
       )
         return;
@@ -588,7 +588,7 @@ namespace RhinoInside.Revit.GH.Components
       var _View_ = Params.IndexOfInputParam("View");
       if
       (
-        Params.Output[_View_].DataType != GH_ParamData.@void &&
+        Params.Input[_View_].DataType != GH_ParamData.@void &&
         !DA.GetData(_View_, ref view)
       )
         return;
