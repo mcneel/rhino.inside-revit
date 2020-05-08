9b82d5ef71fe99a9675c6d90d158bc006bc990f6
Kike <kike@mcneel.com>
2020-05-08 17:01:21 +0200

    Added Types.MullionPosition and Types.MullionProfile

--- src/RhinoInside.Revit.GH/Types/Mullion.cs
+++ src/RhinoInside.Revit.GH/Types/Mullion.cs
@@ -8,11 +8,11 @@ namespace RhinoInside.Revit.GH.Types
   {
     public override string TypeDescription => "Represents a Revit Curtain Grid Mullion Element";
     protected override Type ScriptVariableType => typeof(DB.Mullion);
-    public static explicit operator DB.Mullion(Mullion self) =>
-      self.Document?.GetElement(self) as DB.Mullion;
+    public static explicit operator DB.Mullion(Mullion value) =>
+      value.Document?.GetElement(value) as DB.Mullion;
 
     public Mullion() { }
-    public Mullion(DB.Mullion mullion) : base(mullion) { }
+    public Mullion(DB.Mullion value) : base(value) { }
 
     public override Rhino.Geometry.Curve Axis
     {
@@ -27,6 +27,63 @@ namespace RhinoInside.Revit.GH.Types
         Rhino.Geometry.Curve zeroLengthCurve = new Rhino.Geometry.Line(basepoint, basepoint).ToNurbsCurve();
 
         return axisCurve ?? zeroLengthCurve;
+      }
+    }
+  }
+
+  public class MullionPosition : ElementType
+  {
+    public override string TypeName => "Revit Mullion Position";
+    public override string TypeDescription => "Represents a Revit Mullion Postion";
+    protected override Type ScriptVariableType => typeof(DB.ElementType);
+
+    public MullionPosition() { }
+    public MullionPosition(DB.Document doc, DB.ElementId id) : base(doc, id) { }
+
+    public override string DisplayName
+    {
+      get
+      {
+        var element = (DB.ElementType) this;
+        if (element is object)
+          return $"{element.Category?.Name} : {element.FamilyName} : {element.Name}";
+
+        switch (Id.IntegerValue)
+        {
+          case -3: return "Perpendicular To Face";
+          case -2: return "Parallel To Ground";
+        }
+
+        return base.DisplayName;
+      }
+    }
+  }
+
+  public class MullionProfile : ElementType
+  {
+    public override string TypeName => "Revit Mullion Profile";
+
+    public override string TypeDescription => "Represents a Revit Mullion Profile";
+    protected override Type ScriptVariableType => typeof(DB.ElementType);
+
+    public MullionProfile() { }
+    public MullionProfile(DB.Document doc, DB.ElementId id) : base(doc, id) { }
+
+    public override string DisplayName
+    {
+      get
+      {
+        var element = (DB.FamilySymbol) this;
+        if (element is object)
+          return $"{element.Category?.Name} : {element.FamilyName} : {element.Name}";
+
+        switch (Id.IntegerValue)
+        {
+          case -3: return "Rectangular";
+          case -2: return "Circular";
+        }
+
+        return base.DisplayName;
       }
     }
   }
