5f5aad0bc3793018dec8bfd2402169c3c58ac206
Kike <kike@mcneel.com>
2020-05-20 20:19:25 +0200

    Minor changes

--- src/RhinoInside.Revit.GH/Types/HostObject.cs
+++ src/RhinoInside.Revit.GH/Types/HostObject.cs
@@ -1,15 +1,16 @@
 using System;
+using DB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Types
 {
   public class HostObject : GeometricElement
   {
     public override string TypeDescription => "Represents a Revit host element";
-    protected override Type ScriptVariableType => typeof(Autodesk.Revit.DB.HostObject);
-    public static explicit operator Autodesk.Revit.DB.HostObject(HostObject self) =>
-      self.Document?.GetElement(self) as Autodesk.Revit.DB.HostObject;
+    protected override Type ScriptVariableType => typeof(DB.HostObject);
+    public static explicit operator DB.HostObject(HostObject self) =>
+      self.Document?.GetElement(self) as DB.HostObject;
 
     public HostObject() { }
-    public HostObject(Autodesk.Revit.DB.HostObject host) : base(host) { }
+    public HostObject(DB.HostObject host) : base(host) { }
   }
 }
