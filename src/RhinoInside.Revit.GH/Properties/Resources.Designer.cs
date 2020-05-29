f694b0b5b4dc4eab3391b1af32a4bdf0355de87f
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-05-29 12:32:05 -0700

    new icon: grid line

--- src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
+++ src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace RhinoInside.Revit.GH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap AnalyzeCurtainGridLine {
+            get {
+                object obj = ResourceManager.GetObject("AnalyzeCurtainGridLine", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap AnalyzeCurtainGridMullion {
             get {
                 object obj = ResourceManager.GetObject("AnalyzeCurtainGridMullion", resourceCulture);
