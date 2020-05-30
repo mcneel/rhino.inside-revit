2547398045db11bf28c62666dca5cb80cc884248
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-05-29 18:05:58 -0700

    new icon: comp struct, comp struct layer

--- src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
+++ src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
@@ -343,6 +343,26 @@ namespace RhinoInside.Revit.GH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap CompoundStructure_Destruct {
+            get {
+                object obj = ResourceManager.GetObject("CompoundStructure_Destruct", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap CompoundStructureLayer_Destruct {
+            get {
+                object obj = ResourceManager.GetObject("CompoundStructureLayer_Destruct", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap CurtainGridLine {
             get {
                 object obj = ResourceManager.GetObject("CurtainGridLine", resourceCulture);
