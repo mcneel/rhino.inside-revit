9783f829e8d962b710152475a3960880ecc279ca
Kike <kike@mcneel.com>
2020-06-05 19:00:55 +0200

    Fixed a bug in Types.Panel

--- src/RhinoInside.Revit.GH/Types/Panel.cs
+++ src/RhinoInside.Revit.GH/Types/Panel.cs
@@ -6,7 +6,7 @@ namespace RhinoInside.Revit.GH.Types
   public class Panel : GraphicalElement
   {
     public override string TypeDescription => "Represents a Revit Curtain Grid Panel Element";
-    protected override Type ScriptVariableType => typeof(DB.Mullion);
+    protected override Type ScriptVariableType => typeof(DB.Panel);
     public static explicit operator DB.Panel(Panel self) =>
       self.Document?.GetElement(self) as DB.Panel;
 
