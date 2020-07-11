3e47cb17e4610303c4c400cc1d5687e2be54e611
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-07-11 10:33:55 -0700

    completed model icons

--- src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
+++ src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
@@ -1733,6 +1733,16 @@ namespace RhinoInside.Revit.GH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ModelLine {
+            get {
+                object obj = ResourceManager.GetObject("ModelLine", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ModelLineByCurve {
             get {
                 object obj = ResourceManager.GetObject("ModelLineByCurve", resourceCulture);
