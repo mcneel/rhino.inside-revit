09e6b06401af68342aa87966bbc674ab1cc46d87
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-07-13 14:40:03 -0700

    updated filter icons

--- art/gh-icons/comps/make_filters.sh
+++ art/gh-icons/comps/make_filters.sh
@@ -2,3 +2,5 @@
 # generic filters
 cat filters.txt | xargs -I '{}' cp Filters.png '{}.png'
 cat filters.txt | xargs -I '{}' cp Filters.svg '{}.svg'
+cat filters_inverted.txt | xargs -I '{}' cp FiltersInverted.png '{}.png'
+cat filters_inverted.txt | xargs -I '{}' cp FiltersInverted.svg '{}.svg'
