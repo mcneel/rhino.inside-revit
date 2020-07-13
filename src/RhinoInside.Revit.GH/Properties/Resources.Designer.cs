2156b5dea619e1411ca14148227a78900e9f7c32
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-07-13 10:41:24 -0700

    added direct shape icons

--- src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
+++ src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
@@ -623,6 +623,16 @@ namespace RhinoInside.Revit.GH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap DirectShape {
+            get {
+                object obj = ResourceManager.GetObject("DirectShape", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap DirectShapeByBrep {
             get {
                 object obj = ResourceManager.GetObject("DirectShapeByBrep", resourceCulture);
