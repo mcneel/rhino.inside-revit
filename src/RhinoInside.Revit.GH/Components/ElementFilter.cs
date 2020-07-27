f6b72165c540c5fff62ed48cd0ccc1fda2e021a3
Kike <kike@mcneel.com>
2020-07-27 20:20:57 +0200

    Added 'Filter Element' component.

--- src/RhinoInside.Revit.GH/Components/ElementFilter.cs
+++ src/RhinoInside.Revit.GH/Components/ElementFilter.cs
@@ -9,6 +9,51 @@ using DB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Components
 {
+  public class FilterElement : Component
+  {
+    public override Guid ComponentGuid => new Guid("36180A9E-04CA-4B38-82FE-C6707B32C680");
+    public override GH_Exposure Exposure => GH_Exposure.primary;
+    protected override string IconTag => "Y";
+
+    public FilterElement() : base
+    (
+      name: "Filter Element",
+      nickname: "FiltElem",
+      description: "Evaluate if an Element pass a Filter",
+      category: "Revit",
+      subCategory: "Filter"
+    )
+    { }
+
+    protected override void RegisterInputParams(GH_InputParamManager manager)
+    {
+      manager.AddParameter(new Parameters.Element(), "Elements", "E", "Elements to filter", GH_ParamAccess.list);
+      manager.AddParameter(new Parameters.ElementFilter(), "Filter", "F", "Filter to apply", GH_ParamAccess.item);
+    }
+
+    protected override void RegisterOutputParams(GH_OutputParamManager manager)
+    {
+      manager.AddBooleanParameter("Pass", "P", "True if the input Element is accepted by the Filter. False if the element is rejected.", GH_ParamAccess.list);
+    }
+
+    protected override void TrySolveInstance(IGH_DataAccess DA)
+    {
+      var elements = new List<Types.Element>();
+      if (!DA.GetDataList("Elements", elements))
+        return;
+
+      var filter = default(DB.ElementFilter);
+      if (!DA.GetData("Filter", ref filter))
+        return;
+
+      var pass = new List<bool>(elements.Count);
+      foreach (var element in elements)
+        pass.Add(element.IsValid && filter.PassesFilter(element.Document, element.Id));
+
+      DA.SetDataList("Pass", pass);
+    }
+  }
+
   public abstract class ElementFilterComponent : Component
   {
     protected ElementFilterComponent(string name, string nickname, string description, string category, string subCategory)
