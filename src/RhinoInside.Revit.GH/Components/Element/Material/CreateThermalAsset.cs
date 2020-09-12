a2514112de5fb0f4a06ee820711307b00f5fffe6
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-09-11 21:43:37 -0700

    Completed skeleton of first material components

---
+++ src/RhinoInside.Revit.GH/Components/Element/Material/CreateThermalAsset.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
+using RhinoInside.Revit.External.DB;
+using DB = Autodesk.Revit.DB;
+
+namespace RhinoInside.Revit.GH.Components.Element.Material
+{
+  public class CreateThermalAsset : DocumentComponent
+  {
+    public override Guid ComponentGuid => new Guid("bd9164c4-effb-4145-bb96-006daeaeb99a");
+    public override GH_Exposure Exposure => GH_Exposure.quarternary;
+
+    protected override ParamDefinition[] Inputs => inputs;
+    static readonly ParamDefinition[] inputs =
+    {
+    };
+    protected override ParamDefinition[] Outputs => outputs;
+    static readonly ParamDefinition[] outputs =
+    {
+    };
+
+    public CreateThermalAsset() : base(
+      name: "Add Thermal Asset",
+      nickname: "C-THAST",
+      description: "Create a new instance of thermal asset inside document",
+      category: "Revit",
+      subCategory: "Material"
+    )
+    {
+    }
+
+    protected override void TrySolveInstance(IGH_DataAccess DA)
+    {
+
+    }
+  }
+}
