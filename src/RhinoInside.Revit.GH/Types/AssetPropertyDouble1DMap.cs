361cffb33e71162a7dbb6a096ab27a3c26725dae
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-09-15 20:42:23 -0700

    first draft of a the components backend

---
+++ src/RhinoInside.Revit.GH/Types/AssetPropertyDouble1DMap.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grasshopper.Kernel.Types;
+using DBX = RhinoInside.Revit.External.DB;
+using DB = Autodesk.Revit.DB;
+using Rhino.Geometry;
+using UIFramework;
+
+namespace RhinoInside.Revit.GH.Types
+{
+  public class AssetPropertyDouble1DMap : GH_Goo<DBX.AssetParameterDouble1DMap>
+  {
+    public override string TypeName => "Mappable Asset Property 1D";
+    public override string TypeDescription
+      => "Represents a double[1] property that accepts a texture map";
+    public override bool IsValid => Value != null;
+    public override sealed IGH_Goo Duplicate() => (IGH_Goo) MemberwiseClone();
+
+    public AssetPropertyDouble1DMap() { }
+    public AssetPropertyDouble1DMap(DBX.AssetParameterDouble1DMap prop): base(prop) { }
+
+    public override bool CastFrom(object source)
+    {
+      switch (source)
+      {
+        case DBX.AssetParameterDouble1DMap prop:
+          Value = prop;
+          return true;
+
+        case TextureData tdata:
+          Value = new DBX.AssetParameterDouble1DMap(tdata.Value);
+          return true;
+
+        case GH_Number number:
+          Value = new DBX.AssetParameterDouble1DMap(number.Value);
+          return true;
+      }
+
+      return base.CastFrom(source);
+    }
+
+    public override bool CastTo<Q>(ref Q target)
+    {
+      if (typeof(Q).IsAssignableFrom(typeof(DBX.AssetParameterDouble1DMap)))
+      {
+        target = (Q) (object) Value;
+        return true;
+      }
+
+      if (typeof(Q).IsAssignableFrom(typeof(GH_Number)))
+      {
+        target = (Q) (object) new GH_Number(Value.Value);
+        return true;
+      }
+
+      return base.CastTo<Q>(ref target);
+    }
+
+    public override string ToString()
+    {
+      if (!IsValid)
+        return $"Invalid {TypeName}";
+
+      return $"{Value.GetType().Name}";
+    }
+  }
+}
