0830f6c208d4431d72e66bbbebd2cc8be323c940
kike-garbo <kike.garbo@gmail.com>
2020-09-18 19:43:02 +0200

    Changed `PinUnPinElement` base class to `TransactionalComponent` to manage elements from different documents.

--- src/RhinoInside.Revit.GH/Components/Element/PinUnPinElement.cs
+++ src/RhinoInside.Revit.GH/Components/Element/PinUnPinElement.cs
@@ -1,77 +1,101 @@
 using System;
-using System.Linq;
 using Grasshopper.Kernel;
-using RhinoInside.Revit.Convert.System.Drawing;
-using DB = Autodesk.Revit.DB;
+using Grasshopper.Kernel.Parameters;
 
 namespace RhinoInside.Revit.GH.Components
 {
-  public class PinUnPinElement: TransactionComponent
+  public class PinUnPinElement: TransactionalComponent
   {
-    public override Guid ComponentGuid => new Guid("cc205221-1583-47d1-a715-226c39c3fb34");
+    public override Guid ComponentGuid => new Guid("CC205221-1583-47D1-A715-226C39C3FB34");
     public override GH_Exposure Exposure => GH_Exposure.tertiary | GH_Exposure.obscure;
-    protected override string IconTag => "PU";
+    protected override string IconTag => "PIN";
 
-    public PinUnPinElement()
-    : base(
-      name: "Pin/Unpin Element",
-      nickname: "P/UP",
+    public PinUnPinElement() : base
+    (
+      name: "Element Pin",
+      nickname: "ElemPin",
       description: "Pins or Unpins elements from Revit document",
       category: "Revit",
       subCategory: "Element"
-      )
+    )
+    { }
+
+    protected override ParamDefinition[] Inputs => inputs;
+    static readonly ParamDefinition[] inputs =
     {
+      new ParamDefinition
+      (
+        new Parameters.Element()
+        {
+          Name = "Element",
+          NickName = "E",
+          Description = "Element to access Pin",
+          Access = GH_ParamAccess.item
+        }
+      ),
+      new ParamDefinition
+      (
+        new Param_Boolean()
+        {
+          Name = "Pin",
+          NickName = "P",
+          Description = "New state for Element Pin",
+          Access = GH_ParamAccess.item,
+          Optional = true
+        },
+        ParamVisibility.Default
+      ),
+    };
 
-    }
-
-    protected override void RegisterInputParams(GH_InputParamManager manager)
+    protected override ParamDefinition[] Outputs => outputs;
+    static readonly ParamDefinition[] outputs =
     {
-      manager.AddParameter(
-        param: new Parameters.GraphicalElement(),
-        name: "Element",
-        nickname: "E",
-        description: "Element to query for its identity",
-        access: GH_ParamAccess.item
-      );
-
-      manager[manager.AddBooleanParameter(
-        name: "Pin",
-        nickname: "P",
-        description: "State of pin (defaults to false)",
-        access: GH_ParamAccess.item
-      )].Optional = true;
-    }
-
-    protected override void RegisterOutputParams(GH_OutputParamManager manager)
-    {
-      manager.AddParameter(
-        param: new Parameters.Element(),
-        name: "Element",
-        nickname: "E",
-        description: "Element to query for its identity",
-        access: GH_ParamAccess.item
-      );
-
-      manager.AddBooleanParameter(
-        name: "Pin",
-        nickname: "P",
-        description: "State of pin (defaults to false)",
-        access: GH_ParamAccess.item
-      );
-    }
+      new ParamDefinition
+      (
+        new Parameters.Element()
+        {
+          Name = "Element",
+          NickName = "E",
+          Description = "Element to access Pin",
+          Access = GH_ParamAccess.item
+        }
+      ),
+      new ParamDefinition
+      (
+        new Param_Boolean()
+        {
+          Name = "Pin",
+          NickName = "P",
+          Description = "State for Element Pin",
+          Access = GH_ParamAccess.item
+        }
+      ),
+    };
 
     protected override void TrySolveInstance(IGH_DataAccess DA)
     {
-      DB.Element element = default;
+      var element = default(Types.Element);
       if (!DA.GetData("Element", ref element))
         return;
 
-      bool state = false;
-      if(DA.GetData("Pin", ref state))
-        element.Pinned = state;
+      var _Pin_ = Params.IndexOfInputParam("Pin");
+      if (_Pin_ >= 0 && Params.Input[_Pin_].DataType != GH_ParamData.@void)
+      {
+        bool pinned = false;
+        if (!DA.GetData(_Pin_, ref pinned))
+          return;
+
+        var doc = element.Document;
+        using (var transaction = NewTransaction(doc))
+        {
+          transaction.Start();
+          element.APIElement.Pinned = pinned;
+          transaction.Commit();
+        }
+      }
 
       DA.SetData("Element", element);
-      DA.SetData("Pin", element.Pinned);
+      DA.SetData("Pin", element.APIElement.Pinned);
     }
   }
 }
