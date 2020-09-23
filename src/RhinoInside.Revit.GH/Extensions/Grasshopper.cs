d4421d7d30c47c6bfee0a9dce6be2771136ee179
Kike <kike@mcneel.com>
2020-09-23 13:32:11 +0200

    Added extension method `DuplicateAs<T>` to `IGH_Structure`.

--- src/RhinoInside.Revit.GH/Extensions/Grasshopper.cs
+++ src/RhinoInside.Revit.GH/Extensions/Grasshopper.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
 using RhinoInside.Revit.GH.Types;
 
 namespace Grasshopper.Kernel.Extensions
@@ -162,6 +164,36 @@ namespace Grasshopper.Kernel.Extensions
       };
 
       Menu_AppendConnect(param, menu, DefaultConnectMenuHandler);
+    }
+    #endregion
+
+    #region IGH_Structure
+    public static GH_Structure<T> DuplicateAs<T>(this IGH_Structure structure, bool shallowCopy)
+      where T : IGH_Goo
+    {
+      // GH_Structure<T> constructor is a bit faster if shallowCopy is true because
+      // it doesn't need to cast on each item.
+      if (structure is GH_Structure<T> structureT)
+        return new GH_Structure<T>(structureT, shallowCopy);
+
+      var result = new GH_Structure<T>();
+
+      for (int p = 0; p < structure.PathCount; ++p)
+      {
+        var path = structure.get_Path(p);
+        var srcBranch = structure.get_Branch(path);
+
+        var destBranch = result.EnsurePath(path);
+        destBranch.Capacity = srcBranch.Count;
+
+        var data = srcBranch.As<T>();
+        if (!shallowCopy)
+          data = data.Select(x => x?.Duplicate() is T t ? t : default);
+
+        destBranch.AddRange(data);
+      }
+
+      return result;
     }
     #endregion
   }
