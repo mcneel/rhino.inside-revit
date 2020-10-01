b260e4d702e559d14b278aec182b7588921022e8
kike-garbo <kike.garbo@gmail.com>
2020-10-01 19:34:46 +0200

    Shader Asset -> Appearance Asset

--- src/RhinoInside.Revit.GH/Parameters/AppearanceAsset.cs
+++ src/RhinoInside.Revit.GH/Parameters/AppearanceAsset.cs
@@ -16,9 +16,9 @@ namespace RhinoInside.Revit.GH.Parameters
     public override Guid ComponentGuid => new Guid("941b2ee3-5423-4fee-9df6-27c77fdb53c9");
 
     public AppearanceAsset() : base(
-      "Shader Asset",
+      "Appearance Asset",
       "APAST",
-      "Represents a Revit Shader (Appearance) Asset",
+      "Represents a Revit Appearance Asset",
       "Params",
       "Revit Primitives"
       ) { }
