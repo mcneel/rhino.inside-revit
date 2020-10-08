6cb3cf61ee690ef2f8dfda90c13c18e400a4e17e
kike-garbo <kike.garbo@gmail.com>
2020-10-08 11:47:21 +0200

    Fixed some comments on ElementId extension methods class.

--- src/RhinoInside.Revit/External/DB/Extensions/ElementId.cs
+++ src/RhinoInside.Revit/External/DB/Extensions/ElementId.cs
@@ -12,7 +12,7 @@ namespace RhinoInside.Revit.External.DB.Extensions
     public static bool IsBuiltInId(this ElementId id) => id is object && id <= ElementId.InvalidElementId;
 
     /// <summary>
-    /// Checks if <paramref name="id"/> corresponds to a Category in <paramref name="doc"/>/// 
+    /// Checks if <paramref name="id"/> corresponds to a <see cref="Autodesk.Revit.DB.Category"/> in <paramref name="doc"/>.
     /// </summary>
     /// <param name="id"></param>
     /// <param name="doc"></param>
@@ -30,7 +30,7 @@ namespace RhinoInside.Revit.External.DB.Extensions
     }
 
     /// <summary>
-    /// Checks if <paramref name="id"/> corresponds to a Parameter in <paramref name="doc"/>/// 
+    /// Checks if <paramref name="id"/> corresponds to a <see cref="Autodesk.Revit.DB.Parameter"/> in <paramref name="doc"/>.
     /// </summary>
     /// <param name="id"></param>
     /// <param name="doc"></param>
@@ -48,7 +48,7 @@ namespace RhinoInside.Revit.External.DB.Extensions
     }
 
     /// <summary>
-    /// Checks if <paramref name="id"/> corresponds to a ElementType in <paramref name="doc"/>
+    /// Checks if <paramref name="id"/> corresponds to an <see cref="Autodesk.Revit.DB.ElementType"/> in <paramref name="doc"/>.
     /// </summary>
     /// <param name="id"></param>
     /// <param name="doc"></param>
@@ -62,23 +62,23 @@ namespace RhinoInside.Revit.External.DB.Extensions
     }
 
     /// <summary>
-    /// Checks if id corresponds to a valid BuiltIn Category id
+    /// Checks if id corresponds to a valid <see cref="Autodesk.Revit.DB.BuiltInCategory"/> id.
     /// </summary>
     /// <param name="id"></param>
-    /// <param name="builtInParameter"></param>
+    /// <param name="builtInCategory"></param>
     /// <returns></returns>
-    public static bool TryGetBuiltInCategory(this ElementId id, out BuiltInCategory builtInParameter)
+    public static bool TryGetBuiltInCategory(this ElementId id, out BuiltInCategory builtInCategory)
     {
-      builtInParameter = (BuiltInCategory) id.IntegerValue;
-      if (builtInParameter.IsValid())
+      builtInCategory = (BuiltInCategory) id.IntegerValue;
+      if (builtInCategory.IsValid())
         return true;
 
-      builtInParameter = BuiltInCategory.INVALID;
+      builtInCategory = BuiltInCategory.INVALID;
       return false;
     }
 
     /// <summary>
-    /// Checks if id corresponds to a valid BuiltIn Parameter id
+    /// Checks if id corresponds to a valid <see cref="Autodesk.Revit.DB.BuiltInParameter"/> id.
     /// </summary>
     /// <param name="id"></param>
     /// <param name="builtInParameter"></param>
