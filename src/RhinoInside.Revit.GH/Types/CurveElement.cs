620b249d6c12c73621b6046d4542fc8cb4ff6937
Kike <kike@mcneel.com>
2020-10-16 12:44:44 +0200

    Added `Types.CurveElement.Value`

--- src/RhinoInside.Revit.GH/Types/CurveElement.cs
+++ src/RhinoInside.Revit.GH/Types/CurveElement.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Linq;
 using Grasshopper.Kernel;
-using Grasshopper.Kernel.Types;
 using Rhino.Geometry;
 using RhinoInside.Revit.Convert.Geometry;
-using RhinoInside.Revit.Convert.System.Collections.Generic;
 using DB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Types
@@ -15,8 +12,8 @@ namespace RhinoInside.Revit.GH.Types
 
     public override string TypeDescription => "Represents a Revit Curve Element";
     protected override Type ScriptVariableType => typeof(DB.CurveElement);
-    public static explicit operator DB.CurveElement(CurveElement value) =>
-      value?.IsValid == true ? value.Document.GetElement(value) as DB.CurveElement : default;
+    public static explicit operator DB.CurveElement(CurveElement value) => value?.Value;
+    public new DB.CurveElement Value => base.Value as DB.CurveElement;
 
     public CurveElement() { }
     public CurveElement(DB.CurveElement value) : base(value) { }
@@ -32,24 +29,15 @@ namespace RhinoInside.Revit.GH.Types
     #region IGH_PreviewData
     public override void DrawViewportWires(GH_PreviewWireArgs args)
     {
-      var curveElement = (DB.CurveElement) this;
-      if (curveElement is object)
-      {
-        var curve = curveElement.GeometryCurve.ToCurve();
+      if (Curve is Curve curve)
         args.Pipeline.DrawCurve(curve, args.Color, args.Thickness);
-      }
     }
 
     public override void DrawViewportMeshes(GH_PreviewMeshArgs args) { }
     #endregion
 
-    public override Curve Curve
-    {
-      get
-      {
-        var curveElement = (DB.CurveElement) this;
-        return curveElement?.GeometryCurve?.ToCurve();
-      }
-    }
+    public override Curve Curve => Value is DB.CurveElement curveElement ?
+      curveElement.GeometryCurve.ToCurve() :
+      default;
   }
 }
