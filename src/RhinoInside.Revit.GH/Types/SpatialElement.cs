4455565ca3c2b79f82d58ccdc38bb304a703fe68
Kike <kike@mcneel.com>
2020-10-16 12:25:07 +0200

    Added `Types.SpatialElement.Value`

--- src/RhinoInside.Revit.GH/Types/SpatialElement.cs
+++ src/RhinoInside.Revit.GH/Types/SpatialElement.cs
@@ -3,12 +3,12 @@ using DB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Types
 {
-  public class SpatialElement : GeometricElement
+  public class SpatialElement : InstanceElement
   {
     public override string TypeDescription => "Represents a Revit Spatial Element";
     protected override Type ScriptVariableType => typeof(DB.SpatialElement);
-    public static explicit operator DB.SpatialElement(SpatialElement value) =>
-      value?.IsValid == true ? value.Document.GetElement(value) as DB.SpatialElement : default;
+    public static explicit operator DB.SpatialElement(SpatialElement value) => value?.Value;
+    public new DB.SpatialElement Value => base.Value as DB.SpatialElement;
 
     public SpatialElement() { }
     public SpatialElement(DB.SpatialElement element) : base(element) { }
