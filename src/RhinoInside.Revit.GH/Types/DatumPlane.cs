990255d3f5162be2408d9afa0fcf33e7a54522be
Kike <kike@mcneel.com>
2020-10-16 11:40:57 +0200

    Added `Value` to `Types.DatumPlane`

--- src/RhinoInside.Revit.GH/Types/DatumPlane.cs
+++ src/RhinoInside.Revit.GH/Types/DatumPlane.cs
@@ -17,6 +17,7 @@ namespace RhinoInside.Revit.GH.Types
       value?.IsValid == true ? value.Document.GetElement(value) as DB.DatumPlane : default;
 
     public DatumPlane() { }
+    public DatumPlane(DB.Document doc, DB.ElementId id) : base(doc, id) { }
     public DatumPlane(DB.DatumPlane plane) : base(plane) { }
 
     public override string DisplayName
@@ -36,10 +37,11 @@ namespace RhinoInside.Revit.GH.Types
   {
     public override string TypeDescription => "Represents a Revit level";
     protected override Type ScriptVariableType => typeof(DB.Level);
-    public static explicit operator DB.Level(Level value) =>
-      value?.IsValid == true ? value.Document.GetElement(value) as DB.Level : default;
+    public static explicit operator DB.Level(Level value) => value?.Value;
+    public new DB.Level Value => base.Value as DB.Level;
 
     public Level() { }
+    public Level(DB.Document doc, DB.ElementId id) : base(doc, id) { }
     public Level(DB.Level level) : base(level) { }
 
     #region IGH_PreviewData
@@ -82,13 +84,13 @@ namespace RhinoInside.Revit.GH.Types
       return base.CastFrom(source);
     }
 
-    public double Elevation => (DB.Level) this is DB.Level level ? level.Elevation * Revit.ModelUnits : double.NaN;
+    public double Elevation => Value?.Elevation * Revit.ModelUnits ?? double.NaN;
 
     public override Plane Location
     {
       get
       {
-        return (DB.Level) this is DB.Level level ?
+        return Value is DB.Level level ?
         new Plane
         (
           new Point3d(0.0, 0.0, level.Elevation * Revit.ModelUnits),
@@ -107,17 +109,15 @@ namespace RhinoInside.Revit.GH.Types
 
   public class Grid : DatumPlane
   {
-
     public override string TypeDescription => "Represents a Revit grid";
     protected override Type ScriptVariableType => typeof(DB.Grid);
-    public static explicit operator DB.Grid(Grid value) =>
-      value?.IsValid == true ? value.Document.GetElement(value) as DB.Grid : default;
+    public static explicit operator DB.Grid(Grid value) => value?.Value;
+    public new DB.Grid Value => base.Value as DB.Grid;
 
     public Grid() { }
     public Grid(DB.Grid grid) : base(grid) { }
 
     #region IGH_PreviewData
-
     public override BoundingBox ClippingBox => Curve?.GetBoundingBox(false) ?? BoundingBox.Unset;
 
     public override void DrawViewportWires(GH_PreviewWireArgs args)
@@ -125,8 +125,7 @@ namespace RhinoInside.Revit.GH.Types
       var cplane = args.Viewport.ConstructionPlane();
       if (cplane.ZAxis.IsParallelTo(Vector3d.ZAxis) != 0)
       {
-        var grid = (DB.Grid) this;
-        if (grid is object)
+        if (Value is DB.Grid grid)
         {
           var points = grid.Curve?.Tessellate().ConvertAll(GeometryDecoder.ToPoint3d);
           if (points is object)
@@ -154,14 +153,6 @@ namespace RhinoInside.Revit.GH.Types
     public override void DrawViewportMeshes(GH_PreviewMeshArgs args) { }
     #endregion
 
-    public override Curve Curve
-    {
-      get
-      {
-        var grid = (DB.Grid) this;
-
-        return grid?.Curve.ToCurve();
-      }
-    }
+    public override Curve Curve => Value?.Curve.ToCurve();
   }
 }
