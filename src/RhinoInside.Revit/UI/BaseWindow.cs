6c65bb172878abacb0eba056f1b90af03b5ed9e2
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-12-09 20:04:01 -0800

    Base window updates

--- src/RhinoInside.Revit/UI/BaseWindow.cs
+++ src/RhinoInside.Revit/UI/BaseWindow.cs
@@ -19,20 +19,25 @@ namespace RhinoInside.Revit.UI
 {
   /// <summary>
   /// Rhino UI framework base window type for this addon
+  /// Current implementation is centered on Revit window and uses the generic Rhino icon
   /// </summary>
   class BaseWindow : Form
   {
     public BaseWindow(UIApplication uiApp, int width, int height)
     {
-      Resizable = false;
-      Icon = Icon.FromResource("RhinoInside.Revit.Resources.Rhino-logo.ico", assembly: Assembly.GetExecutingAssembly());
       Owner = Eto.Forms.WpfHelpers.ToEtoWindow(uiApp.MainWindowHandle);
+      Icon = Icon.FromResource("RhinoInside.Revit.Resources.Rhino-logo.ico", assembly: Assembly.GetExecutingAssembly());
+      Resizable = false;
 
       // set window size and center on the parent window
       var size = new Size(width, height);
       MinimumSize = size;
       var windowLocation = uiApp.GetChildWindowCenterLocation(size.Width, size.Height);
       Location = new Point(windowLocation.X, windowLocation.Y);
+
+      // styling
+      Padding = new Padding(10, 0, 10, 10);
+      BackgroundColor = Colors.White;
     }
   }
 }
