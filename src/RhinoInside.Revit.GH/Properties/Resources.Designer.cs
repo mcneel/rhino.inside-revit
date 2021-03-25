c057a7dd18ab730947b774d3f761ec599b1013f7
Ehsan Iran-Nejad <eirannejad@gmail.com>
2021-03-25 13:15:05 -0700

    added icons for new compound const components

--- src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
+++ src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
@@ -543,6 +543,16 @@ namespace RhinoInside.Revit.GH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap CompoundStructure_Construct {
+            get {
+                object obj = ResourceManager.GetObject("CompoundStructure_Construct", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap CompoundStructureGeometry {
             get {
                 object obj = ResourceManager.GetObject("CompoundStructureGeometry", resourceCulture);
@@ -556,6 +566,16 @@ namespace RhinoInside.Revit.GH.Properties {
         internal static System.Drawing.Bitmap CompoundStructureLayer {
             get {
                 object obj = ResourceManager.GetObject("CompoundStructureLayer", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap CompoundStructureLayer_Construct {
+            get {
+                object obj = ResourceManager.GetObject("CompoundStructureLayer_Construct", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
