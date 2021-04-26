a5e9281fffd5d3252d5c7f50679f9524f6379dc1
Ehsan Iran-Nejad <eirannejad@gmail.com>
2021-04-26 11:45:25 -0700

    added capture-release ui and icons

---
+++ src/RhinoInside.Revit/UI/Commands/Grasshopper/CommandGrasshopperReleaseElements.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using DB = Autodesk.Revit.DB;
+
+namespace RhinoInside.Revit.UI
+{
+  [Transaction(TransactionMode.Manual), Regeneration(RegenerationOption.Manual)]
+  class CommandGrasshopperReleaseElements : GrasshopperCommand
+  {
+    public static string CommandName => "Release\nElements";
+
+    public static void CreateUI(RibbonPanel ribbonPanel)
+    {
+      // Create a push button to trigger a command add it to the ribbon panel.
+      var buttonData = NewPushButtonData<CommandGrasshopperReleaseElements, AvailableWhenGHReady>
+      (
+        name: CommandName,
+        iconName: "Ribbon.Grasshopper.ReleaseElements.png",
+        tooltip: "Release elements created and tracked by Grasshopper",
+        url: "https://www.rhino3d.com/inside/revit/beta/guides/rir-grasshopper#element-tracking"
+      );
+
+      if (ribbonPanel.AddItem(buttonData) is PushButton)
+      {
+      }
+    }
+
+    public override Result Execute(ExternalCommandData data, ref string message, DB.ElementSet elements)
+    {
+      // TODO: kike ass magic here please :P
+      return Result.Succeeded;
+    }
+  }
+}
