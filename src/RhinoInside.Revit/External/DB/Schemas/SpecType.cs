e5dafbf4a7d5fbda11b7a9876b358d823d57ea0b
kike-garbo <kike.garbo@gmail.com>
2021-06-01 14:21:11 +0200

    Fixed a units conversion issue related to some more parameter types.

--- src/RhinoInside.Revit/External/DB/Schemas/SpecType.cs
+++ src/RhinoInside.Revit/External/DB/Schemas/SpecType.cs
@@ -67,43 +67,115 @@ namespace RhinoInside.Revit.External.DB.Schemas
     #region Dimensionality
     internal static readonly IReadOnlyDictionary<SpecType, int> LengthDimensionality = new Dictionary<SpecType, int>
     {
-      #region Length
+      #region Common
       { Measurable.Length, +1 },
-      { Measurable.RotationalLineSpringCoefficient, +1 },
-      { Measurable.ReinforcementLength, +1 },
+      { Measurable.Area, +2 },
+      { Measurable.Volume, +3 },
+      { Measurable.Distance, +1 },
+      { Measurable.Speed, +1 },
+      { Measurable.MassDensity, -3 },
+      { Measurable.CostPerArea, -2 },
+      { Measurable.SheetLength, -1 },
+      { Measurable.DecimalSheetLength, -1 },
+      #endregion
 
-      { Measurable.PointSpringCoefficient, -1 },
+      #region Structural
+      { Measurable.Force, +1 },
+      { Measurable.LinearForce, +1 -1 },
+      { Measurable.AreaForce, +1 -2 },
+      { Measurable.StructuralVelocity, +1 },
+      { Measurable.Moment, +1 -1 },
+      { Measurable.MomentOfInertia, +4 },
+      { Measurable.LinearMoment, +1 +1 -1 },
+      { Measurable.Stress, -1 },
+      { Measurable.UnitWeight, +1 -3 },
+      { Measurable.Weight, +1 },
+      { Measurable.WeightPerUnitLength, +1 -1 },
       { Measurable.MassPerUnitLength, -1 },
-      { Measurable.WeightPerUnitLength, -1 },
+      { Measurable.MassPerUnitArea, -2 },
+      { Measurable.PointSpringCoefficient, +1 -1 },
+      { Measurable.LineSpringCoefficient, +1 -2 },
+      { Measurable.AreaSpringCoefficient, +1 -3 },
+      { Measurable.RotationalPointSpringCoefficient, +1 +1 },
+      { Measurable.RotationalLineSpringCoefficient, +1 +1 -1 },
+      { Measurable.Displacement, +1 },
+      { Measurable.StructuralVelocity, +1 },
+      { Measurable.Acceleration, +1 },
+      { Measurable.Energy, +2 },
+      { Measurable.ReinforcementLength, +1 },
+      { Measurable.ReinforcementArea, +2 },
+      { Measurable.ReinforcementVolume, +3 },
+      { Measurable.ReinforcementAreaPerUnitLength, +2 -1 },
+      { Measurable.ReinforcementSpacing, +1 },
+      { Measurable.ReinforcementCover, +1 },
+      { Measurable.BarDiameter, +1 },
+      { Measurable.CrackWidth, +1 },
+      { Measurable.SectionDimension, +1 },
+      { Measurable.SectionProperty, +1 },
+      { Measurable.SectionArea, +2 },
+      { Measurable.SectionModulus, +3 },
+      { Measurable.WarpingConstant, +6 },
+      { Measurable.SurfaceAreaPerUnitLength, +2 -1 },
+      #endregion
+
+      #region HVAC
+      { Measurable.HvacEnergy, +2 },
+      { Measurable.HvacDensity, -3 },
+      { Measurable.HvacFriction, -1 -1 },
+      { Measurable.HvacPowerDensity, -2 },
+      { Measurable.HvacPressure, -1 },
+      { Measurable.HvacVelocity, +1 },
+      { Measurable.AirFlow, +3 },
+      { Measurable.DuctSize, +1 },
+      { Measurable.CrossSection, +2 },
+      { Measurable.HvacRoughness, +1 },
+      { Measurable.HvacViscosity, -1 },
+      { Measurable.AirFlowDensity, +3 -2 },
+      { Measurable.CoolingLoadDividedByArea, -2 },
+      { Measurable.HeatingLoadDividedByArea, -2 },
+      { Measurable.CoolingLoadDividedByVolume, -3 },
+      { Measurable.HeatingLoadDividedByVolume, -3 },
+      { Measurable.AirFlowDividedByVolume, +3 -3 },
+      { Measurable.AirFlowDividedByCoolingLoad, +3 },
+      { Measurable.AreaDividedByCoolingLoad, +2 },
+      { Measurable.AreaDividedByHeatingLoad, +2 },
+      { Measurable.DuctInsulationThickness, +1 },
+      { Measurable.DuctLiningThickness, +1 },
+      #endregion
+
+      #region Electrical
+      { Measurable.Luminance, -2 },
+      { Measurable.ElectricalPowerDensity, -2 },
+      { Measurable.ElectricalResistivity, +1 },
+      { Measurable.WireDiameter, +1 },
+      { Measurable.CableTraySize, +1 },
+      { Measurable.ConduitSize, +1 },
+      #endregion
+
+      #region Piping
+      { Measurable.PipingDensity, -3 },
+      { Measurable.Flow, +3 },
+      { Measurable.PipingFriction, -1 +1 },
+      { Measurable.PipingPressure, -1 },
+      { Measurable.PipingVelocity, +1 },
+      { Measurable.PipingViscosity, -1 },
+      { Measurable.PipeSize, +1 },
+      { Measurable.PipingRoughness, +1 },
+      { Measurable.PipingVolume, +3 },
+      { Measurable.PipeInsulationThickness, +1 },
+      { Measurable.PipeDimension, +1 },
+      { Measurable.PipingMass, 0 },
       { Measurable.PipeMassPerUnitLength, -1 },
       #endregion
 
-      #region Area
-      { Measurable.Area, +2 },
-      { Measurable.AreaForce, +2 },
-      { Measurable.AreaDividedByCoolingLoad, +2 },
-      { Measurable.AreaDividedByHeatingLoad, +2 },
-      { Measurable.SurfaceAreaPerUnitLength, +2 -1 },
-      { Measurable.ReinforcementAreaPerUnitLength, +2 -1 },
-      { Measurable.ReinforcementArea, +2 },
-      { Measurable.SectionArea, +2 },
-      { Measurable.RotationalPointSpringCoefficient, +2 },
-
-      { Measurable.LineSpringCoefficient, -2 },
-      { Measurable.CoolingLoadDividedByArea, -2 },
-      { Measurable.HeatingLoadDividedByArea, -2 },
-      { Measurable.MassPerUnitArea, -2 },
-      #endregion
-
-      #region Volume
-      { Measurable.Volume, +3 },
-      { Measurable.PipingVolume, +3 },
-      { Measurable.ReinforcementVolume, +3 },
-
-      { Measurable.AreaSpringCoefficient, -3 },
-      { Measurable.CoolingLoadDividedByVolume, -3 },
-      { Measurable.HeatingLoadDividedByVolume, -3 },
-      { Measurable.AirFlowDividedByVolume, -3 },
+      #region Energy
+      { Measurable.HeatTransferCoefficient, -2 },
+      { Measurable.ThermalResistance, +2 },
+      { Measurable.ThermalMass, +2 },
+      { Measurable.ThermalConductivity, -1 },
+      { Measurable.SpecificHeat, +2 },
+      { Measurable.SpecificHeatOfVaporization, +2 },
+      { Measurable.Permeability, +1 -2 },
       #endregion
     };
 
