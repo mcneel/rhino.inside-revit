0c759d19edff51e0ec4e53b5f8980ccc9c83c55e
kike-garbo <kike.garbo@gmail.com>
2021-06-24 12:24:59 +0200

    Added 'BuiltInParameterGroups.cs'

--- src/RhinoInside.Revit.GH/Parameters/Input/BuiltInParameterGroups.cs
+++ src/RhinoInside.Revit.GH/Parameters/Input/BuiltInParameterGroups.cs
@@ -3,20 +3,20 @@ using Grasshopper.Kernel;
 
 namespace RhinoInside.Revit.GH.Parameters.Input
 {
-  public class BuiltInParameterByName : Grasshopper.Special.ValueSet<Types.ParameterId>
+  public class BuiltInParameterGroups : Grasshopper.Special.ValueSet<Types.ParameterGroup>
   {
-    public override Guid ComponentGuid => new Guid("C1D96F56-F53C-4DFC-8090-EC2050BDBB66");
+    public override Guid ComponentGuid => new Guid("5D331B12-DA6C-46A7-AA13-F463E42650D1");
     public override GH_Exposure Exposure => GH_Exposure.primary | GH_Exposure.obscure;
 
     protected override System.Drawing.Bitmap Icon =>
       ((System.Drawing.Bitmap) Properties.Resources.ResourceManager.GetObject(GetType().Name)) ??
       base.Icon;
 
-    public BuiltInParameterByName() : base
+    public BuiltInParameterGroups() : base
     (
-      name: "Built-In Parameters",
-      nickname: "Parameters",
-      description: "Provides a picker for built-in parameters",
+      name: "Built-In Parameter Groups",
+      nickname: "Parameter Groups",
+      description: "Provides a picker for built-in parameter Groups",
       category: "Revit",
       subcategory: "Parameter"
     )
@@ -29,7 +29,7 @@ namespace RhinoInside.Revit.GH.Parameters.Input
       if (SourceCount == 0)
       {
         m_data.Clear();
-        m_data.AppendRange(Types.ParameterId.EnumValues);
+        m_data.AppendRange(Types.ParameterGroup.EnumValues);
       }
 
       base.LoadVolatileData();
