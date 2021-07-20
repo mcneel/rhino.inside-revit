0195db3987826431292cafc267080e24910aea7b
kike-garbo <kike.garbo@gmail.com>
2021-07-20 17:10:21 +0200

    Added Fix for Revit 2018

--- src/RhinoInside.Revit/External/DB/Extensions/ParameterFilterElement.cs
+++ src/RhinoInside.Revit/External/DB/Extensions/ParameterFilterElement.cs
@@ -6,17 +6,22 @@ namespace RhinoInside.Revit.External.DB.Extensions
   public static class ParameterFilterElementExtension
   {
 #if !REVIT_2019
-    public static ElementFilter GetElementFilter(this ParameterFilterElement parameterFilter)
+    public static ElementFilter GetElementFilter(this ParameterFilterElement self)
     {
       var filters = new List<ElementFilter>()
       {
-        new ElementMulticategoryFilter(parameterFilter.GetCategories())
+        new ElementMulticategoryFilter(self.GetCategories())
       };
 
-      foreach(var rule in parameterFilter.GetRules())
+      foreach(var rule in self.GetRules())
         filters.Add(new ElementParameterFilter(rule));
 
       return new LogicalAndFilter(filters);
+    }
+
+    public static bool SetElementFilter(this ParameterFilterElement self, ElementFilter elementFilter)
+    {
+      return false;
     }
 #endif
   }
