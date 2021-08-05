02eca04dedf38c576e5293bea36a1ba866e5f983
Ehsan Iran-Nejad <eirannejad@gmail.com>
2021-08-05 14:40:23 -0700

    added phase param icon

--- src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
+++ src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
@@ -2263,6 +2263,16 @@ namespace RhinoInside.Revit.GH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Phase {
+            get {
+                object obj = ResourceManager.GetObject("Phase", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap PinUnPinElement {
             get {
                 object obj = ResourceManager.GetObject("PinUnPinElement", resourceCulture);
