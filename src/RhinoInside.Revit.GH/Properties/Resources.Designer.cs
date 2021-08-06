cf8c72ad4352306a8497072ce630abd9d08361c5
Ehsan Iran-Nejad <eirannejad@gmail.com>
2021-08-06 08:25:41 -0700

    added shared-param comp icon

--- src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
+++ src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
@@ -2543,6 +2543,16 @@ namespace RhinoInside.Revit.GH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap SharedParameters {
+            get {
+                object obj = ResourceManager.GetObject("SharedParameters", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap SiteLocationIdentity {
             get {
                 object obj = ResourceManager.GetObject("SiteLocationIdentity", resourceCulture);
