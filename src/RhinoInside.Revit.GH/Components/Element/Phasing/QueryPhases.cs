1987a24020e939ad95cd615a89bff11a5b7939b3
kike-garbo <kike.garbo@gmail.com>
2021-08-16 16:33:59 +0200

    Closes #457
    - Implement Picker on the parameter.
    - 'Query Phases'.
    - 'Element Phasing' to get-set Phase parameters.

---
+++ src/RhinoInside.Revit.GH/Components/Element/Phasing/QueryPhases.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Linq;
+using System.Windows.Forms;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
+using DB = Autodesk.Revit.DB;
+
+namespace RhinoInside.Revit.GH.Components.Phasing
+{
+  public class QueryPhases : ElementCollectorComponent
+  {
+    public override Guid ComponentGuid => new Guid("91E4D3E1-883A-44D9-A3D2-B836967869E1");
+    public override GH_Exposure Exposure => GH_Exposure.tertiary;
+    protected override string IconTag => "Q";
+
+    protected override DB.ElementFilter ElementFilter => new DB.ElementClassFilter(typeof(DB.Phase));
+
+    #region UI
+    protected override void AppendAdditionalComponentMenuItems(ToolStripDropDown menu)
+    {
+      base.AppendAdditionalComponentMenuItems(menu);
+
+      var activeApp = Revit.ActiveUIApplication;
+      var commandId = Autodesk.Revit.UI.RevitCommandId.LookupPostableCommandId(Autodesk.Revit.UI.PostableCommand.Phases);
+      Menu_AppendItem
+      (
+        menu, $"Open Phases…",
+        (sender, arg) => External.UI.EditScope.PostCommand(activeApp, commandId),
+        activeApp.CanPostCommand(commandId), false
+      );
+    }
+    #endregion
+
+    public QueryPhases() : base
+    (
+      name: "Query Phases",
+      nickname: "Phases",
+      description: "Get document construction phases list",
+      category: "Revit",
+      subCategory: "Document"
+    )
+    { }
+
+    protected override ParamDefinition[] Inputs => inputs;
+    static readonly ParamDefinition[] inputs =
+    {
+      new ParamDefinition(new Parameters.Document(), ParamRelevance.Occasional),
+      ParamDefinition.Create<Param_String>("Name", "N", "Phase name", GH_ParamAccess.item, optional: true),
+      ParamDefinition.Create<Parameters.ElementFilter>("Filter", "F", "Filter", GH_ParamAccess.item, optional: true, relevance: ParamRelevance.Occasional),
+    };
+    protected override ParamDefinition[] Outputs => outputs;
+    static readonly ParamDefinition[] outputs =
+    {
+      ParamDefinition.Create<Parameters.Phase>("Phases", "P", "Phases list", GH_ParamAccess.list)
+    };
+
+    protected override void TrySolveInstance(IGH_DataAccess DA)
+    {
+      if (!Parameters.Document.GetDataOrDefault(this, DA, "Document", out var doc))
+        return;
+
+      if (!Params.TryGetData(DA, "Name", out string name)) return;
+      if (!Params.TryGetData(DA, "Filter", out DB.ElementFilter filter, x => x.IsValidObject)) return;
+
+      using (var collector = new DB.FilteredElementCollector(doc))
+      {
+        var phasesCollector = collector.WherePasses(ElementFilter);
+
+        if (filter is object)
+          phasesCollector = phasesCollector.WherePasses(filter);
+
+        if (name is object && TryGetFilterStringParam(DB.BuiltInParameter.PHASE_NAME, ref name, out var nameFilter))
+          phasesCollector = phasesCollector.WherePasses(nameFilter);
+
+        var options = collector.Cast<DB.Phase>();
+
+        if (name is object)
+          options = options.Where(x => x.get_Parameter(DB.BuiltInParameter.PHASE_NAME).AsString().IsSymbolNameLike(name));
+
+        DA.SetDataList("Phases", options.Select(x => new Types.Phase(x)));
+      }
+    }
+  }
+}
--- src/RhinoInside.Revit.GH/Parameters/Phase.cs
+++ src/RhinoInside.Revit.GH/Parameters/Phase.cs
@@ -13,7 +13,7 @@ namespace RhinoInside.Revit.GH.Parameters
     public override GH_Exposure Exposure => GH_Exposure.quarternary;
     public override Guid ComponentGuid => new Guid("353FFB47-46D6-4FEE-8DBD-40E683416531");
 
-    public Phase() : base("Phase", "Phase", "Represents a Revit document construction phase.", "Params", "Revit Primitives") { }
+    public Phase() : base("Phase", "Phase", "Contains a collection of Revit construction phase elements", "Params", "Revit Primitives") { }
 
     #region UI
     public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
@@ -28,6 +28,64 @@ namespace RhinoInside.Revit.GH.Parameters
         (sender, arg) => External.UI.EditScope.PostCommand(activeApp, commandId),
         activeApp.CanPostCommand(commandId), false
       );
+    }
+
+    protected override void Menu_AppendPromptOne(ToolStripDropDown menu)
+    {
+      if (SourceCount != 0) return;
+      if (Revit.ActiveUIDocument?.Document is null) return;
+
+      var listBox = new ListBox
+      {
+        Sorted = true,
+        BorderStyle = BorderStyle.FixedSingle,
+        Width = (int) (200 * GH_GraphicsUtil.UiScale),
+        Height = (int) (100 * GH_GraphicsUtil.UiScale)
+      };
+      listBox.SelectedIndexChanged += ListBox_SelectedIndexChanged;
+
+      RefreshPhasesList(listBox);
+
+      Menu_AppendCustomItem(menu, listBox);
+    }
+
+    private void RefreshPhasesList(ListBox listBox)
+    {
+      var doc = Revit.ActiveUIDocument.Document;
+
+      listBox.SelectedIndexChanged -= ListBox_SelectedIndexChanged;
+      listBox.Items.Clear();
+      listBox.Items.Add(new Types.Phase());
+
+      {
+        var phases = doc.Phases.Cast<DB.Phase>();
+
+        listBox.DisplayMember = "DisplayName";
+        foreach (var phase in phases)
+          listBox.Items.Add(new Types.Phase(phase));
+      }
+
+      listBox.SelectedIndex = listBox.Items.Cast<Types.Phase>().IndexOf(PersistentValue, 0).FirstOr(-1);
+      listBox.SelectedIndexChanged += ListBox_SelectedIndexChanged;
+    }
+
+    private void ListBox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      if (sender is ListBox listBox)
+      {
+        if (listBox.SelectedIndex != -1)
+        {
+          if (listBox.Items[listBox.SelectedIndex] is Types.Phase value)
+          {
+            RecordPersistentDataEvent($"Set: {value}");
+            PersistentData.Clear();
+            PersistentData.Append(value);
+            OnObjectChanged(GH_ObjectEventType.PersistentData);
+          }
+        }
+
+        ExpireSolution(true);
+      }
     }
     #endregion
   }
