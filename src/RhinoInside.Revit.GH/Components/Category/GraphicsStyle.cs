bf50756156ce95c120c6c2af2b43895577b9f386
kike-garbo <kike.garbo@gmail.com>
2021-08-18 18:11:55 +0200

    Closes #422 Added 'Query Line Styles' component.

--- src/RhinoInside.Revit.GH/Components/Category/GraphicsStyle.cs
+++ src/RhinoInside.Revit.GH/Components/Category/GraphicsStyle.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
 using DB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Components
@@ -23,8 +24,8 @@ namespace RhinoInside.Revit.GH.Components
 
     protected override void RegisterOutputParams(GH_OutputParamManager manager)
     {
-      manager.AddParameter(new Parameters.GraphicsStyle(), "Projection", "P", string.Empty, GH_ParamAccess.list);
-      manager.AddParameter(new Parameters.GraphicsStyle(), "Cut", "C", string.Empty, GH_ParamAccess.list);
+      manager.AddParameter(new Parameters.GraphicsStyle(), "Projection", "P", string.Empty, GH_ParamAccess.item);
+      manager.AddParameter(new Parameters.GraphicsStyle(), "Cut", "C", string.Empty, GH_ParamAccess.item);
     }
 
     protected override void TrySolveInstance(IGH_DataAccess DA)
@@ -35,6 +36,64 @@ namespace RhinoInside.Revit.GH.Components
 
       DA.SetData("Projection", category?.GetGraphicsStyle(DB.GraphicsStyleType.Projection));
       DA.SetData("Cut", category?.GetGraphicsStyle(DB.GraphicsStyleType.Cut));
+    }
+  }
+
+  public class QueryLineStyles : ElementCollectorComponent
+  {
+    public override Guid ComponentGuid => new Guid("54082395-7160-4563-B289-215AFDD33A7F");
+    public override GH_Exposure Exposure => GH_Exposure.secondary;
+
+    protected override DB.ElementFilter ElementFilter => new DB.ElementClassFilter(typeof(DB.GraphicsStyle));
+
+    public QueryLineStyles() : base
+    (
+      name: "Query Line Styles",
+      nickname: "Line Styles",
+      description: "Get document line styles list",
+      category: "Revit",
+      subCategory: "Model"
+    )
+    { }
+
+    protected override ParamDefinition[] Inputs => inputs;
+    static readonly ParamDefinition[] inputs =
+    {
+      new ParamDefinition (new Parameters.Document(), ParamRelevance.Occasional),
+      ParamDefinition.Create<Param_String>("Name", "N", "Line style name", GH_ParamAccess.item, optional: true),
+      ParamDefinition.Create<Parameters.ElementFilter>("Filter", "F", "Filter", GH_ParamAccess.item, optional: true, relevance: ParamRelevance.Occasional)
+    };
+
+    protected override ParamDefinition[] Outputs => outputs;
+    static readonly ParamDefinition[] outputs =
+    {
+      ParamDefinition.Create<Parameters.GraphicsStyle>("Styles", "S", "Line styles list", GH_ParamAccess.list)
+    };
+
+    protected override void TrySolveInstance(IGH_DataAccess DA)
+    {
+      if (!Parameters.Document.GetDataOrDefault(this, DA, "Document", out var doc))
+        return;
+
+      string name = null;
+      DA.GetData("Name", ref name);
+
+      Params.TryGetData(DA, "Filter", out DB.ElementFilter filter);
+
+      using (var categories = doc.Settings.Categories)
+      {
+        var styles = categories.
+          get_Item(DB.BuiltInCategory.OST_Lines).SubCategories.Cast<DB.Category>().
+          Select(x => x.GetGraphicsStyle(DB.GraphicsStyleType.Projection));
+
+        if (filter is object)
+          styles = styles.Where(x => filter.PassesFilter(x));
+
+        if (name is object)
+          styles = styles.Where(x => x.Name == name);
+
+        DA.SetDataList("Styles", styles);
+      }
     }
   }
 }
