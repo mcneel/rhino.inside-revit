4871d0715026d9ba05e2712f6049db435a8f593d
Ehsan Iran-Nejad <eirannejad@gmail.com>
2021-09-08 15:15:16 -0700

    Corrected misspelled icon name in resources and art files

--- src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
+++ src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
@@ -1983,9 +1983,9 @@ namespace RhinoInside.Revit.GH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap GraphicStyle {
+        internal static System.Drawing.Bitmap GraphicsStyle {
             get {
-                object obj = ResourceManager.GetObject("GraphicStyle", resourceCulture);
+                object obj = ResourceManager.GetObject("GraphicsStyle", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
