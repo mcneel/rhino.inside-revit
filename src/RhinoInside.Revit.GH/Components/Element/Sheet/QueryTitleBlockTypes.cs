1b52645795abe35c1e4f2b5e4ae91baf23029009
Ehsan Iran-Nejad <eirannejad@gmail.com>
2021-10-07 15:46:23 -0700

    titleblock selector is now a picker component

--- src/RhinoInside.Revit.GH/Components/Element/Sheet/QueryTitleBlockTypes.cs
+++ src/RhinoInside.Revit.GH/Components/Element/Sheet/QueryTitleBlockTypes.cs
@@ -6,10 +6,11 @@ using DB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Components.Element.Sheet
 {
+  [Obsolete("Since v1.2")]
   public class QueryTitleBlockTypes : ElementCollectorComponent
   {
     public override Guid ComponentGuid => new Guid("c7a57ec8-d4d3-4251-aa91-cc67f833313b");
-    public override GH_Exposure Exposure => GH_Exposure.primary;
+    public override GH_Exposure Exposure => GH_Exposure.hidden;
     protected override DB.ElementFilter ElementFilter => new DB.ElementCategoryFilter(DB.BuiltInCategory.OST_TitleBlocks);
 
     public QueryTitleBlockTypes() : base
