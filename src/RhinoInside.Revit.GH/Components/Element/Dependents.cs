86a0216ff442d5d96e0b53e0b909e996f82b07c9
kike-garbo <kike.garbo@gmail.com>
2021-11-15 19:07:14 +0100

    Updated 'Element Dependents' to return original 'Elements' and also 'Referentials'

--- src/RhinoInside.Revit.GH/Components/Element/Dependents.cs
+++ src/RhinoInside.Revit.GH/Components/Element/Dependents.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Linq;
 using Grasshopper.Kernel;
+using RhinoInside.Revit.External.DB;
 using RhinoInside.Revit.External.DB.Extensions;
 using DB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Components
 {
-  public class ElementDependents : Component
+  [ComponentVersion(introduced: "1.0", updated: "1.4")]
+  public class ElementDependents : ZuiComponent
   {
     public override Guid ComponentGuid => new Guid("97D71AA8-6987-45B9-8F25-B92671E20EF4");
     public override GH_Exposure Exposure => GH_Exposure.tertiary | GH_Exposure.obscure;
@@ -21,29 +23,107 @@ namespace RhinoInside.Revit.GH.Components
       subCategory: "Element")
     { }
 
-    protected override void RegisterInputParams(GH_InputParamManager manager)
+    protected override ParamDefinition[] Inputs => inputs;
+    static readonly ParamDefinition[] inputs =
     {
-      manager.AddParameter(new Parameters.Element(), "Element", "E", string.Empty, GH_ParamAccess.item);
-      manager[manager.AddParameter(new Parameters.ElementFilter(), "Filter", "F", "Filter that will be applied to dependant elements", GH_ParamAccess.item)].Optional = true;
-    }
+      new ParamDefinition
+      (
+        new Parameters.Element()
+        {
+          Name = "Element",
+          NickName = "E",
+        }
+      ),
+      new ParamDefinition
+      (
+        new Parameters.ElementFilter()
+        {
+          Name = "Filter",
+          NickName = "F",
+          Description = "Filter that will be applied to dependant elements",
+          Optional = true
+        },
+        ParamRelevance.Primary
+      ),
+    };
 
-    protected override void RegisterOutputParams(GH_OutputParamManager manager)
+    protected override ParamDefinition[] Outputs => outputs;
+    static readonly ParamDefinition[] outputs =
     {
-      manager.AddParameter(new Parameters.Element(), "Elements", "E", "Dependent elements. From a logical point of view, are the children of this Element", GH_ParamAccess.list);
-    }
+      new ParamDefinition
+      (
+        new Parameters.Element()
+        {
+          Name = "Element",
+          NickName = "E",
+          Description = "Input element",
+          DataMapping = GH_DataMapping.Graft
+        },
+        ParamRelevance.Occasional
+      ),
+      new ParamDefinition
+      (
+        new Parameters.Element()
+        {
+          Name = "Dependents",
+          NickName = "E",
+          Description = "Set of elements that from a logical point of view, are the children of input Element",
+          Access = GH_ParamAccess.list
+        }
+      ),
+      new ParamDefinition
+      (
+        new Parameters.Element()
+        {
+          Name = "Referentials",
+          NickName = "R",
+          Description = "Set of elements that directly or indirectly reference input Element",
+          Access = GH_ParamAccess.list
+        },
+        ParamRelevance.Occasional
+      ),
+    };
 
     protected override void TrySolveInstance(IGH_DataAccess DA)
     {
-      DB.Element element = null;
-      if (!DA.GetData("Element", ref element))
-        return;
+      if (!Params.GetData(DA, "Element", out Types.Element element)) return;
+      Params.GetData(DA, "Filter", out DB.ElementFilter filter);
 
-      DB.ElementFilter filter = null;
-      DA.GetData("Filter", ref filter);
+      filter = CompoundElementFilter.Intersect
+      (
+        new DB.ExclusionFilter(new DB.ElementId[] { element.Id }),
+        filter ?? CompoundElementFilter.Union
+        (
+          CompoundElementFilter.ElementIsNotInternalFilter(element.Document),
+          new DB.ElementClassFilter(typeof(DB.ExtensibleStorage.DataStorage))
+        )
+      );
 
-      var elements = element.GetDependentElements(filter);
+      Params.TrySetData(DA, "Element", () => element);
+      if (Params.IndexOfOutputParam("Referentials") >= 0)
+      {
+        try
+        {
+          var dependents = element.Document.GetDependentElements
+          (
+            new DB.ElementId[] { element.Id },
+            out var relateds,
+            filter
+          );
 
-      DA.SetDataList("Elements", elements.Where(x => x != element.Id).Select(x => Types.Element.FromElementId(element.Document, x)));
+          DA.SetDataList("Dependents", dependents.Select(x => Types.Element.FromElementId(element.Document, x)));
+          DA.SetDataList("Referentials", relateds.Select(x => Types.Element.FromElementId(element.Document, x)));
+
+          return;
+        }
+        catch (Autodesk.Revit.Exceptions.ArgumentException e)
+        { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, $"{e.Message} {element.Id.IntegerValue}"); }
+      }
+
+      {
+        var dependents = element.Value.GetDependentElements(filter);
+        DA.SetDataList("Dependents", dependents.Select(x => Types.Element.FromElementId(element.Document, x)));
+      }
     }
   }
 }
