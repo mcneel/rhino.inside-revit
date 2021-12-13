e314972bd52bb672c6ec3b70242ef6eb3561fb1b
Ehsan Iran-Nejad <eirannejad@gmail.com>
2021-12-12 16:11:01 -0800

    updated publish script

--- script/publish_docs.sh
+++ script/publish_docs.sh
@@ -23,29 +23,9 @@ fi
 # change to docs output directory
 cd "$1"
 
-# set optional commit description
-if [ $2 ]; then
-  GIT_COMMIT_DESC="Generated from $2"
-fi
-
-# check branch is set
-# this is set on github repo secrets
-if [ -z $DOCS_GIT_BRANCH ]; then
-  >&2 echo "DOCS_GIT_BRANCH is required"
-  exit 1
-fi
-
-echo "Publishing docs from $(pwd) to github"
-
-# print config
-echo
-echo "==> Config"
-echo "GIT_BRANCH=$DOCS_GIT_BRANCH"
-echo "GIT_COMMIT_DESC=$GIT_COMMIT_DESC"
-
 # git
 echo
-echo "==> Settin up git access"
+echo "==> Setting up git access..."
 
 # set up git user
 # git config user.name ?
@@ -59,11 +39,11 @@ git add -A . > /dev/null
 git status -s
 
 # commit new docs
-git commit -m "Publish!" -m "$GIT_COMMIT_DESC" || exit 0
+git commit -m "Publish!" -m "Generated from $2" || exit 0
 git status -s
 
 echo
-echo "==> Publish!"
+echo "==> Publishing..."
 
 # push new docs (skip if nothing committed above)
 # git push origin HEAD:$GIT_BRANCH
