15b8b64221d95f82860b11f058028fd667506b72
kike-garbo <kike.garbo@gmail.com>
2022-02-24 18:42:39 +0100

    Added `Rhino.Geometry.Surface.CreateTrimmedSurface` extension method.

--- src/RhinoInside.Revit.External/Extensions/RhinoCommon.cs
+++ src/RhinoInside.Revit.External/Extensions/RhinoCommon.cs
@@ -723,6 +723,251 @@ namespace Rhino.Geometry
     }
   }
 
+  static class SurfaceExtension
+  {
+    struct BrepBoundary
+    {
+      public BrepLoopType type;
+      public List<BrepEdge> edges;
+      public PolyCurve trims;
+      public List<int> orientation;
+    }
+
+    static int AddSurface(Brep brep, Surface surface, Curve[] loops, out List<BrepBoundary>[] shells, double tolerance)
+    {
+      // Extract base surface
+      if (surface is object)
+      {
+        var trimTolerance = tolerance * 0.1;
+
+        int si = brep.AddSurface(surface);
+
+        if (surface is PlaneSurface)
+        {
+          var nurbs = surface.ToNurbsSurface();
+          nurbs.KnotsU.InsertKnot(surface.Domain(0).Mid);
+          nurbs.KnotsV.InsertKnot(surface.Domain(1).Mid);
+          surface = nurbs;
+        }
+
+        // Classify Loops
+        var nesting = new int[loops.Length];
+        var edgeLoops = new BrepBoundary[loops.Length];
+        {
+          var trims = new Curve[loops.Length];
+
+          int index = 0;
+          foreach (var loop in loops)
+          {
+            if (loop is PolyCurve polycurve)
+            {
+              var trim = new PolyCurve();
+              for (int s = 0; s < polycurve.SegmentCount; s++)
+              {
+                var segment = polycurve.SegmentCurve(s);
+                var trimSegment = surface.Pullback(segment, trimTolerance);
+                trim.AppendSegment(trimSegment);
+              }
+
+              trims[index++] = trim;
+            }
+            else trims[index++] = surface.Pullback(loop, trimTolerance);
+          }
+
+          for (int i = 0; i < edgeLoops.Length; ++i)
+          {
+            for (int j = i + 1; j < edgeLoops.Length; ++j)
+            {
+              var containment = Curve.PlanarClosedCurveRelationship(trims[i], trims[j], Plane.WorldXY, tolerance);
+              if (containment == RegionContainment.MutualIntersection)
+              {
+                edgeLoops[i].type = BrepLoopType.Outer;
+                edgeLoops[j].type = BrepLoopType.Outer;
+              }
+              else if (containment == RegionContainment.AInsideB)
+              {
+                nesting[i]++;
+              }
+              else if (containment == RegionContainment.BInsideA)
+              {
+                nesting[j]++;
+              }
+            }
+          }
+
+          // Fix orientation if necessary
+          index = 0;
+          foreach (var loop in loops)
+          {
+            // Ignore intersecting loops
+            if (edgeLoops[index].type == BrepLoopType.Unknown)
+            {
+              if (nesting[index] % 2 != 0)
+                edgeLoops[index].type = BrepLoopType.Inner;
+              else
+                edgeLoops[index].type = BrepLoopType.Outer;
+            }
+
+            switch (trims[index].ClosedCurveOrientation())
+            {
+              case CurveOrientation.Undefined:
+                break;
+              case CurveOrientation.CounterClockwise:
+                if (edgeLoops[index].type == BrepLoopType.Inner) loops[index].Reverse(); break;
+              case CurveOrientation.Clockwise:
+                if (edgeLoops[index].type == BrepLoopType.Outer) loops[index].Reverse(); break;
+            }
+
+            ++index;
+          }
+        }
+
+        // Create Brep Edges and compute trims
+        {
+          int index = 0;
+          foreach (var edgeLoop in loops)
+          {
+            // Ignore unclasified loops
+            if (edgeLoops[index].type == BrepLoopType.Unknown)
+              continue;
+
+            var kinks = new List<double>();
+            {
+              var domain = edgeLoop.Domain;
+              var t = domain.Min;
+              while (edgeLoop.GetNextDiscontinuity(Continuity.C1_locus_continuous, t, domain.Max, out t))
+                kinks.Add(t);
+            }
+
+            var edges = kinks.Count > 0 ? edgeLoop.Split(kinks) : new Curve[] { edgeLoop };
+
+            edgeLoops[index].edges = new List<BrepEdge>();
+            edgeLoops[index].trims = new PolyCurve();
+            edgeLoops[index].orientation = new List<int>();
+
+            foreach (var edge in edges)
+            {
+              var brepEdge = default(BrepEdge);
+              brepEdge = brep.Edges.Add(brep.AddEdgeCurve(edge));
+
+              edgeLoops[index].edges.Add(brepEdge);
+              var segment = edge;
+
+              edgeLoops[index].orientation.Add(segment.TangentAt(segment.Domain.Mid).IsParallelTo(brepEdge.TangentAt(brepEdge.Domain.Mid)));
+
+              var trim = surface.Pullback(segment, trimTolerance);
+              edgeLoops[index].trims.Append(trim);
+            }
+
+            edgeLoops[index].trims.MakeClosed(tolerance);
+
+            ++index;
+          }
+        }
+
+        // Sort edgeLoops in nesting orther, shallow loops first
+        Array.Sort(nesting, edgeLoops);
+
+        var outerLoops = edgeLoops.Where(x => x.type == BrepLoopType.Outer);
+        var innerLoops = edgeLoops.Where(x => x.type == BrepLoopType.Inner);
+
+        // Group Edge loops in shells with the outer loop as the first one
+        shells = outerLoops.
+                 Select(x => new List<BrepBoundary>() { x }).
+                 ToArray();
+
+        if (shells.Length == 1)
+        {
+          shells[0].AddRange(innerLoops);
+        }
+        else
+        {
+          // Iterate in reverse order to found deeper loops before others
+          foreach (var innerLoop in innerLoops.Reverse())
+          {
+            foreach (var shell in shells.Reverse())
+            {
+              var containment = Curve.PlanarClosedCurveRelationship(innerLoop.trims, shell[0].trims, Plane.WorldXY, tolerance);
+              if (containment == RegionContainment.AInsideB)
+              {
+                shell.Add(innerLoop);
+                break;
+              }
+            }
+          }
+        }
+
+        return si;
+      }
+
+      shells = default;
+      return -1;
+    }
+
+    static void TrimSurface(Brep brep, int surface, bool orientationIsReversed, IEnumerable<IEnumerable<BrepBoundary>> shells)
+    {
+      foreach (var shell in shells)
+      {
+        var brepFace = brep.Faces.Add(surface);
+        brepFace.OrientationIsReversed = orientationIsReversed;
+
+        foreach (var loop in shell)
+        {
+          var brepLoop = brep.Loops.Add(loop.type, brepFace);
+
+          var edgeCount = loop.edges.Count;
+          for (int e = 0; e < edgeCount; ++e)
+          {
+            var brepEdge = loop.edges[e];
+
+            int orientation = loop.orientation[e];
+            if (orientation == 0)
+              continue;
+
+            if (loop.trims.SegmentCurve(e) is Curve trim)
+            {
+              var ti = brep.AddTrimCurve(trim);
+              brep.Trims.Add(brepEdge, orientation < 0, brepLoop, ti);
+            }
+          }
+
+          brep.Trims.MatchEnds(brepLoop);
+        }
+      }
+    }
+
+    public static Brep CreateTrimmedSurface(this Surface surface, Curve[] loops, double tolerance)
+    {
+      var brep = new Brep();
+
+      // Set surface
+      var si = AddSurface(brep, surface, loops, out var shells, tolerance);
+      if (si < 0)
+        return null;
+
+      // Set edges & trims
+      TrimSurface(brep, si, false, shells);
+
+      // Set vertices
+      brep.SetVertices();
+
+      // Set flags
+      brep.SetTolerancesBoxesAndFlags
+      (
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true
+      );
+
+      return brep;
+    }
+  }
+
   static class GeometryBaseExtension
   {
     public static bool IsNullOrEmpty(this GeometryBase geometry)
