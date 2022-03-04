bb8cb8d22c7847880f4ccb34bbd69ed153a4c2fa
kike-garbo <kike.garbo@gmail.com>
2022-03-04 17:21:25 +0100

    Added `CompoundElementFilter.ElementClassFilter` method.

--- src/RhinoInside.Revit.External/DB/Extensions/FilteredElementCollector.cs
+++ src/RhinoInside.Revit.External/DB/Extensions/FilteredElementCollector.cs
@@ -39,11 +39,7 @@ namespace RhinoInside.Revit.External.DB.Extensions
 
     public static FilteredElementCollector WhereElementIsKindOf(this FilteredElementCollector collector, Type type)
     {
-      if (type == typeof(Element))
-        return collector.WhereElementIsNotElementType();
-      
-      return (typeof(ElementType).IsAssignableFrom(type) ? collector.WhereElementIsElementType() : collector).
-        WherePasses(ElementExtension.CreateElementClassFilter(type));
+      return type == typeof(Element) ? collector : collector.WherePasses(CompoundElementFilter.ElementClassFilter(type));
     }
 
     public static FilteredElementCollector WhereCategoryIdEqualsTo(this FilteredElementCollector collector, ElementId value)
@@ -78,15 +74,19 @@ namespace RhinoInside.Revit.External.DB.Extensions
 
     public static FilteredElementCollector WhereParameterEqualsTo(this FilteredElementCollector collector, BuiltInParameter paramId, string value, bool caseSensitive = true)
     {
+      if (value is null) return collector;
+
       using (var provider = new ParameterValueProvider(new ElementId(paramId)))
       using (var evaluator = new FilterStringEquals())
-      using (var rule = new FilterStringRule(provider, evaluator, value ?? "", caseSensitive))
+      using (var rule = new FilterStringRule(provider, evaluator, value, caseSensitive))
       using (var filter = new ElementParameterFilter(rule))
         return collector.WherePasses(filter);
     }
 
     public static FilteredElementCollector WhereParameterEqualsTo(this FilteredElementCollector collector, BuiltInParameter paramId, ElementId value)
     {
+      if (value is null) return collector;
+
       using (var provider = new ParameterValueProvider(new ElementId(paramId)))
       using (var evaluator = new FilterNumericEquals())
       using (var rule = new FilterElementIdRule(provider, evaluator, value))
