0408385f539fd6869d415cb55f4b2da2a727fa7b
Pedro <36660222+pedrocortesark@users.noreply.github.com>
2022-04-04 13:25:53 +0200

    Created SpatialElementGeometry

---
+++ src/RhinoInside.Revit.GH/Parameters/Input/SpatialElementBoundaryLocation_ValueList.cs
@@ -0,0 +1,30 @@
+using System;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Special;
+using ARDB = Autodesk.Revit.DB;
+
+namespace RhinoInside.Revit.GH.Parameters.Input
+{
+  [ComponentVersion(introduced: "1.7")]
+  public class SpatialElementBoundaryLocation_ValueList : GH_ValueList
+  {
+    public override Guid ComponentGuid => new Guid("53FDAB6F-B2F5-447D-BBB2-623B05866569");
+    public override GH_Exposure Exposure => GH_Exposure.tertiary;
+
+    public SpatialElementBoundaryLocation_ValueList()
+    {
+      Category = "Revit";
+      SubCategory = "Input";
+      Name = "Spatial Element Boundary Location Line";
+      NickName = "BLL";
+      Description = "Picker for spatial element boundary location line options";
+
+      ListItems.Clear();
+
+      ListItems.Add(new GH_ValueListItem("Finish", ((int) ARDB.SpatialElementBoundaryLocation.Finish).ToString()));
+      ListItems.Add(new GH_ValueListItem("Center", ((int) ARDB.SpatialElementBoundaryLocation.Center).ToString()));
+      ListItems.Add(new GH_ValueListItem("CoreBoundary", ((int) ARDB.SpatialElementBoundaryLocation.CoreBoundary).ToString()));
+      ListItems.Add(new GH_ValueListItem("CoreCenter", ((int) ARDB.SpatialElementBoundaryLocation.CoreCenter).ToString()));
+    }
+  }
+}
