acbc9b3effbd87aea4428b9c5fa28094a3f8aeaa
Kike <kike@mcneel.com>
2022-04-12 11:05:58 +0200

    Revit 2017

--- src/RhinoInside.Revit.External/DB/Extensions/Surface.cs
+++ src/RhinoInside.Revit.External/DB/Extensions/Surface.cs
@@ -13,14 +13,5 @@ namespace RhinoInside.Revit.External.DB.Extensions
       return true;
 #endif
     }
-
-#if !REVIT_2018
-    public static void Project(this Plane plane, XYZ point, out UV uv, out double distance)
-    {
-      var v = point - plane.Origin;
-      uv = new UV(v.DotProduct(plane.XVec), v.DotProduct(plane.YVec));
-      distance = plane.Evaluate(uv).DistanceTo(point);
-    }
-#endif
   }
 }
