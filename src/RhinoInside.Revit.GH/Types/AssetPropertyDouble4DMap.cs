06d58d67f2cb4969b4828c2439ad26aa25f591c9
kike-garbo <kike.garbo@gmail.com>
2022-04-12 17:30:46 +0200

    Fixed two casting to `TextureData`

--- src/RhinoInside.Revit.GH/Types/AssetPropertyDouble4DMap.cs
+++ src/RhinoInside.Revit.GH/Types/AssetPropertyDouble4DMap.cs
@@ -72,7 +72,7 @@ namespace RhinoInside.Revit.GH.Types
 
       if (typeof(Q).IsAssignableFrom(typeof(MAT.TextureData)))
       {
-        target = (Q) (object) Value;
+        target = (Q) (object) Value.TextureValue;
         return true;
       }
 
