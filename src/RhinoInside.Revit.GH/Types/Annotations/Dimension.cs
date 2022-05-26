a6221632e4766cdb6fe473e41897416f533f27b5
Kike <kike@mcneel.com>
2022-05-26 15:37:07 +0200

    Implemented `Types.Dimension.DrawViewportWires`.

--- src/RhinoInside.Revit.GH/Types/Annotations/Dimension.cs
+++ src/RhinoInside.Revit.GH/Types/Annotations/Dimension.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Linq;
 using Rhino.Geometry;
 using ARDB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Types
 {
   using Convert.Geometry;
+  using Grasshopper.Kernel;
+  using RhinoInside.Revit.External.DB.Extensions;
 
   [Kernel.Attributes.Name("Dimension")]
   public class Dimension : GraphicalElement
@@ -22,16 +25,27 @@ namespace RhinoInside.Revit.GH.Types
       {
         if (Value is ARDB.Dimension dimension && dimension.Curve is ARDB.Curve curve)
         {
-          if (curve.Project(dimension.Origin) is ARDB.IntersectionResult result)
+          try
           {
-            var transform = curve.ComputeDerivatives(result.Parameter, false);
-            var origin = transform.Origin.ToPoint3d();
-            var xAxis = transform.BasisX.ToVector3d();
-            var yAxis = transform.BasisY.ToVector3d();
-            if (yAxis.IsZero) yAxis = xAxis.PerpVector();
+            if (!curve.IsBound)
+            {
+              var segments = dimension.Segments.Cast<ARDB.DimensionSegment>().Where(x => x.Value.HasValue).ToArray();
+              if (segments.Length > 0)
+              {
+                var first = segments.First();
+                var start = curve.Project(first.Origin);
 
-            return new Plane(origin, xAxis, yAxis);
+                var last = segments.Last();
+                var end = curve.Project(last.Origin);
+
+                curve.MakeBound(start.Parameter - first.Value.Value * 0.5, end.Parameter + last.Value.Value * 0.5);
+              }
+            }
+
+            if(curve.TryGetLocation(out var origin, out var basisX, out var basisY))
+              return new Plane(origin.ToPoint3d(), basisX.ToVector3d(), basisY.ToVector3d());
           }
+          catch { }
         }
 
         return base.Location;
@@ -42,27 +56,111 @@ namespace RhinoInside.Revit.GH.Types
     {
       get
       {
-        if (Value is ARDB.Dimension dimension && dimension.Curve is ARDB.Curve curve)
+        if (Value is ARDB.Dimension dimension)
         {
-          try
+          if (dimension.Curve is ARDB.Curve curve)
           {
-            if (!curve.IsBound && dimension.Value.HasValue)
+            try
             {
-              if (dimension.Curve.Project(dimension.Origin) is ARDB.IntersectionResult result)
+              if (!curve.IsBound)
               {
-                var startParameter = dimension.Value.Value * -0.5;
-                var endParameter = dimension.Value.Value * +0.5;
-                curve.MakeBound(result.Parameter + startParameter, result.Parameter + endParameter);
+                var segments = dimension.Segments.Cast<ARDB.DimensionSegment>().Where(x => x.Value.HasValue).ToArray();
+                if (segments.Length > 0)
+                {
+                  var first = segments.First();
+                  var start = curve.Project(first.Origin);
+
+                  var last = segments.Last();
+                  var end = curve.Project(last.Origin);
+
+                  curve.MakeBound(start.Parameter - first.Value.Value * 0.5, end.Parameter + last.Value.Value * 0.5);
+                }
+                else if (dimension.Value.HasValue)
+                {
+                  if (dimension.Curve.Project(dimension.Origin) is ARDB.IntersectionResult result)
+                  {
+                    var startParameter = dimension.Value.Value * -0.5;
+                    var endParameter = dimension.Value.Value * +0.5;
+                    curve.MakeBound(result.Parameter + startParameter, result.Parameter + endParameter);
+                  }
+                }
               }
             }
+            catch { }
+
+            return curve.ToCurve();
           }
-          catch { }
-
-          return curve.ToCurve();
-
         }
 
         return default;
+      }
+    }
+
+    static string FormatValue(double value, ARDB.DimensionShape dimensionShape)
+    {
+      switch (dimensionShape)
+      {
+        case ARDB.DimensionShape.Angular:
+          return $"{Rhino.RhinoMath.ToDegrees(value):G2}°";
+
+        case ARDB.DimensionShape.Radial:
+          return $"R {GH_Format.FormatDouble(GeometryDecoder.ToModelLength(value))} {GH_Format.RhinoUnitSymbol()}";
+
+        case ARDB.DimensionShape.Diameter:
+          return $"⌀ {GH_Format.FormatDouble(GeometryDecoder.ToModelLength(value))} {GH_Format.RhinoUnitSymbol()}";
+      }
+
+      return $"{GH_Format.FormatDouble(GeometryDecoder.ToModelLength(value))} {GH_Format.RhinoUnitSymbol()}";
+    }
+
+    public override void DrawViewportWires(GH_PreviewWireArgs args)
+    {
+      if (Value is ARDB.Dimension dimension)
+      {
+        if (dimension.NumberOfSegments > 0)
+        {
+          var segments = dimension.Segments.Cast<ARDB.DimensionSegment>().Where(x => x.Value.HasValue).ToArray();
+          foreach (var segment in segments)
+          {
+            if (!segment.Value.HasValue) continue;
+
+            var dimCurve = dimension.Curve;
+            if (dimCurve.Project(segment.Origin) is ARDB.IntersectionResult result)
+            {
+              var startParameter = segment.Value.Value * -0.5;
+              var endParameter   = segment.Value.Value * +0.5;
+              dimCurve.MakeBound(result.Parameter + startParameter, result.Parameter + endParameter);
+
+              var curve = dimCurve.ToCurve();
+              args.Pipeline.DrawCurve(curve, args.Color, args.Thickness);
+              args.Pipeline.DrawArrowHead(curve.PointAtStart, -curve.TangentAtStart, args.Color, 16, 0.0);
+              args.Pipeline.DrawArrowHead(curve.PointAtEnd, curve.TangentAtEnd, args.Color, 16, 0.0);
+            }
+
+            var text = FormatValue(segment.Value.Value, dimension.DimensionShape);
+            args.Pipeline.DrawDot(segment.TextPosition.ToPoint3d(), text, args.Color, System.Drawing.Color.White);
+          }
+        }
+        else 
+        {
+          if (Curve is Curve curve)
+          {
+            args.Pipeline.DrawCurve(curve, args.Color, args.Thickness);
+            args.Pipeline.DrawArrowHead(curve.PointAtStart, -curve.TangentAtStart, args.Color, 16, 0.0);
+            args.Pipeline.DrawArrowHead(curve.PointAtEnd, curve.TangentAtEnd, args.Color, 16, 0.0);
+          }
+
+          if (dimension.Value.HasValue)
+          {
+            var text = FormatValue(dimension.Value.Value, dimension.DimensionShape);
+            args.Pipeline.DrawDot(dimension.TextPosition.ToPoint3d(), text, args.Color, System.Drawing.Color.White);
+          }
+          else if (dimension is ARDB.SpotDimension spot)
+          {
+            var text = FormatValue(dimension.Origin.Z, dimension.DimensionShape);
+            args.Pipeline.DrawDot(dimension.Origin.ToPoint3d(), text, args.Color, System.Drawing.Color.White);
+          }
+        }
       }
     }
   }
