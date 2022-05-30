41a4466b118c346be4c84c23c0feea1961fed247
kike-garbo <kike.garbo@gmail.com>
2022-05-30 13:01:59 +0200

    Added `Types.TextElement` type.

---
+++ src/RhinoInside.Revit.GH/Types/Annotations/TextElement.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using Rhino.Geometry;
+using ARDB = Autodesk.Revit.DB;
+
+namespace RhinoInside.Revit.GH.Types
+{
+  using Convert.Geometry;
+  using Grasshopper.Kernel;
+  using RhinoInside.Revit.External.DB.Extensions;
+
+  [Kernel.Attributes.Name("Text Element")]
+  public class TextElement : GraphicalElement
+  {
+    protected override Type ValueType => typeof(ARDB.TextElement);
+    public new ARDB.TextElement Value => base.Value as ARDB.TextElement;
+
+    public TextElement() { }
+    public TextElement(ARDB.TextElement element) : base(element) { }
+
+    public override Plane Location
+    {
+      get
+      {
+        if (Value is ARDB.TextElement text)
+        {
+          return new Plane
+          (
+            text.Coord.ToPoint3d(),
+            text.BaseDirection.ToVector3d(),
+            text.UpDirection.ToVector3d()
+          );
+        }
+
+        return base.Location;
+      }
+    }
+  }
+}
