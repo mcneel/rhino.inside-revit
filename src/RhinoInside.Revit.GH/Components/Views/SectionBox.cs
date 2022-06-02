06a9d23d6004fee10e6fe7cbc5ed9c52f59b9702
kike-garbo <kike.garbo@gmail.com>
2022-06-02 10:14:16 +0200

    Fixed Views namespace.

--- src/RhinoInside.Revit.GH/Components/Views/SectionBox.cs
+++ src/RhinoInside.Revit.GH/Components/Views/SectionBox.cs
@@ -8,7 +8,7 @@ using RhinoInside.Revit.Convert.Geometry;
 using RhinoInside.Revit.External.DB.Extensions;
 using ARDB = Autodesk.Revit.DB;
 
-namespace RhinoInside.Revit.GH.Components.View
+namespace RhinoInside.Revit.GH.Components.Views
 {
   [ComponentVersion(introduced: "1.7")]
   public class ViewSectionBox : TransactionalChainComponent
