6f8fecc6341b52dd8b33ef78fe9d7127130d1beb
Kike <kike@mcneel.com>
2022-06-28 13:00:12 +0200

    Renamed 'Group Elements' to 'Group Members'

--- src/RhinoInside.Revit.GH/Components/Model/Group/GroupElements.cs
+++ src/RhinoInside.Revit.GH/Components/Model/Group/GroupElements.cs
@@ -4,37 +4,46 @@ using Grasshopper.Kernel;
 
 namespace RhinoInside.Revit.GH.Components.ModelElements
 {
-  public class GroupElements : Component
+  [ComponentVersion(introduced: "1.0", updated: "1.9")]
+  public class GroupElements : ZuiComponent
   {
     public override Guid ComponentGuid => new Guid("7C7D3739-7609-4F7F-BAB5-1E3648508891");
     public override GH_Exposure Exposure => GH_Exposure.tertiary;
 
     public GroupElements() : base
     (
-      name: "Group Elements",
+      name: "Group Members",
       nickname: "Group",
-      description: "Get group elements list",
+      description: "Get group members list",
       category: "Revit",
       subCategory: "Model"
     )
     { }
 
-    protected override void RegisterInputParams(GH_InputParamManager manager)
+    protected override ParamDefinition[] Inputs => inputs;
+    static readonly ParamDefinition[] inputs =
     {
-      manager.AddParameter(new Parameters.Group(), "Group", "G", "Group to query", GH_ParamAccess.item);
-    }
-    protected override void RegisterOutputParams(GH_OutputParamManager manager)
+      ParamDefinition.Create<Parameters.Group>("Group", "G", "Group to query", GH_ParamAccess.item)
+    };
+
+    protected override ParamDefinition[] Outputs => outputs;
+    static readonly ParamDefinition[] outputs =
     {
-      manager.AddParameter(new Parameters.Element(), "Elements", "E", "Group Elements", GH_ParamAccess.list);
+      ParamDefinition.Create<Parameters.GraphicalElement>("Members", "M", "Group members", GH_ParamAccess.list)
+    };
+
+    public override void AddedToDocument(GH_Document document)
+    {
+      if (Params.Output<IGH_Param>("Elements") is IGH_Param elements)
+        elements.Name = "Members";
+
+      base.AddedToDocument(document);
     }
 
     protected override void TrySolveInstance(IGH_DataAccess DA)
     {
-      var group = default(Types.Group);
-      if (!DA.GetData("Group", ref group) || !group.IsValid)
-        return;
-
-      DA.SetDataList("Elements", group.Value.GetMemberIds().Select(x => Types.Element.FromElementId(group.Document, x)));
+      if (!Params.GetData(DA, "Group", out Types.Group group, x => x.IsValid)) return;
+      DA.SetDataList("Members", group.Value.GetMemberIds().Select(x => Types.GraphicalElement.FromElementId(group.Document, x)));
     }
   }
 }
