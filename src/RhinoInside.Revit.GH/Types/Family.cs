4f67076ac72853b78697adc54957f2adabe924c9
kike-garbo <kike.garbo@gmail.com>
2022-07-13 00:13:55 +0200

    Moved `Types.CombinableElement` from Family.cs to CombinableElement.cs

--- src/RhinoInside.Revit.GH/Types/Family.cs
+++ src/RhinoInside.Revit.GH/Types/Family.cs
@@ -47,39 +47,4 @@ namespace RhinoInside.Revit.GH.Types
       return base.CastFrom(source);
     }
   }
-
-  [Kernel.Attributes.Name("Form Element")]
-  public class CombinableElement : GeometricElement, IGH_InstanceElement
-  {
-    protected override Type ValueType => typeof(ARDB.CombinableElement);
-    public new ARDB.CombinableElement Value => base.Value as ARDB.CombinableElement;
-
-    public CombinableElement() { }
-    public CombinableElement(ARDB.CombinableElement element) : base(element) { }
-
-    #region Category
-    public override Category Category
-    {
-      set
-      {
-        if (value is object && Value is ARDB.Element element)
-        {
-          using (var parameter = element.get_Parameter(ARDB.BuiltInParameter.FAMILY_ELEM_SUBCATEGORY))
-          {
-            if (parameter is null)
-            {
-              if (value.Id != ARDB.ElementId.InvalidElementId)
-                throw new Exceptions.RuntimeErrorException($"{((IGH_Goo) this).TypeName} '{DisplayName}' does not support assignment of a Category.");
-            }
-            else
-            {
-              AssertValidDocument(value, nameof(Category));
-              parameter.Update(value);
-            }
-          }
-        }
-      }
-    }
-    #endregion
-  }
 }
