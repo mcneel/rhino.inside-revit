3904547e520f561683fc4f945f278b6e83cbf860
Kike <kike@mcneel.com>
2022-07-14 14:21:49 +0200

    Improved 'Analyze Curtain Wall' on models with a lot of walls.

--- src/RhinoInside.Revit.GH/Components/Element/CurtainWall/AnalyzeCurtainWall.cs
+++ src/RhinoInside.Revit.GH/Components/Element/CurtainWall/AnalyzeCurtainWall.cs
@@ -1,53 +1,58 @@
 using System;
 using Grasshopper.Kernel;
-
 using ARDB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Components.Walls
 {
+  using External.DB.Extensions;
+
   public class AnalyzeCurtainWall : Component
   {
     public override Guid ComponentGuid => new Guid("734B2DAC-1CD2-4D51-B7BD-D3D377CF62DE");
     public override GH_Exposure Exposure => GH_Exposure.secondary;
     protected override string IconTag => "ACW";
 
-    public AnalyzeCurtainWall() : base(
+    public AnalyzeCurtainWall() : base
+    (
       name: "Analyze Curtain Wall",
       nickname: "A-CW",
       description: "Analyze given Curtain Wall element",
       category: "Revit",
       subCategory: "Wall"
     )
-    {
-    }
+    { }
 
     protected override void RegisterInputParams(GH_InputParamManager manager)
     {
-      manager.AddParameter(
+      manager.AddParameter
+      (
         param: new Parameters.Wall(),
         name: "Curtain Wall",
         nickname: "CW",
         description: "Curtain Wall element",
         access: GH_ParamAccess.item
-        );
+      );
     }
 
     protected override void RegisterOutputParams(GH_OutputParamManager manager)
     {
-      manager.AddParameter(
+      manager.AddParameter
+      (
         param: new Parameters.CurtainGrid(),
         name: "Curtain Grid",
         nickname: "CG",
         description: "Curtain Grid definition associated with input Curtain Wall",
         access: GH_ParamAccess.item
-        );
-      manager.AddParameter(
+      );
+
+      manager.AddParameter
+      (
         param: new Parameters.Wall(),
         name: "Host Wall",
         nickname: "HW",
         description: "Host Wall instance is the input Curtain Wall is embedded",
         access: GH_ParamAccess.item
-        );
+      );
     }
 
     protected override void TrySolveInstance(IGH_DataAccess DA)
@@ -64,17 +69,15 @@ namespace RhinoInside.Revit.GH.Components.Walls
 
         // determine if curtain wall is embeded in another wall
         // find all the wall elements that are intersecting the bbox of this wall
-        var bbox = wallInstance.get_BoundingBox(null);
-        var outline = new ARDB.Outline(bbox.Min, bbox.Max);
-        var bbf = new ARDB.BoundingBoxIntersectsFilter(outline);
-        var walls = new ARDB.FilteredElementCollector(wallInstance.Document).WherePasses(bbf).OfClass(typeof(ARDB.Wall)).ToElements();
+        var bbf = new ARDB.BoundingBoxIntersectsFilter(wallInstance.GetOutline());
+        var walls = new ARDB.FilteredElementCollector(wallInstance.Document).OfClass(typeof(ARDB.Wall)).WherePasses(bbf);
         // ask for embedded wall inserts from these instances
         foreach (ARDB.Wall wall in walls)
         {
           var embeddedWalls = wall.FindInserts(addRectOpenings: false, includeShadows: false, includeEmbeddedWalls: true, includeSharedEmbeddedInserts: false);
           if (embeddedWalls.Contains(wallInstance.Id))
           {
-            DA.SetData("Host Wall", Types.Element.FromElement(wall));
+            DA.SetData("Host Wall", Types.Wall.FromElement(wall));
             break;
           }
         }
