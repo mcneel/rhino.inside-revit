a6fa3c53b40e89a94432351c98231745f8a36a72
Ehsan Iran-Nejad <eirannejad@gmail.com>
2022-07-18 10:19:53 -0700

    added detail line icon

--- src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
+++ src/RhinoInside.Revit.GH/Properties/Resources.Designer.cs
@@ -163,6 +163,16 @@ namespace RhinoInside.Revit.GH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap AddDetailLine {
+            get {
+                object obj = ResourceManager.GetObject("AddDetailLine", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap AddDetailView {
             get {
                 object obj = ResourceManager.GetObject("AddDetailView", resourceCulture);
