faae5b6fe085738a1f089ccdadba1c35328cfa55
kike-garbo <kike.garbo@gmail.com>
2022-11-12 19:23:23 +0100

    Added some error checking to `ARDB.DatumPlane.GetSketchPlane`.

--- src/RhinoInside.Revit.External/DB/Extensions/Level.cs
+++ src/RhinoInside.Revit.External/DB/Extensions/Level.cs
@@ -110,7 +110,7 @@ namespace RhinoInside.Revit.External.DB.Extensions
 
   public static class DatumPlaneExtension
   {
-    internal static PlaneEquation GetPlaneEquation(this DatumPlane datum)
+    static PlaneEquation GetPlaneEquation(this DatumPlane datum)
     {
       switch (datum)
       {
@@ -140,12 +140,16 @@ namespace RhinoInside.Revit.External.DB.Extensions
 
     public static SketchPlane GetSketchPlane(this DatumPlane datum, bool ensureSketchPlane = false)
     {
+      if (ensureSketchPlane && !datum.Document.IsModifiable)
+        throw new InvalidOperationException($"There is no started transaction in course on {nameof(datum)} document.");
+
       using (var collector = new FilteredElementCollector(datum.Document).OfClass(typeof(SketchPlane)))
       {
         var minDistance = double.PositiveInfinity;
         var closestSketchPlane = default(SketchPlane);
         var comparer = GeometryObjectEqualityComparer.Default;
         var datumEquation = GetPlaneEquation(datum);
+        var datumNormal = datumEquation.Normal;
         var datuName = datum.Name;
 
         foreach (var sketchPlane in collector.Cast<SketchPlane>())
@@ -156,7 +160,7 @@ namespace RhinoInside.Revit.External.DB.Extensions
           {
             var equation = new PlaneEquation(plane.Origin, plane.Normal);
 
-            if (!comparer.Equals(equation.Normal, datumEquation.Normal))
+            if (!comparer.Equals(equation.Normal, datumNormal))
               continue;
 
             var distance = Math.Abs(equation.D - datumEquation.D);
@@ -172,10 +176,7 @@ namespace RhinoInside.Revit.External.DB.Extensions
           return closestSketchPlane;
       }
 
-      if (ensureSketchPlane)
-        return SketchPlane.Create(datum.Document, datum.Id);
-
-      return default;
+      return ensureSketchPlane ? SketchPlane.Create(datum.Document, datum.Id) : default;
     }
   }
 }
