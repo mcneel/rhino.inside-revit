8a3ec31a8d85f457aa3ce5eede3c5667a29111c0
kike-garbo <kike.garbo@gmail.com>
2022-11-12 21:11:45 +0100

    Moved `Types.ElementId.Proxy` functionality to `Types.Element.Proxy`

--- src/RhinoInside.Revit.GH/Types/Element.cs
+++ src/RhinoInside.Revit.GH/Types/Element.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -506,6 +507,7 @@ namespace RhinoInside.Revit.GH.Types
       return false;
     }
 
+    [TypeConverter(typeof(Proxy.ObjectConverter))]
     protected new class Proxy : ElementId.Proxy
     {
       protected new Element owner => base.owner as Element;
@@ -517,15 +519,115 @@ namespace RhinoInside.Revit.GH.Types
         return owner.DisplayName;
       }
 
-      [System.ComponentModel.Description("The element identifier in this session.")]
-      [System.ComponentModel.RefreshProperties(System.ComponentModel.RefreshProperties.All)]
+      protected virtual bool IsValidId(ARDB.Document doc, ARDB.ElementId id) =>
+        owner.GetType() == Element.FromElementId(doc, id).GetType();
+
+      [Description("Element is built in Revit.")]
+      public bool IsBuiltIn => owner.IsReferencedData && owner.Id.IsBuiltInId();
+
+      [Description("The element identifier in this session.")]
+      [RefreshProperties(RefreshProperties.All)]
       public virtual int? Id
       {
         get => owner.Id?.ToValue();
       }
 
-      [System.ComponentModel.Description("A human readable name for the Element.")]
+      [Description("A human readable name for the Element.")]
       public string Name => owner.Nomen;
+
+      class ObjectConverter : ExpandableObjectConverter
+      {
+        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes)
+        {
+          var properties = base.GetProperties(context, value, attributes);
+          if (value is Proxy proxy && proxy.Valid)
+          {
+            var element = proxy.owner.Document?.GetElement(proxy.owner.Id);
+            if (element is object)
+            {
+              var parameters = element.GetParameters(External.DB.ParameterClass.Any).
+                Select(p => new ParameterPropertyDescriptor(p)).
+                ToArray();
+
+              var descriptors = new PropertyDescriptor[properties.Count + parameters.Length];
+              properties.CopyTo(descriptors, 0);
+              parameters.CopyTo(descriptors, properties.Count);
+
+              return new PropertyDescriptorCollection(descriptors, true);
+            }
+          }
+
+          return properties;
+        }
+      }
+
+      private class ParameterPropertyDescriptor : PropertyDescriptor
+      {
+        readonly ARDB.Parameter parameter;
+        public ParameterPropertyDescriptor(ARDB.Parameter p) : base(p.Definition?.Name ?? p.Id.ToValue().ToString(), null) { parameter = p; }
+        public override Type ComponentType => typeof(Proxy);
+        public override bool IsReadOnly => true;
+        public override string Name => parameter.Definition?.Name ?? string.Empty;
+        public override string Category => parameter.Definition is null ? string.Empty : ARDB.LabelUtils.GetLabelFor(parameter.Definition.ParameterGroup);
+        public override string Description
+        {
+          get
+          {
+            var description = string.Empty;
+            if (parameter.Definition is ARDB.Definition definition)
+            {
+              External.DB.Schemas.DataType dataType = definition.GetDataType();
+              description = dataType.Label.ToLower();
+
+              if (string.IsNullOrEmpty(description))
+                description = parameter.StorageType.ToString();
+            }
+
+            string parameterClass = "Unknown";
+            if (parameter.Id.IsBuiltInId()) parameterClass = "Built-in";
+            else if (parameter.IsShared) parameterClass = "Shared";
+            else parameterClass = "Project";
+
+            if (parameter.IsReadOnly)
+              description = "read only " + description;
+
+            description = $"{parameterClass} {description} parameter.{Environment.NewLine}{Environment.NewLine}";
+            description += $"ParameterId : {((External.DB.Schemas.ParameterId) parameter.GetTypeId()).FullName}";
+
+            if (parameter.Id.TryGetBuiltInParameter(out var builtInParameter))
+              description += $"{Environment.NewLine}BuiltInParameter : {builtInParameter.ToStringGeneric()}";
+
+            return description;
+          }
+        }
+        public override bool Equals(object obj)
+        {
+          if (obj is ParameterPropertyDescriptor other)
+            return other.parameter.Id == parameter.Id;
+
+          return false;
+        }
+        public override int GetHashCode() => parameter.Id.GetHashCode();
+        public override bool ShouldSerializeValue(object component) { return false; }
+        public override void ResetValue(object component) { }
+        public override bool CanResetValue(object component) { return false; }
+        public override void SetValue(object component, object value) { }
+        public override Type PropertyType => typeof(string);
+        public override object GetValue(object component)
+        {
+          if (parameter.Element is object && parameter.Definition is object)
+          {
+            if (parameter.StorageType == ARDB.StorageType.String)
+              return parameter.AsString();
+
+            return parameter.Element.GetParameterFormatOptions(parameter.Id) is ARDB.FormatOptions options ?
+              parameter.AsValueString(options) :
+              parameter.AsValueString();
+          }
+
+          return null;
+        }
+      }
     }
 
     public override IGH_GooProxy EmitProxy() => new Proxy(this);
--- src/RhinoInside.Revit.GH/Types/ElementId.cs
+++ src/RhinoInside.Revit.GH/Types/ElementId.cs
@@ -1,7 +1,6 @@
 using System;
 using System.ComponentModel;
 using System.Drawing;
-using System.Linq;
 using Grasshopper.Kernel.Types;
 using Grasshopper.Special;
 using ARDB = Autodesk.Revit.DB;
@@ -101,7 +100,6 @@ namespace RhinoInside.Revit.GH.Types
       return false;
     }
 
-    [TypeConverter(typeof(Proxy.ObjectConverter))]
     protected class Proxy : IGH_GooProxy
     {
       protected readonly ElementId owner;
@@ -126,109 +124,10 @@ namespace RhinoInside.Revit.GH.Types
       [Description("The Guid of document this element belongs to.")]
       public Guid DocumentGUID => owner.DocumentGUID;
 
-      protected virtual bool IsValidId(ARDB.Document doc, ARDB.ElementId id) =>
-        owner.GetType() == Element.FromElementId(doc, id).GetType();
-
       [Description("A stable unique identifier for an element within the document.")]
       public string UniqueID => owner.UniqueID;
       [Description("API Object Type.")]
       public virtual Type ObjectType => owner.Value?.GetType();
-      [Description("Element is built in Revit.")]
-      public bool IsBuiltIn => owner.IsReferencedData && owner.Id.IsBuiltInId();
-
-      class ObjectConverter : ExpandableObjectConverter
-      {
-        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes)
-        {
-          var properties = base.GetProperties(context, value, attributes);
-          if (value is Proxy proxy && proxy.Valid)
-          {
-            var element = proxy.owner.Document?.GetElement(proxy.owner.Id);
-            if (element is object)
-            {
-              var parameters = element.GetParameters(External.DB.ParameterClass.Any).
-                Select(p => new ParameterPropertyDescriptor(p)).
-                ToArray();
-
-              var descriptors = new PropertyDescriptor[properties.Count + parameters.Length];
-              properties.CopyTo(descriptors, 0);
-              parameters.CopyTo(descriptors, properties.Count);
-
-              return new PropertyDescriptorCollection(descriptors, true);
-            }
-          }
-
-          return properties;
-        }
-      }
-
-      private class ParameterPropertyDescriptor : PropertyDescriptor
-      {
-        readonly ARDB.Parameter parameter;
-        public ParameterPropertyDescriptor(ARDB.Parameter p) : base(p.Definition?.Name ?? p.Id.ToValue().ToString(), null) { parameter = p; }
-        public override Type ComponentType => typeof(Proxy);
-        public override bool IsReadOnly => true;
-        public override string Name => parameter.Definition?.Name ?? string.Empty;
-        public override string Category => parameter.Definition is null ? string.Empty : ARDB.LabelUtils.GetLabelFor(parameter.Definition.ParameterGroup);
-        public override string Description
-        {
-          get
-          {
-            var description = string.Empty;
-            if (parameter.Definition is ARDB.Definition definition)
-            {
-              External.DB.Schemas.DataType dataType = definition.GetDataType();
-              description = dataType.Label.ToLower();
-
-              if (string.IsNullOrEmpty(description))
-                description = parameter.StorageType.ToString();
-            }
-
-            string parameterClass = "Unknown";
-            if (parameter.Id.IsBuiltInId()) parameterClass = "Built-in";
-            else if (parameter.IsShared) parameterClass = "Shared";
-            else parameterClass = "Project";
-
-            if (parameter.IsReadOnly)
-              description = "read only " + description;
-
-            description = $"{parameterClass} {description} parameter.{Environment.NewLine}{Environment.NewLine}";
-            description += $"ParameterId : {((External.DB.Schemas.ParameterId) parameter.GetTypeId()).FullName}";
-
-            if (parameter.Id.TryGetBuiltInParameter(out var builtInParameter))
-              description += $"{Environment.NewLine}BuiltInParameter : {builtInParameter.ToStringGeneric()}";
-
-            return description;
-          }
-        }
-        public override bool Equals(object obj)
-        {
-          if (obj is ParameterPropertyDescriptor other)
-            return other.parameter.Id == parameter.Id;
-
-          return false;
-        }
-        public override int GetHashCode() => parameter.Id.GetHashCode();
-        public override bool ShouldSerializeValue(object component) { return false; }
-        public override void ResetValue(object component) { }
-        public override bool CanResetValue(object component) { return false; }
-        public override void SetValue(object component, object value) { }
-        public override Type PropertyType => typeof(string);
-        public override object GetValue(object component)
-        {
-          if (parameter.Element is object && parameter.Definition is object)
-          {
-            if (parameter.StorageType == ARDB.StorageType.String)
-              return parameter.AsString();
-
-            return parameter.Element.GetParameterFormatOptions(parameter.Id) is ARDB.FormatOptions options ?
-              parameter.AsValueString(options) :
-              parameter.AsValueString();
-          }
-
-          return null;
-        }
-      }
     }
 
     public virtual IGH_GooProxy EmitProxy() => new Proxy(this);
