f93d0ed20c00c7578b1a7336d28c00b1e1117137
kike-garbo <kike.garbo@gmail.com>
2022-12-14 15:03:37 +0100

    Looks like images can't change its type.

--- src/RhinoInside.Revit.GH/Components/Import/AddImage.cs
+++ src/RhinoInside.Revit.GH/Components/Import/AddImage.cs
@@ -142,11 +142,7 @@ namespace RhinoInside.Revit.GH.Components.Annotations
     {
       if (image is null) return false;
       if (image.OwnerViewId != view.Id) return false;
-      if (image.GetTypeId() != type.Id)
-      {
-        if (!image.CanHaveTypeAssigned()) return false;
-        type.ChangeTypeId(type.Id);
-      }
+      if (image.GetTypeId() != type.Id) return false;
 
       if (!image.GetLocation(ARDB.BoxPlacement.Center).IsAlmostEqualTo(point))
         image.SetLocation(point, ARDB.BoxPlacement.Center);
