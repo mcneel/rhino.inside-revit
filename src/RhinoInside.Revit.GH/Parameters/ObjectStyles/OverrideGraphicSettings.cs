79e82dbc0726c9e5d85c3867dcebd3a86347e301
kike-garbo <kike.garbo@gmail.com>
2022-12-29 21:20:34 +0100

    Renamed 'Graphic Settings' to 'Graphic Overrides'

--- src/RhinoInside.Revit.GH/Parameters/ObjectStyles/OverrideGraphicSettings.cs
+++ src/RhinoInside.Revit.GH/Parameters/ObjectStyles/OverrideGraphicSettings.cs
@@ -7,6 +7,6 @@ namespace RhinoInside.Revit.GH.Parameters
   {
     public override GH_Exposure Exposure => GH_Exposure.secondary | GH_Exposure.hidden;
     public override Guid ComponentGuid => new Guid("3AE08A20-8360-4ECF-ACB0-5FD42A715545");
-    public OverrideGraphicSettings() : base("Graphic Settings", "GSettings", "Contains a collection of Revit graphic settings", "Params", "Revit") { }
+    public OverrideGraphicSettings() : base("Graphic Overrides", "Overrides", "Contains a collection of Revit graphic overrides", "Params", "Revit") { }
   }
 }
