af4d963873f08f654f2ae2f39e0267b89964bae6
kike-garbo <kike.garbo@gmail.com>
2022-12-29 21:19:22 +0100

    Moved `OverrideGraphicSettings` type to 'OverrideGraphicSettings.cs'

--- src/RhinoInside.Revit.GH/Parameters/CompoundStructure.cs
+++ src/RhinoInside.Revit.GH/Parameters/CompoundStructure.cs
@@ -16,11 +16,4 @@ namespace RhinoInside.Revit.GH.Parameters
     public override Guid ComponentGuid => new Guid("A5172644-3F23-4F6F-82DF-0F61D46BA6B9");
     public CompoundStructureLayer() : base("Compound Structure Layer", "CSLayer", "Contains a collection of Revit compound structure layers", "Params", "Revit") { }
   }
-
-  public class OverrideGraphicSettings : Param<Types.OverrideGraphicSettings>
-  {
-    public override GH_Exposure Exposure => GH_Exposure.secondary | GH_Exposure.hidden;
-    public override Guid ComponentGuid => new Guid("3AE08A20-8360-4ECF-ACB0-5FD42A715545");
-    public OverrideGraphicSettings() : base("Graphic Settings", "GSettings", "Contains a collection of Revit graphic settings", "Params", "Revit") { }
-  }
 }
---
+++ src/RhinoInside.Revit.GH/Parameters/ObjectStyles/OverrideGraphicSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using Grasshopper.Kernel;
+
+namespace RhinoInside.Revit.GH.Parameters
+{
+  public class OverrideGraphicSettings : Param<Types.OverrideGraphicSettings>
+  {
+    public override GH_Exposure Exposure => GH_Exposure.secondary | GH_Exposure.hidden;
+    public override Guid ComponentGuid => new Guid("3AE08A20-8360-4ECF-ACB0-5FD42A715545");
+    public OverrideGraphicSettings() : base("Graphic Settings", "GSettings", "Contains a collection of Revit graphic settings", "Params", "Revit") { }
+  }
+}
