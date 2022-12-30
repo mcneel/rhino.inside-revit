5fdafa0e460b669bbaa66867bbf06355e1cc658f
kike-garbo <kike.garbo@gmail.com>
2022-12-30 16:47:31 +0100

    Added `OverrideGraphicSettingsExtensions` for porting purposes.

---
+++ src/RhinoInside.Revit.External/DB/Extensions/OverrideGraphicSettings.cs
@@ -0,0 +1,177 @@
+using Autodesk.Revit.DB;
+
+namespace RhinoInside.Revit.External.DB.Extensions
+{
+  public static class OverrideGraphicSettingsExtensions
+  {
+    public static bool IsSurfaceForegroundPatternVisible(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.IsSurfaceForegroundPatternVisible;
+#else
+      return settings.IsProjectionFillPatternVisible;
+#endif
+    }
+
+    public static ElementId SurfaceForegroundPatternId(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.SurfaceForegroundPatternId;
+#else
+      return settings.ProjectionFillPatternId;
+#endif
+    }
+
+    public static Color SurfaceForegroundPatternColor(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.SurfaceForegroundPatternColor;
+#else
+      return settings.ProjectionFillColor;
+#endif
+    }
+
+    public static bool IsSurfaceBackgroundPatternVisible(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.IsSurfaceBackgroundPatternVisible;
+#else
+      return false;
+#endif
+    }
+
+    public static ElementId SurfaceBackgroundPatternId(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.SurfaceBackgroundPatternId;
+#else
+      return ElementIdExtension.InvalidElementId;
+#endif
+    }
+
+    public static Color SurfaceBackgroundPatternColor(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.SurfaceBackgroundPatternColor;
+#else
+      return Color.InvalidColorValue;
+#endif
+    }
+
+    public static bool IsCutForegroundPatternVisible(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.IsCutForegroundPatternVisible;
+#else
+      return settings.IsCutFillPatternVisible;
+#endif
+    }
+
+    public static ElementId CutForegroundPatternId(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.CutForegroundPatternId;
+#else
+      return settings.CutFillPatternId;
+#endif
+    }
+
+    public static Color CutForegroundPatternColor(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.CutForegroundPatternColor;
+#else
+      return settings.CutFillColor;
+#endif
+    }
+
+    public static bool IsCutBackgroundPatternVisible(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.IsCutBackgroundPatternVisible;
+#else
+      return false;
+#endif
+    }
+
+    public static ElementId CutBackgroundPatternId(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.CutBackgroundPatternId;
+#else
+      return ElementIdExtension.InvalidElementId;
+#endif
+    }
+
+    public static Color CutBackgroundPatternColor(this OverrideGraphicSettings settings)
+    {
+#if REVIT_2019
+      return settings.CutBackgroundPatternColor;
+#else
+      return Color.InvalidColorValue;
+#endif
+    }
+
+#if !REVIT_2019
+    public static OverrideGraphicSettings SetSurfaceForegroundPatternVisible(this OverrideGraphicSettings settings, bool visible)
+    {
+      return settings.SetProjectionFillPatternVisible(visible);
+    }
+
+    public static OverrideGraphicSettings SetSurfaceForegroundPatternId(this OverrideGraphicSettings settings, ElementId fillPatternId)
+    {
+      return settings.SetProjectionFillPatternId(fillPatternId);
+    }
+
+    public static OverrideGraphicSettings SetSurfaceForegroundPatternColor(this OverrideGraphicSettings settings, Color color)
+    {
+      return settings.SetProjectionFillColor(color);
+    }
+
+    public static OverrideGraphicSettings SetSurfaceBackgroundPatternVisible(this OverrideGraphicSettings settings, bool visible)
+    {
+      return settings;
+    }
+
+    public static OverrideGraphicSettings SetSurfaceBackgroundPatternId(this OverrideGraphicSettings settings, ElementId fillPatternId)
+    {
+      return settings;
+    }
+
+    public static OverrideGraphicSettings SetSurfaceBackgroundPatternColor(this OverrideGraphicSettings settings, Color color)
+    {
+      return settings;
+    }
+
+    public static OverrideGraphicSettings SetCutForegroundPatternVisible(this OverrideGraphicSettings settings, bool visible)
+    {
+      return settings.SetCutFillPatternVisible(visible);
+    }
+
+    public static OverrideGraphicSettings SetCutForegroundPatternId(this OverrideGraphicSettings settings, ElementId fillPatternId)
+    {
+      return settings.SetCutFillPatternId(fillPatternId);
+    }
+
+    public static OverrideGraphicSettings SetCutForegroundPatternColor(this OverrideGraphicSettings settings, Color color)
+    {
+      return settings.SetCutFillColor(color);
+    }
+
+    public static OverrideGraphicSettings SetCutBackgroundPatternVisible(this OverrideGraphicSettings settings, bool visible)
+    {
+      return settings;
+    }
+
+    public static OverrideGraphicSettings SetCutBackgroundPatternId(this OverrideGraphicSettings settings, ElementId fillPatternId)
+    {
+      return settings;
+    }
+
+    public static OverrideGraphicSettings SetCutBackgroundPatternColor(this OverrideGraphicSettings settings, Color color)
+    {
+      return settings;
+    }
+#endif
+  }
+}
--- src/RhinoInside.Revit.GH/Types/ObjectStyles/OverrideGraphicSettings.cs
+++ src/RhinoInside.Revit.GH/Types/ObjectStyles/OverrideGraphicSettings.cs
@@ -31,17 +31,13 @@ namespace RhinoInside.Revit.GH.Types
           if (settings.ProjectionLineColor.IsValid)               description.Add($"Projection Lines : Color = {GH_Format.FormatColour(settings.ProjectionLineColor.ToColor())}");
           if (settings.ProjectionLineWeight != InvalidPenNumber)  description.Add($"Projection Lines : Weight = {settings.ProjectionLineWeight}");
 
+          if (!settings.IsSurfaceForegroundPatternVisible())      description.Add($"Surface Patterns : Foreground Visible = {settings.IsSurfaceForegroundPatternVisible()}");
+          if (settings.SurfaceForegroundPatternId().IsValid())    description.Add($"Surface Patterns : Foreground Pattern = {(Document?.GetElement(settings.SurfaceForegroundPatternId())?.Name ?? "<Solid fill>")}");
+          if (settings.SurfaceForegroundPatternColor().IsValid)   description.Add($"Surface Patterns : Foreground Color = {GH_Format.FormatColour(settings.SurfaceForegroundPatternColor().ToColor())}");
 #if REVIT_2019
-          if (!settings.IsSurfaceForegroundPatternVisible)        description.Add($"Surface Patterns : Foreground Visible = {settings.IsSurfaceForegroundPatternVisible}");
-          if (settings.SurfaceForegroundPatternId.IsValid())      description.Add($"Surface Patterns : Foreground Pattern = {(Document?.GetElement(settings.SurfaceForegroundPatternId)?.Name ?? "<Solid fill>")}");
-          if (settings.SurfaceForegroundPatternColor.IsValid)     description.Add($"Surface Patterns : Foreground Color = {GH_Format.FormatColour(settings.SurfaceForegroundPatternColor.ToColor())}");
-          if (!settings.IsSurfaceBackgroundPatternVisible)        description.Add($"Surface Patterns : Background Visible = {settings.IsSurfaceBackgroundPatternVisible}");
-          if (settings.SurfaceBackgroundPatternId.IsValid())      description.Add($"Surface Patterns : Background Pattern = {(Document?.GetElement(settings.SurfaceBackgroundPatternId)?.Name ?? "<Solid fill>")}");
-          if (settings.SurfaceBackgroundPatternColor.IsValid)     description.Add($"Surface Patterns : Background Color = {GH_Format.FormatColour(settings.SurfaceBackgroundPatternColor.ToColor())}");
-#else
-          if (!settings.IsProjectionFillPatternVisible)           description.Add($"Surface Patterns : Foreground Visible = {settings.IsProjectionFillPatternVisible}");
-          if (settings.ProjectionFillPatternId.IsValid())         description.Add($"Surface Patterns : Foreground Pattern = {(Document?.GetElement(settings.ProjectionFillPatternId)?.Name ?? "<Solid fill>")}");
-          if (settings.ProjectionFillColor.IsValid)               description.Add($"Surface Patterns : Foreground Color = {GH_Format.FormatColour(settings.ProjectionFillColor.ToColor())}");
+          if (!settings.IsSurfaceBackgroundPatternVisible())      description.Add($"Surface Patterns : Background Visible = {settings.IsSurfaceBackgroundPatternVisible()}");
+          if (settings.SurfaceBackgroundPatternId().IsValid())    description.Add($"Surface Patterns : Background Pattern = {(Document?.GetElement(settings.SurfaceBackgroundPatternId())?.Name ?? "<Solid fill>")}");
+          if (settings.SurfaceBackgroundPatternColor().IsValid)   description.Add($"Surface Patterns : Background Color = {GH_Format.FormatColour(settings.SurfaceBackgroundPatternColor().ToColor())}");
 #endif
           if (settings.Transparency != default)                   description.Add($"Surface : Transparency = {settings.Transparency}%");
 
@@ -49,19 +45,14 @@ namespace RhinoInside.Revit.GH.Types
           if (settings.CutLineColor.IsValid)                      description.Add($"Cut Lines : Color = {GH_Format.FormatColour(settings.CutLineColor.ToColor())}");
           if (settings.CutLineWeight != InvalidPenNumber)         description.Add($"Cut Lines : Weight = {settings.CutLineWeight}");
 
+          if (!settings.IsCutForegroundPatternVisible())          description.Add($"Cut Patterns : Foreground Visible = {settings.IsCutForegroundPatternVisible()}");
+          if (settings.CutForegroundPatternId().IsValid())        description.Add($"Cut Patterns : Foreground Pattern = {(Document?.GetElement(settings.CutForegroundPatternId())?.Name ?? "<Solid fill>")}");
+          if (settings.CutForegroundPatternColor().IsValid)       description.Add($"Cut Patterns : Foreground Color = {GH_Format.FormatColour(settings.CutForegroundPatternColor().ToColor())}");
 #if REVIT_2019
-          if (!settings.IsCutForegroundPatternVisible)            description.Add($"Cut Patterns : Foreground Visible = {settings.IsCutForegroundPatternVisible}");
-          if (settings.CutForegroundPatternId.IsValid())          description.Add($"Cut Patterns : Foreground Pattern = {(Document?.GetElement(settings.CutForegroundPatternId)?.Name ?? "<Solid fill>")}");
-          if (settings.CutForegroundPatternColor.IsValid)         description.Add($"Cut Patterns : Foreground Color = {GH_Format.FormatColour(settings.CutForegroundPatternColor.ToColor())}");
-          if (!settings.IsCutBackgroundPatternVisible)            description.Add($"Cut Patterns : Background Visible = {settings.IsCutBackgroundPatternVisible}");
-          if (settings.CutBackgroundPatternId.IsValid())          description.Add($"Cut Patterns : Background Pattern = {(Document?.GetElement(settings.CutBackgroundPatternId)?.Name ?? "<Solid fill>")}");
-          if (settings.CutBackgroundPatternColor.IsValid)         description.Add($"Cut Patterns : Background Color = {GH_Format.FormatColour(settings.CutBackgroundPatternColor.ToColor())}");
-#else
-          if (!settings.IsCutFillPatternVisible)                  description.Add($"Cut Patterns : Foreground Visible = {settings.IsCutFillPatternVisible}");
-          if (settings.CutFillPatternId.IsValid())                description.Add($"Cut Patterns : Foreground Pattern = {(Document?.GetElement(settings.CutFillPatternId)?.Name ?? "<Solid fill>")}");
-          if (settings.CutFillColor.IsValid)                      description.Add($"Cut Patterns : Foreground Color = {GH_Format.FormatColour(settings.CutFillColor.ToColor())}");
+          if (!settings.IsCutBackgroundPatternVisible())          description.Add($"Cut Patterns : Background Visible = {settings.IsCutBackgroundPatternVisible()}");
+          if (settings.CutBackgroundPatternId().IsValid())        description.Add($"Cut Patterns : Background Pattern = {(Document?.GetElement(settings.CutBackgroundPatternId())?.Name ?? "<Solid fill>")}");
+          if (settings.CutBackgroundPatternColor().IsValid)       description.Add($"Cut Patterns : Background Color = {GH_Format.FormatColour(settings.CutBackgroundPatternColor().ToColor())}");
 #endif
-
           if (description.Count > 0)
             return Environment.NewLine + string.Join(Environment.NewLine, description.Select(x => $"  {x}"));
 
@@ -81,29 +72,29 @@ namespace RhinoInside.Revit.GH.Types
     public OverrideGraphicSettings(OverrideGraphicSettings value) : base(value?.Document, value is null ? null : new ARDB.OverrideGraphicSettings(value.Value)) { }
     public OverrideGraphicSettings(ARDB.Document doc, ARDB.OverrideGraphicSettings value) : base(doc, value) { }
     public OverrideGraphicSettings(ARDB.Document doc) : this(doc, doc is null ? null : new ARDB.OverrideGraphicSettings()) { }
-    internal OverrideGraphicSettings(ARDB.Document doc, OverrideGraphicSettings value) : base(doc, new ARDB.OverrideGraphicSettings(value.Value))
+    internal OverrideGraphicSettings(ARDB.Document doc, OverrideGraphicSettings other) : base(doc, new ARDB.OverrideGraphicSettings(other.Value))
     {
-      if (value.Document.IsEquivalent(Document)) return;
+      if (other.Document.IsEquivalent(Document)) return;
 
-      if (Document is null || value.Document is null)
+      if (Document is null || other.Document is null)
       {
         if (Value.ProjectionLinePatternId.ToBuiltInLinePattern() != ERDB.BuiltInLinePattern.Solid) Value.SetProjectionLinePatternId(ElementIdExtension.InvalidElementId);
         if (Value.CutLinePatternId.ToBuiltInLinePattern() != ERDB.BuiltInLinePattern.Solid) Value.SetCutLinePatternId(ElementIdExtension.InvalidElementId);
 
-        if (Value.SurfaceForegroundPatternId != FillPatternElement.SolidId) Value.SetSurfaceForegroundPatternId(ElementIdExtension.InvalidElementId);
-        if (Value.SurfaceBackgroundPatternId != FillPatternElement.SolidId) Value.SetSurfaceBackgroundPatternId(ElementIdExtension.InvalidElementId);
-        if (Value.CutForegroundPatternId != FillPatternElement.SolidId) Value.SetCutForegroundPatternId(ElementIdExtension.InvalidElementId);
-        if (Value.CutBackgroundPatternId != FillPatternElement.SolidId) Value.SetCutBackgroundPatternId(ElementIdExtension.InvalidElementId);
+        if (Value.SurfaceForegroundPatternId() != FillPatternElement.SolidId) Value.SetSurfaceForegroundPatternId(ElementIdExtension.InvalidElementId);
+        if (Value.SurfaceBackgroundPatternId() != FillPatternElement.SolidId) Value.SetSurfaceBackgroundPatternId(ElementIdExtension.InvalidElementId);
+        if (Value.CutForegroundPatternId() != FillPatternElement.SolidId) Value.SetCutForegroundPatternId(ElementIdExtension.InvalidElementId);
+        if (Value.CutBackgroundPatternId() != FillPatternElement.SolidId) Value.SetCutBackgroundPatternId(ElementIdExtension.InvalidElementId);
       }
       else
       {
-        Value.SetProjectionLinePatternId(Document.LookupElement(value.Document, value.Value.ProjectionLinePatternId));
-        Value.SetCutLinePatternId(Document.LookupElement(value.Document, value.Value.CutLinePatternId));
+        Value.SetProjectionLinePatternId(Document.LookupElement(other.Document, other.Value.ProjectionLinePatternId));
+        Value.SetCutLinePatternId(Document.LookupElement(other.Document, other.Value.CutLinePatternId));
 
-        Value.SetSurfaceForegroundPatternId(Document.LookupElement(value.Document, value.Value.SurfaceForegroundPatternId));
-        Value.SetSurfaceBackgroundPatternId(Document.LookupElement(value.Document, value.Value.SurfaceBackgroundPatternId));
-        Value.SetCutForegroundPatternId(Document.LookupElement(value.Document, value.Value.CutForegroundPatternId));
-        Value.SetCutBackgroundPatternId(Document.LookupElement(value.Document, value.Value.CutBackgroundPatternId));
+        Value.SetSurfaceForegroundPatternId(Document.LookupElement(other.Document, other.Value.SurfaceForegroundPatternId()));
+        Value.SetSurfaceBackgroundPatternId(Document.LookupElement(other.Document, other.Value.SurfaceBackgroundPatternId()));
+        Value.SetCutForegroundPatternId(Document.LookupElement(other.Document, other.Value.CutForegroundPatternId()));
+        Value.SetCutBackgroundPatternId(Document.LookupElement(other.Document, other.Value.CutBackgroundPatternId()));
       }
     }
 
