4de33820da16ad39f1024afffe0c426454a64520
kike-garbo <kike.garbo@gmail.com>
2023-01-07 16:32:00 +0100

    Fix for Revit 2019

--- src/RhinoInside.Revit.External/DB/Schemas/CategoryId.cs
+++ src/RhinoInside.Revit.External/DB/Schemas/CategoryId.cs
@@ -12,7 +12,12 @@ namespace RhinoInside.Revit.External.DB.Schemas
     static readonly CategoryId empty = new CategoryId();
     public static new CategoryId Empty => empty;
 
-    public string LocalizedLabel => Autodesk.Revit.DB.LabelUtils.GetLabelFor((Autodesk.Revit.DB.BuiltInCategory) this);
+    public string LocalizedLabel =>
+#if REVIT_2020
+      Autodesk.Revit.DB.LabelUtils.GetLabelFor((Autodesk.Revit.DB.BuiltInCategory) this);
+#else
+      Label;
+#endif
 
     public CategoryId() { }
     public CategoryId(string id) : base(id)
