072c9f08f956b94558f9ce86fe38ff9e2f2db33a
kike-garbo <kike.garbo@gmail.com>
2023-03-13 16:34:49 +0100

    Added `Types.PointCloudFilter` Implemented `Types.PointCloudInstance` preview.

--- src/RhinoInside.Revit.GH/Components/Import/QueryCloudPoints.cs
+++ src/RhinoInside.Revit.GH/Components/Import/QueryCloudPoints.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using Rhino.Geometry;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
 using Grasshopper.Kernel.Parameters;
 using ARDB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Components.Annotations
 {
+  using External.DB;
   using External.DB.Extensions;
   using Convert.Geometry;
 
@@ -42,24 +44,12 @@ namespace RhinoInside.Revit.GH.Components.Annotations
       ),
       new ParamDefinition
       (
-        new Param_Plane()
+        new Parameters.PointCloudFilter
         {
-          Name = "Clipping Planes",
-          NickName = "CP",
-          Description = $"Planes which will be used for quick filtering cells of points.{Environment.NewLine}Usually the bounding box of the area to query.",
-          Optional = true,
-          Access = GH_ParamAccess.list
-        }, ParamRelevance.Secondary
-      ),
-      new ParamDefinition
-      (
-        new Param_Plane()
-        {
-          Name = "Planes",
-          NickName = "P",
-          Description = "Planes which will be used for exact filtering of individual points.",
-          Optional = true,
-          Access = GH_ParamAccess.list
+          Name = "Filter",
+          NickName = "F",
+          Description = $"Convex shape to be used as a volume filter.{Environment.NewLine}Plane, Box and Mesh are accepted.",
+          Optional = true
         }, ParamRelevance.Primary
       ),
       new ParamDefinition
@@ -118,31 +108,40 @@ namespace RhinoInside.Revit.GH.Components.Annotations
     protected override void TrySolveInstance(IGH_DataAccess DA)
     {
       if (!Params.GetData(DA, "Point Cloud", out Types.PointCloudInstance pointCloud, x => x.IsValid)) return;
-      if (!Params.TryGetDataList(DA, "Clipping Planes", out IList<Plane> clippingPlanes)) return;
-      if (!Params.TryGetDataList(DA, "Planes", out IList<Plane> planes)) return;
-      if (!Params.TryGetData(DA, "Average Distance", out double? averageDistance, x => !double.IsInfinity(x) && !double.IsNaN(x))) return;
-      if (!Params.GetData(DA, "Limit", out int? numPoints)) return;
+      if (!Params.TryGetData(DA, "Filter", out Types.PointCloudFilter filter)) return;
+      if (!Params.TryGetData(DA, "Average Distance", out double? averageDistance, x => NumericTolerance.IsFinite(x))) return;
+      if (!Params.GetData(DA, "Limit", out int? numPoints, x => x >= 0)) return;
 
-      if (numPoints < 1) return;
+      if (numPoints < 1)
+      {
+        Params.TrySetData(DA, "Count", () => 0);
+        return;
+      }
+
+      if (numPoints == 1) averageDistance = 0.0;
 
       var box = pointCloud.Box;
       var corners = box.GetCorners();
       var diagonal = corners[0].DistanceTo(corners[6]);
 
-      // One plane should faster than all.
-      clippingPlanes = clippingPlanes ?? new List<Plane> { new Plane(corners[0], corners[1], corners[2]) };
-      planes = planes ?? new Plane[0];
-
       var cloud = pointCloud.Value;
-      var type = cloud.Document.GetElement(cloud.GetTypeId()) as ARDB.PointCloudType;
-      var transform = cloud.GetTransform();
 
-      var filter = ARDB.PointClouds.PointCloudFilterFactory.CreateMultiPlaneFilter(planes.Concat(clippingPlanes).Select(x => x.ToPlane()).ToList(), planes.Count);
-      var points = cloud.GetPoints(filter, GeometryEncoder.ToInternalLength(averageDistance ?? diagonal / 1000.0), numPoints.Value);
+      var filterValue = filter?.Value;
+      if (filterValue is null)
+      {
+        // One plane should be enough.
+        filter = new Types.PointCloudFilter();
+        filterValue = ARDB.PointClouds.PointCloudFilterFactory.CreateMultiPlaneFilter
+          (new ARDB.Plane[] { new Plane(corners[0], corners[1], corners[2]).ToPlane() });
+      }
+
+      var points = cloud.GetPoints(filterValue, GeometryEncoder.ToInternalLength(averageDistance ?? diagonal / 1000.0), numPoints.Value);
+      var transform = cloud.GetTransform();
 
       Params.TrySetDataList(DA, "Points", () => points.Select(x => GeometryDecoder.ToPoint3d(transform.OfPoint(x))));
       if (cloud.HasColor())
       {
+        var type = cloud.Document.GetElement(cloud.GetTypeId()) as ARDB.PointCloudType;
         switch (type.ColorEncoding)
         {
           case ARDB.PointClouds.PointCloudColorEncoding.ARGB:
