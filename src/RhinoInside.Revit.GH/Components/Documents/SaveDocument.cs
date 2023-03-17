6bbf7671b495be9f6986d5c4b60afb1794ba586c
kike-garbo <kike.garbo@gmail.com>
2023-03-17 11:48:40 +0100

    Updated 'Save Component Family' and 'Save Document' to return back the 'Path'. This way 'Save Component Family' can be chained with 'Load Component Family' to duplicate a Family.

--- src/RhinoInside.Revit.GH/Components/Documents/SaveDocument.cs
+++ src/RhinoInside.Revit.GH/Components/Documents/SaveDocument.cs
@@ -3,10 +3,12 @@ using System.IO;
 using System.Linq;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Parameters;
+using RhinoInside.Revit.External.DB.Extensions;
 using ARDB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Components.Documents
 {
+  [ComponentVersion(introduced: "1.0", updated: "1.13")]
   public class DocumentSave : ZuiComponent
   {
     public override Guid ComponentGuid => new Guid("FBB2E4A2-CC2A-470E-B7E8-CB3320166CC5");
@@ -26,7 +28,7 @@ namespace RhinoInside.Revit.GH.Components.Documents
     protected override ParamDefinition[] Inputs => inputs;
     static readonly ParamDefinition[] inputs =
     {
-      new ParamDefinition(new Parameters.Document(), ParamRelevance.Occasional),
+      new ParamDefinition(new Parameters.Document()),
       new ParamDefinition
       (
         new Param_FilePath()
@@ -35,44 +37,32 @@ namespace RhinoInside.Revit.GH.Components.Documents
           NickName = "P",
           Optional = true,
           FileFilter = "Project File (*.rvt)|*.rvt"
-        }
+        }, ParamRelevance.Primary
       ),
-      ParamDefinition.Create<Param_Boolean>("Overwrite", "OF", "Overwrite file on disk", defaultValue: false, GH_ParamAccess.item),
-      ParamDefinition.Create<Param_Boolean>("Compact", "C", "Compact the file", defaultValue: false, GH_ParamAccess.item),
-      ParamDefinition.Create<Param_Integer>("Backups", "B", "The maximum number of backups to keep on disk", -1, GH_ParamAccess.item),
-      ParamDefinition.Create<Parameters.View>("View", "View", "The view that will be used to generate the file preview", GH_ParamAccess.item, optional: true)
+      ParamDefinition.Create<Param_Boolean>("Overwrite", "O", "Overwrite file on disk", defaultValue: false),
+      ParamDefinition.Create<Param_Boolean>("Compact", "C", "Compact the file", defaultValue: false, relevance: ParamRelevance.Secondary),
+      ParamDefinition.Create<Param_Integer>("Backups", "B", "The maximum number of backups to keep on disk", -1, relevance: ParamRelevance.Secondary),
+      ParamDefinition.Create<Parameters.View>("View", "View", "The view that will be used to generate the file preview", optional: true, relevance: ParamRelevance.Occasional)
     };
 
     protected override ParamDefinition[] Outputs => outputs;
     static readonly ParamDefinition[] outputs =
     {
-      new ParamDefinition(new Parameters.Document())
+      new ParamDefinition(new Parameters.Document(), ParamRelevance.Primary),
+      ParamDefinition.Create<Param_String>("Path", "P", relevance: ParamRelevance.Primary),
+      ParamDefinition.Create<Param_Boolean>("Written", "W", "Wheter or not file is being written to disk", relevance: ParamRelevance.Primary),
     };
 
     protected override void TrySolveInstance(IGH_DataAccess DA)
     {
-      if (!Parameters.Document.GetDataOrDefault(this, DA, "Document", out var doc))
-        return;
-
-      var filePath = string.Empty;
-      DA.GetData("Path", ref filePath);
-
-      var overwrite = false;
-      if (!DA.GetData("Overwrite", ref overwrite))
-        return;
-
-      var compact = false;
-      if (!DA.GetData("Compact", ref compact))
-        return;
-
-      var backups = -1;
-      if (!DA.GetData("Backups", ref backups))
-        return;
-
-      var view = default(ARDB.View);
-      if (DA.GetData("View", ref view) && !view.Document.Equals(doc))
+      if (!Parameters.Document.GetDataOrDefault(this, DA, "Document", out var doc)) return;
+      if (!Params.TryGetData(DA, "Path", out string filePath)) return;
+      if (!Params.TryGetData(DA, "Overwrite", out bool? overwrite)) return;
+      if (!Params.TryGetData(DA, "Compact", out bool? compact)) return;
+      if (!Params.TryGetData(DA, "Backups", out int? backups)) return;
+      if (Params.TryGetData(DA, "View", out Types.View view) && view?.Document.IsEquivalent(doc) is false)
       {
-        AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"View '{view.Title}' is not a valid view in document {doc.Title}");
+        AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"View '{view.Value.Title}' is not a valid view in document {doc.Title}");
         return;
       }
 
@@ -82,9 +72,10 @@ namespace RhinoInside.Revit.GH.Components.Documents
 
         if (string.IsNullOrEmpty(filePath))
         {
-          if (overwrite)
+          var wasSaved = !string.IsNullOrEmpty(doc.PathName);
+          if (overwrite is true && wasSaved)
           {
-            using (var saveOptions = new ARDB.SaveOptions() { Compact = compact })
+            using (var saveOptions = new ARDB.SaveOptions() { Compact = compact is true })
             {
               if (view is object)
                 saveOptions.PreviewViewId = view.Id;
@@ -92,9 +83,21 @@ namespace RhinoInside.Revit.GH.Components.Documents
               doc.Save(saveOptions);
             }
 
-            DA.SetData("Document", doc);
+            Params.TrySetData(DA, "Document", () => doc);
+            Params.TrySetData(DA, "Path", () => doc.PathName);
+            Params.TrySetData(DA, "Written", () => true);
           }
-          else AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, $"Failed to collect data from 'Path'.");
+          else
+          {
+            if (!wasSaved && overwrite is true)
+              AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"Document '{doc.GetName()}' is never being saved before, please specify a valid Path.");
+            else
+            {
+              Params.TrySetData(DA, "Document", () => doc);
+              Params.TrySetData(DA, "Path", () => wasSaved ? doc.PathName : null);
+              Params.TrySetData(DA, "Written", () => false);
+            }
+          }
         }
         else
         {
@@ -105,28 +108,35 @@ namespace RhinoInside.Revit.GH.Components.Documents
           {
             if (!Path.HasExtension(filePath))
             {
-              if (doc.IsFamilyDocument)
-                filePath += ".rfa";
-              else
-                filePath += ".rvt";
+              if (doc.IsFamilyDocument) filePath += ".rfa";
+              else                      filePath += ".rvt";
             }
 
-            using (var saveAsOptions = new ARDB.SaveAsOptions() { OverwriteExistingFile = overwrite, Compact = compact })
+            var exist = File.Exists(filePath);
+            if (overwrite is true || !exist)
             {
-              if (backups > -1)
-                saveAsOptions.MaximumBackups = backups;
+              using (var saveAsOptions = new ARDB.SaveAsOptions() { OverwriteExistingFile = overwrite is true, Compact = compact is true })
+              {
+                if (backups > -1)
+                  saveAsOptions.MaximumBackups = backups.Value;
 
-              if (view is object)
-                saveAsOptions.PreviewViewId = view.Id;
+                if (view is object)
+                  saveAsOptions.PreviewViewId = view.Id;
 
-              doc.SaveAs(filePath, saveAsOptions);
-              DA.SetData("Document", doc);
+                doc.SaveAs(filePath, saveAsOptions);
+                Params.TrySetData(DA, "Document", () => doc);
+                Params.TrySetData(DA, "Path", () => filePath);
+                Params.TrySetData(DA, "Written", () => true);
+              }
+            }
+            else
+            {
+              Params.TrySetData(DA, "Document", () => doc);
+              Params.TrySetData(DA, "Path", () => filePath);
+              Params.TrySetData(DA, "Written", () => false);
             }
           }
-          else
-          {
-            AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Path should be absolute.");
-          }
+          else AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Path should be absolute.");
         }
       }
       finally
--- src/RhinoInside.Revit.GH/Components/Family/Save.cs
+++ src/RhinoInside.Revit.GH/Components/Family/Save.cs
@@ -2,13 +2,15 @@ using System;
 using System.IO;
 using System.Linq;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
 using ARDB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Components.Families
 {
   using External.DB.Extensions;
 
-  public class FamilySave : Component
+  [ComponentVersion(introduced:"1.0", updated: "1.13")]
+  public class FamilySave : ZuiComponent
   {
     public override Guid ComponentGuid => new Guid("C2B9B045-8FD2-4124-9294-D9BA809B44B1");
     public override GH_Exposure Exposure => GH_Exposure.tertiary;
@@ -24,98 +26,130 @@ namespace RhinoInside.Revit.GH.Components.Families
     )
     { }
 
-    protected override void RegisterInputParams(GH_InputParamManager manager)
+    protected override ParamDefinition[] Inputs => inputs;
+    static readonly ParamDefinition[] inputs =
     {
-      manager.AddParameter(new Parameters.Family(), "Family", "F", "Family to save", GH_ParamAccess.item);
+      new ParamDefinition
+      (
+        new Parameters.Family()
+        {
+          Name = "Family",
+          NickName = "F",
+          Description = "Family to save"
+        }
+      ),
+      new ParamDefinition
+      (
+        new Param_FilePath()
+        {
+          Name = "Path",
+          NickName = "P",
+          FileFilter = "Family Files (*.rfa)|*.rfa"
+        }
+      ),
+      new ParamDefinition
+      (
+        new Param_Boolean()
+        {
+          Name = "Overwrite",
+          NickName = "O",
+          Description = "Overwrite file on disk",
+        }.SetDefaultVale(false)
+      ),
+      new ParamDefinition
+      (
+        new Param_Boolean()
+        {
+          Name = "Compact",
+          NickName = "C",
+          Description = "Compact the file",
+        }.SetDefaultVale(false), ParamRelevance.Secondary
+      ),
+      new ParamDefinition
+      (
+        new Param_Integer()
+        {
+          Name = "Backups",
+          NickName = "B",
+          Description = "The maximum number of backups to keep on disk",
+        }.SetDefaultVale(-1), ParamRelevance.Secondary
+      ),
+    };
 
-      var path = new Grasshopper.Kernel.Parameters.Param_FilePath();
-      path.FileFilter = "Family File (*.rfa)|*.rfa";
-      manager[manager.AddParameter(path, "Path", "P", string.Empty, GH_ParamAccess.item)].Optional = true;
-
-      manager.AddBooleanParameter("Overwrite", "O", "Overwrite file on disk", GH_ParamAccess.item, false);
-      manager.AddBooleanParameter("Compact", "C", "Compact the file", GH_ParamAccess.item, false);
-      manager.AddIntegerParameter("Backups", "B", "The maximum number of backups to keep on disk", GH_ParamAccess.item, -1);
-      manager[manager.AddParameter(new Parameters.View(), "Preview View", "PV", "The view that will be used to generate the file preview", GH_ParamAccess.item)].Optional = true;
-    }
-
-    protected override void RegisterOutputParams(GH_OutputParamManager manager)
+    protected override ParamDefinition[] Outputs => outputs;
+    static readonly ParamDefinition[] outputs =
     {
-      manager.AddParameter(new Parameters.Family(), "Family", "F", string.Empty, GH_ParamAccess.item);
-    }
+      new ParamDefinition
+      (
+        new Parameters.Family()
+        {
+          Name = "Family",
+          NickName = "F",
+        }, ParamRelevance.Primary
+      ),
+      new ParamDefinition
+      (
+        new Param_String()
+        {
+          Name = "Path",
+          NickName = "P",
+        }, ParamRelevance.Primary
+      ),
+      new ParamDefinition
+      (
+        new Param_Boolean()
+        {
+          Name = "Written",
+          NickName = "W",
+          Description = "Wheter or not file is being written to disk",
+        }, ParamRelevance.Primary
+      ),
+    };
 
     protected override void TrySolveInstance(IGH_DataAccess DA)
     {
-      ARDB.Family family = null;
-      if (!DA.GetData("Family", ref family))
-        return;
-
-      var filePath = string.Empty;
-      DA.GetData("Path", ref filePath);
-
-      var overwrite = false;
-      if (!DA.GetData("Overwrite", ref overwrite))
-        return;
-
-      var compact = false;
-      if (!DA.GetData("Compact", ref compact))
-        return;
-
-      var backups = -1;
-      if (!DA.GetData("Backups", ref backups))
-        return;
+      if (!Params.TryGetData(DA, "Family", out ARDB.Family family)) return;
+      if (!Params.GetData(DA, "Path", out string filePath, x => !string.IsNullOrWhiteSpace(x))) return;
+      if (!Params.TryGetData(DA, "Overwrite", out bool? overwrite)) return;
+      if (!Params.TryGetData(DA, "Compact", out bool? compact)) return;
+      if (!Params.TryGetData(DA, "Backups", out int? backups)) return;
 
       if (family.Document.EditFamily(family) is ARDB.Document familyDoc) using (familyDoc)
       {
-        var view = default(ARDB.View);
-        if (DA.GetData("Preview View", ref view))
-        {
-          if (!view.Document.Equals(familyDoc))
-            AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"View '{view.Title}' is not a valid view in document {familyDoc.GetTitle()}"); return;
-        }
-
         try
         {
-          if (string.IsNullOrEmpty(filePath))
+          if (filePath.Last() == Path.DirectorySeparatorChar)
+            filePath = Path.Combine(filePath, familyDoc.Title);
+
+          if (Path.IsPathRooted(filePath) && filePath.Contains(Path.DirectorySeparatorChar))
           {
-            if (overwrite)
+            if (!Path.HasExtension(filePath))
+              filePath += ".rfa";
+
+            var exist = File.Exists(filePath);
+            if (overwrite is true || !exist)
             {
-              using (var saveOptions = new ARDB.SaveOptions() { Compact = compact })
-              {
-                if (view is object)
-                  saveOptions.PreviewViewId = view.Id;
-
-                familyDoc.Save(saveOptions);
-                DA.SetData("Family", family);
-              }
-            }
-            else AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, $"Failed to collect data from 'Path'.");
-          }
-          else
-          {
-            if (filePath.Last() == Path.DirectorySeparatorChar)
-              filePath = Path.Combine(filePath, familyDoc.Title);
-
-            if (Path.IsPathRooted(filePath) && filePath.Contains(Path.DirectorySeparatorChar))
-            {
-              if (!Path.HasExtension(filePath))
-                filePath += ".rfa";
-
-              using (var saveAsOptions = new ARDB.SaveAsOptions() { OverwriteExistingFile = overwrite, Compact = compact })
+              using (var saveAsOptions = new ARDB.SaveAsOptions() { OverwriteExistingFile = true, Compact = compact is true })
               {
                 if (backups > -1)
-                  saveAsOptions.MaximumBackups = backups;
-
-                if (view is object)
-                  saveAsOptions.PreviewViewId = view.Id;
+                  saveAsOptions.MaximumBackups = backups.Value;
 
                 familyDoc.SaveAs(filePath, saveAsOptions);
-                DA.SetData("Family", family);
+                Params.TrySetData(DA, "Family", () => family);
+                Params.TrySetData(DA, "Path", () => filePath);
+                Params.TrySetData(DA, "Written", () => true);
               }
             }
             else
             {
-              AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Path should be absolute.");
+              Params.TrySetData(DA, "Family", () => family);
+              Params.TrySetData(DA, "Path", () => filePath);
+              Params.TrySetData(DA, "Written", () => false);
             }
+          }
+          else
+          {
+            AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Path should be absolute.");
           }
         }
         catch (Autodesk.Revit.Exceptions.InvalidOperationException e) { AddRuntimeMessage(GH_RuntimeMessageLevel.Error, e.Message); }
