eee18f25a078f01895b7db74d82b5e31a4b07aff
kike-garbo <kike.garbo@gmail.com>
2023-05-09 16:52:58 +0200

    Now 'Built-In Categories' is a little more compact by default.

--- src/RhinoInside.Revit.GH/Parameters/Input/BuiltInCategories.cs
+++ src/RhinoInside.Revit.GH/Parameters/Input/BuiltInCategories.cs
@@ -22,7 +22,6 @@ namespace RhinoInside.Revit.GH.Parameters.Input
     )
     {
       IconDisplayMode = GH_IconDisplayMode.name;
-      LayoutLevel = 2;
     }
 
     protected override void LoadVolatileData()
