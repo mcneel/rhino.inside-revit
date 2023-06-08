b2f8fd12ab49d64e2e7a8874b7ac29774bc52a7d
kike-garbo <kike.garbo@gmail.com>
2023-06-08 14:25:07 +0200

    Added 'Sketch Lines' component.

--- src/RhinoInside.Revit.GH/Components/Model/SketchDeconstruct.cs
+++ src/RhinoInside.Revit.GH/Components/Model/SketchDeconstruct.cs
@@ -1,23 +1,26 @@
 using System;
 using System.Linq;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
+using Rhino.Geometry;
 
 namespace RhinoInside.Revit.GH.Components.ModelElements
 {
+  using Convert.Geometry;
   using External.DB.Extensions;
 
-  [ComponentVersion(introduced: "1.0", updated: "1.11")]
-  public class SketchDeconstruct : ZuiComponent
+  [ComponentVersion(introduced: "1.0", updated: "1.15")]
+  public class SketchLines : ZuiComponent
   {
     public override Guid ComponentGuid => new Guid("F9BC3F5E-7415-485E-B74C-5CB855B818B8");
-    public override GH_Exposure Exposure => GH_Exposure.secondary | GH_Exposure.hidden;
+    public override GH_Exposure Exposure => GH_Exposure.secondary;
     protected override string IconTag => string.Empty;
 
-    public SketchDeconstruct() : base
+    public SketchLines() : base
     (
-      name: "Deconstruct Sketch",
-      nickname: "DecSktch",
-      description: string.Empty,
+      name: "Sketch Lines",
+      nickname: "S-Lines",
+      description: "Get the model lines of the given sketch element",
       category: "Revit",
       subCategory: "Model"
     )
@@ -32,7 +35,7 @@ namespace RhinoInside.Revit.GH.Components.ModelElements
         {
           Name = "Sketch",
           NickName = "S",
-          Description = "Sketch to deconstruct.",
+          Description = "Sketch to access model lines.",
         }
       ),
     };
@@ -73,9 +76,19 @@ namespace RhinoInside.Revit.GH.Components.ModelElements
         {
           Name = "Model Lines",
           NickName = "ML",
-          Description = $"Sketch curves grouped by boundary-profile.",
+          Description = "Sketch lines grouped by boundary-profile.",
           Access = GH_ParamAccess.tree
         }, ParamRelevance.Primary
+      ),
+      new ParamDefinition
+      (
+        new Param_Integer()
+        {
+          Name = "Boundary Condition",
+          NickName = "BC",
+          Description = "Sketch lines boundary condition grouped by boundary-profile.\n\n-1 = Internal loop\n 0 = Open loop\n+1= External loop",
+          Access = GH_ParamAccess.tree
+        }, ParamRelevance.Secondary
       ),
       new ParamDefinition
       (
@@ -102,6 +115,30 @@ namespace RhinoInside.Revit.GH.Components.ModelElements
         sketch?.Value?.GetProfileCurveElements().
         Select(x => x.Select(Types.CurveElement.FromElement)).
         TakeWhileIsNotEscapeKeyDown(this)
+      );
+
+      Params.TrySetDataTree
+      (
+        DA, "Boundary Condition", () =>
+        {
+          var brep = sketch.TrimmedSurface;
+
+          return sketch?.Value?.GetProfileCurveElements().
+          Select
+          (x =>
+          {
+            var segment = x[0].GeometryCurve;
+            var point = segment.Evaluate(segment.GetRawParameter(0.5), normalized: false).ToPoint3d();
+            var loop = brep.Loops.OrderBy(e => e.To3dCurve() is Curve loopCurve && loopCurve.ClosestPoint(point, out var t, GeometryTolerance.Model.VertexTolerance) ? loopCurve.PointAt(t).DistanceTo(point) : double.PositiveInfinity).FirstOrDefault();
+            switch (loop?.LoopType)
+            {
+              case BrepLoopType.Outer: return Enumerable.Repeat(+1, x.Count);
+              case BrepLoopType.Inner: return Enumerable.Repeat(-1, x.Count);
+              default: return Enumerable.Repeat(0, x.Count);
+            }
+          }).
+          TakeWhileIsNotEscapeKeyDown(this);
+        }
       );
 
       Params.TrySetData(DA, "Slope Arrow", () => sketch?.SlopeArrow);
