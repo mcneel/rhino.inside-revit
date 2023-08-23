ff0147564dc571dc371b12ba573ace1d4c39e1c4
kike-garbo <kike.garbo@gmail.com>
2023-08-23 10:22:13 +0200

    Fixed 'Python Editor' button tool-tip info.

--- src/RhinoInside.Revit.AddIn/Commands/Rhinoceros/CommandPython.cs
+++ src/RhinoInside.Revit.AddIn/Commands/Rhinoceros/CommandPython.cs
@@ -48,7 +48,6 @@ namespace RhinoInside.Revit.AddIn.Commands
 
       if (ribbonPanel.AddItem(buttonData) is PushButton pushButton)
       {
-        pushButton.LongDescription = $"Use CTRL key to open only Python editor window without restoring other tool windows";
         StoreButton(CommandName, pushButton);
       }
 #endif
