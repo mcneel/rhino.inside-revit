4b8f203bb21ee7914485fe0f75a1f72c0c8a1b5d
kike-garbo <kike.garbo@gmail.com>
2023-08-25 08:52:09 +0200

    Cleaning.

--- src/RhinoInside.Revit.Loader/Loader.cs
+++ src/RhinoInside.Revit.Loader/Loader.cs
@@ -9,47 +9,8 @@ using Autodesk.Revit.UI;
 
 namespace RhinoInside.Revit.AddIn
 {
-  class Distribution
-  {
-    public Distribution(int majorVersion) => MajorVersion = majorVersion;
-
-    public readonly int MajorVersion;
-    public bool Available => GetRhinoVersionInfo()?.Major == MajorVersion;
-
-    string RegistryPath => $@"SOFTWARE\McNeel\Rhinoceros\{MajorVersion}.0";
-
-    string SystemDir =>
-#if DEBUG
-      Microsoft.Win32.Registry.GetValue($@"HKEY_CURRENT_USER\{RegistryPath}-WIP-Developer-Debug-trunk\Install", "Path", null) as string ??
-#endif
-      Microsoft.Win32.Registry.GetValue($@"HKEY_LOCAL_MACHINE\{RegistryPath}\Install", "Path", null) as string;
-
-    public string BuildType =>
-#if DEBUG
-      Microsoft.Win32.Registry.GetValue($@"HKEY_CURRENT_USER\{RegistryPath}-WIP-Developer-Debug-trunk\Install", "BuildType", null) as string ??
-#endif
-      Microsoft.Win32.Registry.GetValue($@"HKEY_LOCAL_MACHINE\{RegistryPath}\Install", "BuildType", null) as string;
-
-    public string DisplayBuildType => BuildType?.ToUpperInvariant() == "COMMERCIAL" ? string.Empty : BuildType;
-
-    string RhinoExePath => string.IsNullOrEmpty(SystemDir) ? null : Path.Combine(SystemDir, "Rhino.exe");
-
-    public Version GetRhinoVersionInfo()
-    {
-      var RhinoVersionInfo = File.Exists(RhinoExePath) ? FileVersionInfo.GetVersionInfo(RhinoExePath) : null;
-      return new Version
-      (
-        RhinoVersionInfo?.FileMajorPart ?? 0,
-        RhinoVersionInfo?.FileMinorPart ?? 0,
-        RhinoVersionInfo?.FileBuildPart ?? 0,
-        RhinoVersionInfo?.FilePrivatePart ?? 0
-      );
-    }
-  }
-
   public class Loader : IExternalApplication
   {
-    internal static readonly Version Version = Assembly.GetExecutingAssembly().GetName().Version;
     internal static readonly Guid AddInId = new Guid("02EFF7F0-4921-4FD3-91F6-A87B6BA9BF74");
     IExternalApplication _ExternalApplication;
 
@@ -61,7 +22,7 @@ namespace RhinoInside.Revit.AddIn
       var assembly = Assembly.GetExecutingAssembly();
       var directory = Path.GetDirectoryName(assembly.Location);
 
-      if (GetDistribution() is Distribution distribution)
+      if (Distribution.Pick() is Distribution distribution)
       {
         try
         {
@@ -80,47 +41,85 @@ namespace RhinoInside.Revit.AddIn
       return _ExternalApplication?.OnShutdown(controlledApplication) ?? Result.Failed;
     }
 
-    Distribution GetDistribution()
+    class Distribution
     {
-      var distributions = new Distribution[]
+      public Distribution(int majorVersion) => MajorVersion = majorVersion;
+
+      public readonly int MajorVersion;
+      public bool Available => GetRhinoVersionInfo()?.Major == MajorVersion;
+
+      string RegistryPath => $@"SOFTWARE\McNeel\Rhinoceros\{MajorVersion}.0";
+
+      string SystemDir =>
+#if DEBUG
+        Microsoft.Win32.Registry.GetValue($@"HKEY_CURRENT_USER\{RegistryPath}-WIP-Developer-Debug-trunk\Install", "Path", null) as string ??
+#endif
+        Microsoft.Win32.Registry.GetValue($@"HKEY_LOCAL_MACHINE\{RegistryPath}\Install", "Path", null) as string;
+
+      public string BuildType =>
+#if DEBUG
+        Microsoft.Win32.Registry.GetValue($@"HKEY_CURRENT_USER\{RegistryPath}-WIP-Developer-Debug-trunk\Install", "BuildType", null) as string ??
+#endif
+        Microsoft.Win32.Registry.GetValue($@"HKEY_LOCAL_MACHINE\{RegistryPath}\Install", "BuildType", null) as string;
+
+      public string DisplayBuildType => BuildType?.ToUpperInvariant() == "COMMERCIAL" ? string.Empty : BuildType;
+
+      string RhinoExePath => string.IsNullOrEmpty(SystemDir) ? null : Path.Combine(SystemDir, "Rhino.exe");
+
+      public Version GetRhinoVersionInfo()
       {
-        new Distribution(7),
-        new Distribution(8),
-      };
-
-      var available = distributions.Where(x => x.Available).ToArray();
-      if (available.Length == 0) return distributions[0];
-      if (available.Length == 1) return available[0];
-
-      using
-      (
-        var taskDialog = new TaskDialog("Loading…")
-        {
-          Id = typeof(Loader).FullName,
-          MainIcon = TaskDialogIcon.TaskDialogIconInformation,
-          TitleAutoPrefix = true,
-          AllowCancellation = false,
-          MainInstruction = "Looks like you have many supported Rhino versions installed.",
-          MainContent = "Please pick which one you want to use.",
-          //VerificationText = "Do not show again"
-        }
-      )
-      {
-        for (int d = 0; d < 4 && d < available.Length; d++)
-        {
-          var distribution = available[d];
-          taskDialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink1 + d, $"Rhino {distribution.MajorVersion} {distribution.DisplayBuildType}", $"{distribution.GetRhinoVersionInfo()}");
-        }
-
-        taskDialog.DefaultButton = TaskDialogResult.CommandLink1;
-
-        var result = taskDialog.Show();
-
-        if (TaskDialogResult.CommandLink1 <= result && result <= TaskDialogResult.CommandLink4)
-          return distributions[result - TaskDialogResult.CommandLink1];
+        var RhinoVersionInfo = File.Exists(RhinoExePath) ? FileVersionInfo.GetVersionInfo(RhinoExePath) : null;
+        return new Version
+        (
+          RhinoVersionInfo?.FileMajorPart ?? 0,
+          RhinoVersionInfo?.FileMinorPart ?? 0,
+          RhinoVersionInfo?.FileBuildPart ?? 0,
+          RhinoVersionInfo?.FilePrivatePart ?? 0
+        );
       }
 
-      return null;
+      public static Distribution Pick()
+      {
+        var distributions = new Distribution[]
+        {
+          new Distribution(7),
+          new Distribution(8),
+        };
+
+        var available = distributions.Where(x => x.Available).ToArray();
+        if (available.Length == 0) return distributions[0];
+        if (available.Length == 1) return available[0];
+
+        using
+        (
+          var taskDialog = new TaskDialog("Loading…")
+          {
+            Id = typeof(Loader).FullName,
+            MainIcon = TaskDialogIcon.TaskDialogIconInformation,
+            TitleAutoPrefix = true,
+            AllowCancellation = false,
+            MainInstruction = "Looks like you have many supported Rhino versions installed.",
+            MainContent = "Please pick which one you want to use.",
+            //VerificationText = "Do not show again"
+          }
+        )
+        {
+          for (int d = 0; d < 4 && d < available.Length; d++)
+          {
+            var distribution = available[d];
+            taskDialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink1 + d, $"Rhino {distribution.MajorVersion} {distribution.DisplayBuildType}", $"{distribution.GetRhinoVersionInfo()}");
+          }
+
+          taskDialog.DefaultButton = TaskDialogResult.CommandLink1;
+
+          var result = taskDialog.Show();
+
+          if (TaskDialogResult.CommandLink1 <= result && result <= TaskDialogResult.CommandLink4)
+            return distributions[result - TaskDialogResult.CommandLink1];
+        }
+
+        return null;
+      }
     }
   }
 }
