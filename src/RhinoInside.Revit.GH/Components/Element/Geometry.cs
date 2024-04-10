317307355380b97c5fe020dc3105793de1a6ce81
kike-garbo <kike.garbo@gmail.com>
2024-04-10 12:17:55 +0200

    We don't need references here.

--- src/RhinoInside.Revit.GH/Components/Element/Geometry.cs
+++ src/RhinoInside.Revit.GH/Components/Element/Geometry.cs
@@ -468,7 +468,7 @@ namespace RhinoInside.Revit.GH.Components.Geometry
       }
 
       using (scope)
-      using (var options = new ARDB.Options() { DetailLevel = detailLevel ?? ARDB.ViewDetailLevel.Medium, ComputeReferences = true })
+      using (var options = new ARDB.Options() { DetailLevel = detailLevel ?? ARDB.ViewDetailLevel.Medium })
       {
         var _Elements_ = Params.IndexOfOutputParam("Elements");
         var _Geometry_ = Params.IndexOfOutputParam("Geometry");
