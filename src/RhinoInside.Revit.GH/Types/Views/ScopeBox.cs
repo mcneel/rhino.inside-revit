658658d2c1dabee3d53a49261e88597c190a7cfe
kike-garbo <kike.garbo@gmail.com>
2024-08-23 11:37:21 +0200

    Implemented `Types.ScopeBox.BakeElement`.

--- src/RhinoInside.Revit.GH/Types/Views/ScopeBox.cs
+++ src/RhinoInside.Revit.GH/Types/Views/ScopeBox.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Grasshopper.Kernel;
 using Rhino.Geometry;
+using Rhino.DocObjects;
+using Rhino;
 using ERDB = RhinoInside.Revit.External.DB;
 using ARDB = Autodesk.Revit.DB;
 
@@ -15,7 +17,7 @@ namespace RhinoInside.Revit.GH.Types
   using ARDB_ScopeBox = ARDB.Element;
 
   [Kernel.Attributes.Name("Scope Box")]
-  public class ScopeBox : GraphicalElement
+  public class ScopeBox : GraphicalElement, Bake.IGH_BakeAwareElement
   {
     protected override Type ValueType => typeof(ARDB_ScopeBox);
     public new ARDB_ScopeBox Value => base.Value as ARDB_ScopeBox;
@@ -137,5 +139,123 @@ namespace RhinoInside.Revit.GH.Types
       }
     }
     #endregion
+
+    #region IGH_BakeAwareElement
+    bool IGH_BakeAwareData.BakeGeometry(RhinoDoc doc, ObjectAttributes att, out Guid guid) =>
+      BakeElement(new Dictionary<ARDB.ElementId, Guid>(), true, doc, att, out guid);
+
+    public bool BakeElement
+    (
+      IDictionary<ARDB.ElementId, Guid> idMap,
+      bool overwrite,
+      RhinoDoc doc,
+      ObjectAttributes att,
+      out Guid guid
+    )
+    {
+      // 1. Check if is already cloned
+      if (idMap.TryGetValue(Id, out guid))
+        return true;
+
+      if (Value is ARDB_ScopeBox sectionBox)
+      {
+        att = att?.Duplicate() ?? doc.CreateDefaultAttributes();
+        att.Name = DisplayName;
+        if (Category.BakeElement(idMap, false, doc, att, out var layerGuid))
+          att.LayerIndex = doc.Layers.FindId(layerGuid).Index;
+
+        // 2. Check if already exist
+        var index = doc.Groups.Find(att.Name);
+
+        // 3. Update if necessary
+        if (index < 0 || overwrite)
+        {
+          if (index < 0) index = doc.Groups.Add(att.Name);
+          if (overwrite && index >= 0)
+          {
+            var objects = doc.Objects.FindByGroup(index);
+            var viewportIds = new Dictionary<int, HashSet<Guid>>();
+            foreach (var obj in objects)
+            {
+              if (obj is ClippingPlaneObject clipper)
+                viewportIds[viewportIds.Count] = new HashSet<Guid>(clipper.ClippingPlaneGeometry.ViewportIds());
+
+              doc.Objects.Delete(obj, quiet: true, ignoreModes: true);
+            }
+
+            var box = Box;
+
+            // Lines
+            {
+              var rectangles = new Rectangle3d[]
+              {
+                new Rectangle3d(new Plane(box.PointAt(0.0, 0.5, 0.5), box.Plane.YAxis, box.Plane.ZAxis), box.Y, box.Z), // left
+                new Rectangle3d(new Plane(box.PointAt(1.0, 0.5, 0.5), box.Plane.ZAxis, box.Plane.YAxis), box.Z, box.Y), // right
+                new Rectangle3d(new Plane(box.PointAt(0.5, 0.0, 0.5), box.Plane.ZAxis, box.Plane.XAxis), box.Z, box.X), // front
+                new Rectangle3d(new Plane(box.PointAt(0.5, 1.0, 0.5), box.Plane.XAxis, box.Plane.ZAxis), box.X, box.Z), // back
+                new Rectangle3d(new Plane(box.PointAt(0.5, 0.5, 0.0), box.Plane.XAxis, box.Plane.YAxis), box.X, box.Y), // bottom
+                new Rectangle3d(new Plane(box.PointAt(0.5, 0.5, 1.0), box.Plane.YAxis, box.Plane.XAxis), box.Y, box.X)  // top
+              };
+
+              using (var rectanglesAtt = att.Duplicate())
+              {
+                rectanglesAtt.AddToGroup(index);
+                rectanglesAtt.PlotColorSource = ObjectPlotColorSource.PlotColorFromDisplay;
+                foreach (var rectangle in rectangles)
+                  doc.Objects.Add(new PolylineCurve(rectangle.ToPolyline()), rectanglesAtt);
+              }
+            }
+
+#if RHINO_8
+            // Clipping Planes
+            {
+              ClippingPlaneSurface CreateClippingPlane(Plane plane, Interval u, Interval v) =>
+                new ClippingPlaneSurface(new PlaneSurface(plane, u, v));
+              
+              var planes = new (int Name, ClippingPlaneSurface Surface)[]
+              {
+                (0, CreateClippingPlane(new Plane(box.PointAt(0.0, 0.5, 0.5), box.Plane.YAxis, box.Plane.ZAxis), box.Y, box.Z)), // left
+                (1, CreateClippingPlane(new Plane(box.PointAt(1.0, 0.5, 0.5), box.Plane.ZAxis, box.Plane.YAxis), box.Z, box.Y)), // right
+                (2, CreateClippingPlane(new Plane(box.PointAt(0.5, 0.0, 0.5), box.Plane.ZAxis, box.Plane.XAxis), box.Z, box.X)), // front
+                (3, CreateClippingPlane(new Plane(box.PointAt(0.5, 1.0, 0.5), box.Plane.XAxis, box.Plane.ZAxis), box.X, box.Z)), // back
+                (4, CreateClippingPlane(new Plane(box.PointAt(0.5, 0.5, 0.0), box.Plane.XAxis, box.Plane.YAxis), box.X, box.Y)), // bottom
+                (5, CreateClippingPlane(new Plane(box.PointAt(0.5, 0.5, 1.0), box.Plane.YAxis, box.Plane.XAxis), box.Y, box.X))  // top
+              };
+
+              using (var planesAtt = att.Duplicate())
+              {
+                planesAtt.AddToGroup(index);
+                planesAtt.ColorSource = ObjectColorSource.ColorFromObject;
+                planesAtt.ObjectColor = System.Drawing.Color.FromArgb(0);
+                planesAtt.PlotColorSource = ObjectPlotColorSource.PlotColorFromDisplay;
+                var name = 0;
+                foreach (var plane in planes)
+                {
+                  plane.Surface.ParticipationListsEnabled = true;
+                  plane.Surface.SetClipParticipation(Array.Empty<Guid>(), new int[] { att.LayerIndex }, true);
+                  if (viewportIds.TryGetValue(name++, out var viewports))
+                  {
+                    foreach (var id in viewports)
+                      plane.Surface.AddClipViewportId(id);
+                  }
+                  doc.Objects.Add(plane.Surface, planesAtt);
+                }
+              }
+            }
+#endif
+          }
+        }
+
+        // TODO: Create a V5 Uuid out of the name
+        //guid = new Guid(0, 0, 0, BitConverter.GetBytes((long) index));
+        //idMap.Add(Id, guid);
+
+        return true;
+      }
+
+      return false;
+    }
+
+#endregion
   }
 }
