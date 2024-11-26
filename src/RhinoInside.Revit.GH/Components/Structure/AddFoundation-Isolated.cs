7f926f70016fff11d7c60ee9a5c6409299fa61a0
kike-garbo <kike.garbo@gmail.com>
2024-11-26 20:14:46 +0100

    Fixes #1220

--- src/RhinoInside.Revit.GH/Components/Structure/AddFoundation-Isolated.cs
+++ src/RhinoInside.Revit.GH/Components/Structure/AddFoundation-Isolated.cs
@@ -129,7 +129,7 @@ namespace RhinoInside.Revit.GH.Components.Structure
               break;
 
             case ARDB.FamilyHostingBehavior.Wall:
-              if (!(host is ARDB.Wall)) throw new Exceptions.RuntimeArgumentException("Type", $"Type '{type.Name}' instances should be hosted on a Wall.");
+              if (!(host is ARDB.Wall) && !(host is ARDB.FaceWall)) throw new Exceptions.RuntimeArgumentException("Type", $"Type '{type.Name}' instances should be hosted on a Wall.");
               break;
 
             case ARDB.FamilyHostingBehavior.Floor:
