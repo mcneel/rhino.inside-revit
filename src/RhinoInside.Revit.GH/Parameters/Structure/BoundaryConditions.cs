f47f22240c38b45e83e04c9d15ca9e7b47fe425c
Pedro Cortés <36660222+pedrocortesark@users.noreply.github.com>
2024-12-16 12:35:34 +0100

    Created base components for Boundary Conditions

---
+++ src/RhinoInside.Revit.GH/Parameters/Structure/BoundaryConditions.cs
@@ -0,0 +1,16 @@
+using System;
+using Grasshopper.Kernel;
+using ARDB = Autodesk.Revit.DB;
+
+namespace RhinoInside.Revit.GH.Parameters
+{
+  [ComponentVersion(introduced: "1.27")]
+  public class BoundaryConditions : GraphicalElement<Types.BoundaryConditions, ARDB.Structure.BoundaryConditions>
+  {
+    public override GH_Exposure Exposure => GH_Exposure.primary | GH_Exposure.obscure;
+    public override Guid ComponentGuid => new Guid("DEE191F4-9D73-46C7-9BD0-EBBBD5B8D4A6");
+
+    public BoundaryConditions() : base("Boundary Conditions", "Boundary Conditions", "Contains a collection of Revit Boundary Conditions", "Params", "Revit Elements") { }
+
+  }
+}
