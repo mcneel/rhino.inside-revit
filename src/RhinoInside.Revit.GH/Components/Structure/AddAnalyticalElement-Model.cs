85de0103254d4c9acc8ae4acf7e889c940a48827
kike-garbo <kike.garbo@gmail.com>
2024-12-17 12:58:08 +0100

    Updated descriptions.

--- src/RhinoInside.Revit.GH/Components/Structure/AddAnalyticalElement-Model.cs
+++ src/RhinoInside.Revit.GH/Components/Structure/AddAnalyticalElement-Model.cs
@@ -31,7 +31,7 @@ namespace RhinoInside.Revit.GH.Components.Structure
     (
       name: "Add Analytical Element (Model)",
       nickname: "ME-Analytical",
-      description: "Given a model element, it extracts its analytical element to the active Revit document",
+      description: "Given a model element, it adds an analytical element representation to the active Revit document",
       category: "Revit",
       subCategory: "Structure"
     )
