f1e4f9562e2f9ddadb45914738eea63455eaa37f
Pedro Cortes <p.cortes.nieves@gmail.com>
2024-12-18 12:48:56 +0100

    Revert "Added specific types for BoundaryConditions"
    This reverts commit e1f65ba9d979e90eb4c6df0bda69c2a78aac0e07.

---
+++ src/RhinoInside.Revit.GH/Components/Structure/AddBoundaryConditions-Area.cs
@@ -0,0 +1,143 @@
+using System;
+using Grasshopper.Kernel;
+using RhinoInside.Revit.Convert.Geometry;
+using RhinoInside.Revit.External.DB.Extensions;
+using ARDB = Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using System.Linq;
+using Rhino.Geometry;
+
+namespace RhinoInside.Revit.GH.Components.Structure
+{
+  [ComponentVersion(introduced: "1.27")]
+  public class AddAreaBoundaryConditions : ElementTrackerComponent
+  {
+    public override Guid ComponentGuid => new Guid("8C0FF24B-B5D7-40AA-BD4E-1DFF9326DE86");
+
+    public override GH_Exposure Exposure => GH_Exposure.senary;
+
+    public AddAreaBoundaryConditions() : base
+    (
+      name: "Add Boundary Conditions (Area)",
+      nickname: "BC-Area",
+      description: "Given a referenced face, this component adds area boundary conditions to the analytical model.",
+      category: "Revit",
+      subCategory: "Structure"
+    )
+    { }
+
+    protected override ParamDefinition[] Inputs => inputs;
+    static readonly ParamDefinition[] inputs =
+    {
+      new ParamDefinition
+      (
+        new Parameters.Document()
+        {
+          Name = "Document",
+          NickName = "DOC",
+          Description = "Document",
+          Optional = true
+        }, ParamRelevance.Occasional
+      ),
+      new ParamDefinition
+      (
+        new Parameters.GeometryFace()
+        {
+          Name = "Face",
+          NickName = "F",
+          Description = "Face to reference the boundary condition",
+        }
+      )
+    };
+
+    protected override ParamDefinition[] Outputs => outputs;
+    static readonly ParamDefinition[] outputs =
+    {
+      new ParamDefinition
+      (
+        new Parameters.BoundaryConditions()
+        {
+          Name = _BoundaryConditions_,
+          NickName = _BoundaryConditions_.Substring(0, 1),
+          Description = $"Output {_BoundaryConditions_}",
+        }
+      )
+    };
+
+    const string _BoundaryConditions_ = "Boundary Conditions";
+    static readonly ARDB.BuiltInParameter[] ExcludeUniqueProperties =
+    {
+
+    };
+
+    protected override void TrySolveInstance(IGH_DataAccess DA)
+    {
+      if (!Parameters.Document.TryGetDocumentOrCurrent(this, DA, "Document", out var doc) || !doc.IsValid) return;
+
+      ReconstructElement<ARDB.Structure.BoundaryConditions>
+      (
+        doc.Value, _BoundaryConditions_, boundaryConditions =>
+        {
+          if (!Params.GetData(DA, "Face", out Types.GeometryFace face)) return null;
+
+          boundaryConditions = Reconstruct
+          (
+            boundaryConditions,
+            doc.Value,
+            face
+          );
+
+          DA.SetData(_BoundaryConditions_, boundaryConditions);
+          return boundaryConditions;
+        }
+      );
+    }
+
+    bool Reuse(ARDB.Structure.BoundaryConditions bConditions, Types.GeometryFace face)
+    {
+      if (bConditions is null) return false;
+
+      var loopA = bConditions.GetLoops().First();
+      var loopB = GeometryEncoder.ToCurveLoop( face.PolySurface.Faces.First().OuterLoop.To3dCurve() );
+
+      for (int i = 0; i < loopA.Count(); i++)
+      {
+        if (!(loopA.ElementAt(i).IsSameKindAs(loopB.ElementAt(i)) &&
+          loopA.ElementAt(i).AlmostEquals(loopB.ElementAt(i))))
+        {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    ARDB.Structure.BoundaryConditions Create(ARDB.Document doc, Types.GeometryFace face)
+    {
+      var bConditions = doc.Create.NewAreaBoundaryConditions(face.GetReference(),
+                                                             TranslationRotationValue.Fixed, 0.0,
+                                                             TranslationRotationValue.Fixed, 0.0,
+                                                             TranslationRotationValue.Fixed, 0.0);
+      return bConditions;
+    }
+
+    ARDB.Structure.BoundaryConditions Reconstruct
+    (
+      ARDB.Structure.BoundaryConditions bConditions,
+      ARDB.Document doc,
+      Types.GeometryFace surface
+    )
+    {
+      if (!Reuse(bConditions, surface))
+      {
+        bConditions = bConditions.ReplaceElement
+        (
+          Create(doc, surface),
+          ExcludeUniqueProperties
+        );
+      }
+
+      return bConditions;
+    }
+  }
+}
