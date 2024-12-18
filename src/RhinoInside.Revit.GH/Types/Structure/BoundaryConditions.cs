0d1ae6208104146ef5f4bcea2772f6e5bb1ee957
Pedro Cortes <p.cortes.nieves@gmail.com>
2024-12-18 12:47:22 +0100

    Added types for specific Boundary Conditions

--- src/RhinoInside.Revit.GH/Types/Structure/BoundaryConditions.cs
+++ src/RhinoInside.Revit.GH/Types/Structure/BoundaryConditions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using ARDB = Autodesk.Revit.DB;
 
 namespace RhinoInside.Revit.GH.Types
@@ -11,6 +12,30 @@ namespace RhinoInside.Revit.GH.Types
 
     public BoundaryConditions() { }
     public BoundaryConditions(ARDB.Structure.BoundaryConditions boundaryConditions) : base(boundaryConditions) { }
+  }
+
+  [Kernel.Attributes.Name("Area Boundary Conditions")]
+  public class AreaBoundaryConditions : BoundaryConditions
+  {
+    protected override Type ValueType => typeof(AreaBoundaryConditions);
+    public AreaBoundaryConditions() { }
+    public AreaBoundaryConditions(ARDB.Structure.BoundaryConditions boundaryConditions) : base(boundaryConditions) { }
+  }
+
+  [Kernel.Attributes.Name("Line Boundary Conditions")]
+  public class LineBoundaryConditions : BoundaryConditions
+  {
+    protected override Type ValueType => typeof(PointBoundaryConditions);
+    public LineBoundaryConditions() { }
+    public LineBoundaryConditions(ARDB.Structure.BoundaryConditions boundaryConditions) : base(boundaryConditions) { }
+  }
+
+  [Kernel.Attributes.Name("Point Boundary Conditions")]
+  public class PointBoundaryConditions : BoundaryConditions
+  {
+    protected override Type ValueType => typeof(PointBoundaryConditions);
+    public PointBoundaryConditions() { }
+    public PointBoundaryConditions(ARDB.Structure.BoundaryConditions boundaryConditions) : base(boundaryConditions) { }
   }
 }
 
