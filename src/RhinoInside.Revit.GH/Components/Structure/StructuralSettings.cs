67e578c9da75262abd7d530f496a2f8ed7996d83
Pedro Cortes <p.cortes.nieves@gmail.com>
2024-12-19 09:55:20 +0100

    Added structural settings component

---
+++ src/RhinoInside.Revit.GH/Components/Structure/StructuralSettings.cs
@@ -0,0 +1,195 @@
+using System;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
+using RhinoInside.Revit.GH.Exceptions;
+using ARDB = Autodesk.Revit.DB;
+
+namespace RhinoInside.Revit.GH.Components.Structure
+{
+  [ComponentVersion(introduced: "1.27")]
+  public class StructuralSettings : TransactionalChainComponent
+  {
+    public override Guid ComponentGuid => new Guid("B5144C5D-F374-4786-99A7-6A579ED2FD59");
+
+    public StructuralSettings() : base
+    (
+      name: "Structural Settings",
+      nickname: "SS",
+      description: "Main structural settings associated with a Revit document.",
+      category: "Revit",
+      subCategory: "Structure"
+    )
+    { }
+
+    protected override ParamDefinition[] Inputs => inputs;
+    static readonly ParamDefinition[] inputs =
+    {
+      new ParamDefinition
+      (
+        new Parameters.Document()
+        {
+          Name = "Document",
+          NickName = "DOC",
+          Description = "Document",
+          Optional = true
+        }, ParamRelevance.Occasional
+      ),
+      new ParamDefinition
+      (
+        new Param_Number
+        {
+          Name = _Spacing_,
+          NickName = _Spacing_.Substring(0,1),
+          Description = "Symbol spacing for boundary conditions.",
+          Optional = true,
+        }, ParamRelevance.Primary
+      ),
+      new ParamDefinition
+      (
+        new Parameters.FamilySymbol()
+        {
+          Name = _Fixed_,
+          NickName = _Fixed_.Substring(0,1),
+          Description = "The FamilySymbol to represent a fixed boundary condition.",
+          Optional = true,
+        }, ParamRelevance.Primary
+      ),
+      new ParamDefinition
+      (
+        new Parameters.FamilySymbol()
+        {
+          Name = _Pinned_,
+          NickName = _Pinned_.Substring(0,1),
+          Description = "The FamilySymbol to represent a pinned boundary condition.",
+          Optional = true,
+        }, ParamRelevance.Primary
+      ),
+      new ParamDefinition
+      (
+        new Parameters.FamilySymbol()
+        {
+          Name = _Roller_,
+          NickName = _Roller_.Substring(0,1),
+          Description = "The FamilySymbol to represent a roller boundary condition.",
+          Optional = true,
+        }, ParamRelevance.Primary
+      ),
+      new ParamDefinition
+      (
+        new Parameters.FamilySymbol()
+        {
+          Name = _User_,
+          NickName = _User_.Substring(0,1),
+          Description = "The FamilySymbol to represent a user defined boundary condition.",
+          Optional = true,
+        }, ParamRelevance.Primary
+      )
+    };
+
+    protected override ParamDefinition[] Outputs => outputs;
+    static readonly ParamDefinition[] outputs =
+    {
+      new ParamDefinition
+      (
+        new Param_Number
+        {
+          Name = _Spacing_,
+          NickName = _Spacing_.Substring(0,1),
+          Description = "Symbol spacing for boundary conditions.",
+        }, ParamRelevance.Primary
+      ),
+      new ParamDefinition
+      (
+        new Parameters.FamilySymbol()
+        {
+          Name = _Fixed_,
+          NickName = _Fixed_.Substring(0,1),
+          Description = "The FamilySymbol to represent a fixed boundary condition.",
+        }, ParamRelevance.Primary
+      ),
+      new ParamDefinition
+      (
+        new Parameters.FamilySymbol()
+        {
+          Name = _Pinned_,
+          NickName = _Pinned_.Substring(0,1),
+          Description = "The FamilySymbol to represent a pinned boundary condition.",
+        }, ParamRelevance.Primary
+      ),
+      new ParamDefinition
+      (
+        new Parameters.FamilySymbol()
+        {
+          Name = _Roller_,
+          NickName = _Roller_.Substring(0,1),
+          Description = "The FamilySymbol to represent a roller boundary condition.",
+        }, ParamRelevance.Primary
+      ),
+      new ParamDefinition
+      (
+        new Parameters.FamilySymbol()
+        {
+          Name = _User_,
+          NickName = _User_.Substring(0,1),
+          Description = "The FamilySymbol to represent a user defined boundary condition.",
+        }, ParamRelevance.Primary
+      )
+    };
+
+    const string _Spacing_ = "Spacing";
+    const string _Fixed_ = "Fixed";
+    const string _Pinned_ = "Pinned";
+    const string _Roller_ = "Roller";
+    const string _User_ = "User";
+
+    protected override void TrySolveInstance(IGH_DataAccess DA)
+    {
+      if (!Parameters.Document.TryGetDocumentOrCurrent(this, DA, "Document", out var doc)) return;
+      var settings = Parameters.Document.GetStructuralSettings(this, DA, "Document", out var hasSymbols);
+
+      if (!Params.TryGetData(DA, _Fixed_, out Types.FamilySymbol fixedSymbol)) return;
+      if (!Params.TryGetData(DA, _Pinned_, out Types.FamilySymbol pinnedSymbol)) return;
+      if (!Params.TryGetData(DA, _Roller_, out Types.FamilySymbol rollerSymbol)) return;
+      if (!Params.TryGetData(DA, _User_, out Types.FamilySymbol userSymbol)) return;
+
+      if (!Params.TryGetData(DA, _Spacing_, out double? spacing)) return;
+      if (spacing.HasValue)
+      {
+        StartTransaction(doc.Value);
+        settings.BoundaryConditionAreaAndLineSymbolSpacing = spacing.Value / Revit.ModelUnits;
+      }
+      Params.TrySetData(DA, _Spacing_, () => settings.BoundaryConditionAreaAndLineSymbolSpacing * Revit.ModelUnits);
+
+      if (fixedSymbol is object)
+      {
+        StartTransaction(doc.Value);
+        settings.BoundaryConditionFamilySymbolFixed = fixedSymbol.Id;
+      }
+      Params.TrySetData(DA, _Fixed_, () => Types.FamilySymbol.FromElementId(doc.Value, settings.BoundaryConditionFamilySymbolFixed));
+
+      if (pinnedSymbol is object)
+      {
+        StartTransaction(doc.Value);
+        settings.BoundaryConditionFamilySymbolPinned = pinnedSymbol.Id;
+      }
+      Params.TrySetData(DA, "Pinned",
+          () => doc.Value.GetElement(settings.BoundaryConditionFamilySymbolPinned) as ARDB.FamilySymbol);
+
+      if (rollerSymbol is object)
+      {
+        StartTransaction(doc.Value);
+        settings.BoundaryConditionFamilySymbolRoller = rollerSymbol.Id;
+      }
+      Params.TrySetData(DA, "Roller",
+          () => doc.Value.GetElement(settings.BoundaryConditionFamilySymbolRoller) as ARDB.FamilySymbol);
+
+      if (userSymbol is object)
+      {
+        StartTransaction(doc.Value);
+        settings.BoundaryConditionFamilySymbolUserDefined = userSymbol.Id;
+      }
+      Params.TrySetData(DA, "User",
+          () => doc.Value.GetElement(settings.BoundaryConditionFamilySymbolUserDefined) as ARDB.FamilySymbol);
+    }
+  }
+}
