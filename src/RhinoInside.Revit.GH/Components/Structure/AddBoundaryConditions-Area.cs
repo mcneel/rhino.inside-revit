a7ebb2bc060716b3b0e178c8d59b57e6f8c455bc
kike-garbo <kike.garbo@gmail.com>
2024-12-20 19:19:02 +0100

    Chech for a valid fixed symbol.

--- src/RhinoInside.Revit.GH/Components/Structure/AddBoundaryConditions-Area.cs
+++ src/RhinoInside.Revit.GH/Components/Structure/AddBoundaryConditions-Area.cs
@@ -75,8 +75,11 @@ namespace RhinoInside.Revit.GH.Components.Structure
     protected override void TrySolveInstance(IGH_DataAccess DA)
     {
       if (!Parameters.Document.TryGetDocumentOrCurrent(this, DA, "Document", out var doc) || !doc.IsValid) return;
-      if (!Parameters.Document.TryGetStructuralSettings(doc, out var _))
+      if (!Parameters.Document.TryGetStructuralSettings(doc, out var settings) || !settings.BoundaryConditionFamilySymbolFixed.IsValid())
+      {
         AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Document does not have symbols for Boundary Conditions representation.");
+        return;
+      }
 
       ReconstructElement<ARDB.Structure.BoundaryConditions>
       (
