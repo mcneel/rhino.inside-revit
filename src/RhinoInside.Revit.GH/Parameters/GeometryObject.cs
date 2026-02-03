0bf27fa7781b0dde439dbc37bd2df46d6736a368
kike-garbo <kike.garbo@gmail.com>
2026-02-03 13:23:31 +0100

    Added some casting to 'Point Reference' parameter.

--- src/RhinoInside.Revit.GH/Parameters/GeometryObject.cs
+++ src/RhinoInside.Revit.GH/Parameters/GeometryObject.cs
@@ -88,7 +88,7 @@ namespace RhinoInside.Revit.GH.Parameters
   {
     public override GH_Exposure Exposure => GH_Exposure.tertiary | GH_Exposure.hidden;
     public override Guid ComponentGuid => new Guid("7A41402E-7B6C-4523-9B57-E8485713F461");
-    public GeometryObject() : base("Geometry", "Geometry", "Contains a collection of Revit geometry", "Params", "Revit") { }
+    public GeometryObject() : base("Geometry Reference", "Geometry", "Contains a collection of Revit geometry", "Params", "Revit") { }
     protected override string IconTag => string.Empty;
 
     #region UI
@@ -106,7 +106,7 @@ namespace RhinoInside.Revit.GH.Parameters
   {
     public override GH_Exposure Exposure => GH_Exposure.tertiary;
     public override Guid ComponentGuid => new Guid("BC1B160A-DC04-4139-AB7D-1AECBDE7FF88");
-    public GeometryPoint() : base("Point", "Point", "Contains a collection of Revit points", "Params", "Revit") { }
+    public GeometryPoint() : base("Point Reference", "Point", "Contains a collection of Revit points", "Params", "Revit") { }
 
     #region ISelectionFilter
     public virtual bool AllowElement(ARDB.Element elem) => true;
@@ -117,7 +117,7 @@ namespace RhinoInside.Revit.GH.Parameters
     #region UI methods
     protected override IEnumerable<string> ConvertsTo => base.ConvertsTo.Concat
     (
-      new string[] { "Box", "Point", "Line Style", "Category" }
+      new string[] { "Point", "Plane", "Box", "Curve Reference", "Line Style", "Category" }
     );
 
     //protected override GH_GetterResult Prompt_Plural(ref List<Types.IGH_PointGeometry> value)
@@ -207,7 +207,7 @@ namespace RhinoInside.Revit.GH.Parameters
   {
     public override GH_Exposure Exposure => GH_Exposure.tertiary;
     public override Guid ComponentGuid => new Guid("B79FD0FD-63AE-4776-A0A7-6392A3A58B0D");
-    public GeometryCurve() : base("Curve", "Curve", "Contains a collection of Revit curves", "Params", "Revit") { }
+    public GeometryCurve() : base("Curve Reference", "Curve", "Contains a collection of Revit curves", "Params", "Revit") { }
 
     #region ISelectionFilter
     public virtual bool AllowElement(ARDB.Element elem) => true;
@@ -268,7 +268,7 @@ namespace RhinoInside.Revit.GH.Parameters
   {
     public override GH_Exposure Exposure => GH_Exposure.tertiary;
     public override Guid ComponentGuid => new Guid("759700ED-BC79-4986-A6AB-84921A7C9293");
-    public GeometryFace() : base("Face", "Face", "Contains a collection of Revit faces", "Params", "Revit") { }
+    public GeometryFace() : base("Face Reference", "Face", "Contains a collection of Revit faces", "Params", "Revit") { }
 
     #region ISelectionFilter
     public virtual bool AllowElement(ARDB.Element elem) => true;
