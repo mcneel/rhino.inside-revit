117ac90ddd87c58911666cd36681c4237bca2e44
kike-garbo <kike.garbo@gmail.com>
2026-02-28 20:23:13 +0100

    Added `PathExtension.TryGetFullyQualifiedPath`.

--- src/RhinoInside.Revit.GH/Components/Views/ExportImage.cs
+++ src/RhinoInside.Revit.GH/Components/Views/ExportImage.cs
@@ -277,7 +277,7 @@ namespace RhinoInside.Revit.GH.Components.Views
         if (folder?.Any(x => Path.GetInvalidPathChars().Contains(x)) is true)
           throw new Exceptions.RuntimeArgumentException("Folder", $"'{folder}' is not a valid folder name", folder);
 
-        if (folder?.IsFullyQualifiedPath() is false)
+        if (!PathExtension.TryGetFullyQualifiedPath(ref folder))
           throw new Exceptions.RuntimeArgumentException("Folder", $"'{folder}' is not a valid absolute path", folder);
       }
 
